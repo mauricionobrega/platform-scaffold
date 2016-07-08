b403fd9ac3a18e169b86afe0cab67be7de22c5a1
Mike Klemarewski <mikek@mobify.com>
2016-07-08 12:39:07 -0700

    Update deps and refactor

--- app/containers/app/container.js
+++ app/containers/app/container.js
@@ -1,7 +1,35 @@
 import {connect} from 'react-redux'
 
-import App from '../../components/app'
 import * as appActions from './actions'
+
+class App extends React.Component {
+
+    componentDidMount() {
+        this.props.removePreloader()
+    }
+
+    render() {
+        let templateName = 't-' + this.props.children.props.route.routeName
+
+        return (
+            <div id="outer-container" className={templateName} style={{height: '100%'}}>
+                <main id="page-wrap">
+                    <header>
+                        Header content
+                    </header>
+
+                    <div id="content">
+                        {this.props.children}
+                    </div>
+
+                    <footer>
+                        Footer content
+                    </footer>
+                </main>
+            </div>
+        )
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
