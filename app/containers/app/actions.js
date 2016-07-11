77d4ca11cf1c6399780daef37e9e90c7e3db9f5e
Mike Klemarewski <mikek@mobify.com>
2016-07-11 11:30:29 -0700

    Remove preloader stuff

--- app/containers/app/actions.js
+++ app/containers/app/actions.js
@@ -1,17 +1,1 @@
 import {createAction} from '../../utils/utils'
-
-export const preloaderRemoved = createAction('Preloader Removed')
-
-export const removePreloader = () => {
-    return (dispatch) => {
-        let preloaderNodes = document.getElementsByClassName('capture-remove')
-
-        if (typeof preloaderNodes !== 'undefined') {
-            for (let i = 0; i < preloaderNodes.length; i++) {
-                preloaderNodes[i].parentNode.removeChild(preloaderNodes[i])
-            }
-        }
-
-        dispatch(preloaderRemoved())
-    }
-}
