95a047708a5a05004b55e2e4b2971964e82889a6
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-07-13 13:14:40 -0700

    Add redux-ava and port the reducer tests over.

--- app/containers/plp/reducer.test.js
+++ app/containers/plp/reducer.test.js
@@ -1,16 +1,12 @@
 import test from 'ava'
+import {reducerTest} from 'redux-ava'
 import {Map} from 'immutable'
 
 import reducer from './reducer'
 
-test('unknown action type leaves state unchanged', (t) => {
-    const action = {
-        type: 'qwertyuiop'
-    }
-    const inputState = Map({
-        test: true,
-        item: false,
-    })
-
-    t.is(reducer(inputState, action), inputState)
-})
+test('unknown action type leaves state unchanged', reducerTest(
+    reducer,
+    Map({test: true}),
+    {type: 'QWERTYUIOP'},
+    Map({test: true})
+))
