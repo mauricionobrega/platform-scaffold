fd4810386f3d9acf27351f37d967cbca61350ae0
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-07-18 15:55:46 -0700

    Add scripts for generating components.

---
+++ component-skeletons/stateful.jsx
@@ -0,0 +1,25 @@
+import {PropTypes} from 'react'
+
+class <%= component.Name %> extends React.Component {
+    componentDidUpdate() {
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+    }
+
+    render() {
+        const {
+        } = this.props
+
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+<%= component.Name %>.propTypes = {
+
+}
+
+export default <%= component.Name %>
---
+++ component-skeletons/stateless.jsx
@@ -0,0 +1,16 @@
+import {PropTypes} from 'react'
+import {Link} from 'react-router'
+
+const <%= component.Name %> = ({}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+<%= component.Name %>.propTypes = {
+
+}
+
+export default <%= component.Name %>
