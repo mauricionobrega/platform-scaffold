cb7bb702a4d2eab9f39d57dadc5a7874f635adb9
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-07-19 10:04:02 -0700

    Split out some stuff from generate-root-reducer.js

--- generators/common.js
+++ generators/common.js
@@ -13,9 +13,11 @@ const APP_CONTAINER_DIR = path.join('app', 'containers')
 
 const camel2Pascal = (name) => name.replace(/^[a-z]/, (c) => c.toUpperCase())
 const camel2dashed = (name) => name.replace(/[A-Z]/g, (c) => `-${c.toLowerCase()}`)
+const dashed2camel = (name) => name.replace(/-(\w)/g, (_, letter) => letter.toUpperCase())
 
 const greenWrite = (text) => process.stdout.write(chalk.green(text))
 const redWrite = (text) => process.stdout.write(chalk.red(text))
+const printCheckMark = () => greenWrite(' ✓\n')
 
 const processTemplate = (varname, context) => (templateString) =>
       template(templateString, {variable: varname})(context)
@@ -25,15 +27,38 @@ const getUserInput = (schema) => {
     return prompt.getAsync(schema)
 }
 
+const clearNulls = (items) => items.filter((item) => item !== null)
+
+const step = (message, operation) => (value) => {
+    return Promise.resolve(value)
+        .tap(() => process.stdout.write(message))
+        .then(operation)
+        .tap(printCheckMark)
+}
+
+const spreadStep = (message, operation) => (value) => {
+    return Promise.resolve(value)
+        .tap(() => process.stdout.write(message))
+        .spread(operation)
+        .tap(printCheckMark)
+}
+
+
 module.exports = {
     APP_CONTAINER_DIR,
 
     camel2Pascal,
     camel2dashed,
+    dashed2camel,
 
     greenWrite,
     redWrite,
+    printCheckMark,
 
     getUserInput,
     processTemplate,
+    clearNulls,
+
+    step,
+    spreadStep,
 }
--- generators/generate-root-reducer.js
+++ generators/generate-root-reducer.js
@@ -3,65 +3,44 @@
 
 const Promise = require('bluebird')
 
-const chalk = require('chalk')
 const fs = Promise.promisifyAll(require('fs'))
 const path = require('path')
 const template = require('lodash.template')
 
-const APP_CONTAINER_DIR = path.join(__dirname, 'app', 'containers')
-
-const printCheckMark = () => process.stdout.write(chalk.green(' ✓\n'))
-
-const step = (message, operation) => (value) => {
-    return Promise.resolve(value)
-        .tap(() => process.stdout.write(message))
-        .then(operation)
-        .tap(printCheckMark)
-}
-
-const spreadStep = (message, operation) => (value) => {
-    return Promise.resolve(value)
-        .tap(() => process.stdout.write(message))
-        .spread(operation)
-        .tap(printCheckMark)
-}
-
-const dashed2camel = (name) => name.replace(/-(\w)/g, (_, letter) => letter.toUpperCase())
-
-const clearNulls = (items) => items.filter((item) => item !== null)
+const common = require('./common')
 
 const filterDirectories = (items) => {
     return Promise.map(items, (item) => {
-        return fs.statAsync(path.join(APP_CONTAINER_DIR, item))
+        return fs.statAsync(path.join(common.APP_CONTAINER_DIR, item))
             .then((stats) => { return stats.isDirectory() ? item : null })
     })
-        .then(clearNulls)
+        .then(common.clearNulls)
 }
 
 const filterReducers = (dirs) => {
     return Promise.map(dirs, (dir) => {
-        return fs.statAsync(path.join(APP_CONTAINER_DIR, dir, 'reducer.js'))
+        return fs.statAsync(path.join(common.APP_CONTAINER_DIR, dir, 'reducer.js'))
             .then((stats) => { return stats.isFile() ? dir : null })
             .catch(() => null)
-    }).then(clearNulls)
+    }).then(common.clearNulls)
 }
 
 const containersFromDirs = (dirs) => dirs.map((directory) => {
     return {
         directory,
-        identifier: dashed2camel(directory)
+        identifier: common.dashed2camel(directory)
     }
 })
 
 const getContainers = () => {
-    return fs.readdirAsync(APP_CONTAINER_DIR)
+    return fs.readdirAsync(common.APP_CONTAINER_DIR)
         .then(filterDirectories)
         .then(filterReducers)
         .then(containersFromDirs)
 }
 
 const getTemplate = (containers) => {
-    return fs.readFileAsync(path.resolve('reducers.template.js'), 'utf8')
+    return fs.readFileAsync(path.join('generators', 'reducers.template.js'), 'utf8')
         .then((scriptTemplate) => {
             return [scriptTemplate, {containers}]
         })
@@ -71,14 +50,14 @@ const generateRootReducer = (scriptTemplate, context) =>
       template(scriptTemplate, {variable: 'context'})(context)
 
 const writeOutScript = (script) => fs.writeFileAsync(
-    path.join(APP_CONTAINER_DIR, 'reducers.js'),
+    path.join(common.APP_CONTAINER_DIR, 'reducers.js'),
     script,
     'utf8'
 )
 
 Promise.resolve()
-    .then(step('Finding container directories', getContainers))
-    .then(step('Loading root reducer template', getTemplate))
-    .then(spreadStep('Generating program text', generateRootReducer))
-    .then(step('Writing new root reducer', writeOutScript))
-    .then(() => process.stdout.write(chalk.green('Finished successfully\n')))
+    .then(common.step('Finding container directories', getContainers))
+    .then(common.step('Loading root reducer template', getTemplate))
+    .then(common.spreadStep('Generating program text', generateRootReducer))
+    .then(common.step('Writing new root reducer', writeOutScript))
+    .then(() => common.greenWrite('Finished successfully\n'))
