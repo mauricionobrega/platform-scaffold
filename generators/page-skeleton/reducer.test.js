d1c5f238e7c063329f9e4419c7d5cede51817973
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-07-19 13:50:00 -0700

    Add tests to the generated page container.

---
+++ generators/page-skeleton/reducer.test.js
@@ -0,0 +1,17 @@
+import test from 'ava'
+import {Map} from 'immutable'
+
+import reducer from './reducer'
+// import * as Actions from './actions'
+
+test('unknown action type leaves state unchanged', (t) => {
+    const action = {
+        type: 'qwertyuiop'
+    }
+    const inputState = Map({
+        test: true,
+        item: false,
+    })
+
+    t.is(reducer(inputState, action), inputState)
+})
