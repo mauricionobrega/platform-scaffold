435cc50d66e47a15db1d77d87ceda56bba637b65
Mike Klemarewski <mikek@mobify.com>
2016-07-27 11:17:01 -0700

    Add in the preloader

---
+++ app/preloader/preload.js
@@ -0,0 +1,6 @@
+// This script will run before your project bundle is loaded
+// (the views, UI, etc.). Use this script to do things like RUM
+// (Real User Monitoring) setup.
+
+// document.querySelector('.js-preload-logo').addEventListener('load', function() {
+// });
