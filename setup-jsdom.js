9a58a8cc1d77b5f0b64b5687b508b121be3d8948
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-08-09 17:21:16 -0700

    Fix lint error.

--- setup-jsdom.js
+++ setup-jsdom.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-commonjs */
 global.document = require('jsdom').jsdom('<body></body>')
+/* eslint-enable import/no-commonjs */
 global.window = document.defaultView
 global.navigator = window.navigator
