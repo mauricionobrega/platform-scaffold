04b1d6ba236fd5f063336a07e533850b27970f01
Mike Klemarewski <mikek@mobify.com>
2016-08-10 15:33:09 -0700

    Add tasks to upload and save-credentials

--- webpack/production.js
+++ webpack/production.js
@@ -9,11 +9,7 @@ const baseMainConfig = require('./base.main')
 // Add production flag to main app config
 const productionMainConfig = assign(baseMainConfig, {
     plugins: [].concat(baseMainConfig.plugins,
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production')
-            }
-        })
+        // Add production plugins here
     )
 })
 
