58c413f69da4933776442785f0bf7808f2cae13e
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-08-16 10:45:32 -0700

    Switch over from react-router Link to the SDK one

--- app/containers/home/container.js
+++ app/containers/home/container.js
@@ -1,6 +1,6 @@
 import {PropTypes} from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router'
+import Link from 'progressive-web-sdk/dist/components/link'
 import styles from './home.scss'
 
 import * as homeActions from './actions'
