ddabf399a909866f94b4e542da5d758200608081
Jeff Kamo <kamojeffrey@gmail.com>
2016-08-29 12:45:54 -0700

    Add missing NODE_ENV settings to production webpack config

--- webpack/production.js
+++ webpack/production.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-commonjs */
 
+const webpack = require('webpack')
 const assign = require('lodash.assign')
 
 const baseLoaderConfig = require('./base.loader')
@@ -9,6 +10,11 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 // Add production flag to main app config
 const productionMainConfig = assign(baseMainConfig, {
     // Extend base config with production settings here
+    plugins: [].concat(baseMainConfig.plugins, [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+        })
+    ])
 })
 
 baseMainConfig.module.loaders = baseMainConfig.module.loaders.concat({
