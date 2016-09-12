bcb64742366d46b50089d17d30fb8c51dde57956
Mike Klemarewski <mikek@mobify.com>
2016-09-12 10:52:08 -0700

    Return promise from onSubmit

--- app/containers/login/actions.js
+++ app/containers/login/actions.js
@@ -2,8 +2,6 @@ import {jqueryResponse} from 'progressive-web-sdk/dist/jquery-response'
 import {createAction} from '../../utils/utils'
 import loginParser from './parsers/login'
 
-const loginUrl = 'http://www.merlinspotions.com/customer/account/loginPost/'
-
 export const receiveLoginContents = createAction('Login page contents received')
 
 export const fetchLoginContents = () => {
@@ -21,33 +19,6 @@ export const fetchLoginContents = () => {
         })
         .then(([$, $response]) => {
             dispatch(receiveLoginContents(loginParser($, $response)))
-        })
-    }
-}
-
-export const attemptLogin = (formData) => {
-    return (dispatch) => {
-        const postBody = ''
-            .concat(`form_key=${formData.form_key}`)
-            .concat('&')
-            .concat(`login[username]=${encodeURIComponent(formData.login.username)}`)
-            .concat('&')
-            .concat(`login[password]=${encodeURIComponent(formData.login.password)}`)
-            .concat('&')
-            .concat(`persistent_remember_me=${formData.persistent_remember_me}`)
-
-        fetch(loginUrl, {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Cookie': `${document.cookie}; form_key=${formData.form_key}`,
-            },
-            body: postBody
-        }).then((response) => {
-            if (response.status === 200) {
-                window.location.href = response.url
-            }
         })
     }
 }
--- app/containers/login/container.js
+++ app/containers/login/container.js
@@ -10,14 +10,45 @@ import LoginForm from '../../components/login-form'
 
 import * as loginActions from './actions'
 
+const loginUrl = 'http://www.merlinspotions.com/customer/account/loginPost/'
+
 export class Login extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.onSubmitLoginForm = this.onSubmitLoginForm.bind(this)
+    }
 
     componentDidMount() {
         this.props.fetchLoginContents()
     }
 
     onSubmitLoginForm(formData) {
-        this.props.attemptLogin(formData)
+        const postBody = ''
+            .concat(`form_key=${formData.form_key}`)
+            .concat('&')
+            .concat(`login[username]=${encodeURIComponent(formData.login.username)}`)
+            .concat('&')
+            .concat(`login[password]=${encodeURIComponent(formData.login.password)}`)
+            .concat('&')
+            .concat(`persistent_remember_me=${formData.persistent_remember_me}`)
+
+        // Returning a promise from onSubmit helps Redux-Form determine when
+        // the form is finished submitting
+        return fetch(loginUrl, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cookie': `${document.cookie}; form_key=${formData.form_key}`,
+            },
+            body: postBody
+        }).then((response) => {
+            if (response.status === 200) {
+                window.location.href = response.url
+            }
+        })
     }
 
     shouldComponentUpdate(nextProps) {
@@ -31,7 +62,7 @@ export class Login extends React.Component {
                 {this.props.loginForm.fields &&
                     <LoginForm
                         formFields={this.props.loginForm.fields}
-                        onSubmit={this.onSubmitLoginForm.bind(this)} />
+                        onSubmit={this.onSubmitLoginForm} />
                 }
             </div>
         )
@@ -47,8 +78,7 @@ export const mapStateToProps = (state, props) => {
 
 export const mapDispatchToProps = (dispatch, props) => {
     return {
-        fetchLoginContents: () => dispatch(loginActions.fetchLoginContents()),
-        attemptLogin: (formData) => dispatch(loginActions.attemptLogin(formData))
+        fetchLoginContents: () => dispatch(loginActions.fetchLoginContents())
     }
 }
 
