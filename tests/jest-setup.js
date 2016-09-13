08397d5da88db0c32e7601237edec4599fa49ec7
fractaltheory <michael@mobify.com>
2016-09-13 15:48:49 -0700

    Comments

--- tests/jest-setup.js
+++ tests/jest-setup.js
@@ -1,2 +1,4 @@
+// Our tests require our specific Promise polyfill to run properly
 global.Promise = require.requireActual('es6-promise');
+// Parser tests need to be supplied a selector library
 global.$ = require('../static/js/jquery.min.js');
