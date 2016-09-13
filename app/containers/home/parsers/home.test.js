b30df254ece6ca515657878ddc82cf3e8d4d01b5
fractaltheory <michael@mobify.com>
2016-09-13 16:42:35 -0700

    Prefer using the full path to the test fixture

--- app/containers/home/parsers/home.test.js
+++ app/containers/home/parsers/home.test.js
@@ -1,9 +1,8 @@
-import path from 'path'
 import {jquerifyHtmlFile} from 'progressive-web-sdk/dist/test-utils'
 import homeParser from './home'
 
 test('the title is parsed', () => {
-    const home = homeParser(global.$, jquerifyHtmlFile(path.join(__dirname, 'home.test.html')))
+    const home = homeParser(global.$, jquerifyHtmlFile('app/containers/home/parsers/home.test.html'))
     expect(home).toBeTruthy()
     expect(home.title).toBeTruthy()
     expect(home.title).toBe('Home page')
