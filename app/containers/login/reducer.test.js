6b1904960ecca23873dcfbd635332557b55b9887
Mike Klemarewski <mikek@mobify.com>
2016-09-13 10:28:39 -0700

    Remove unused var

--- app/containers/login/reducer.test.js
+++ app/containers/login/reducer.test.js
@@ -2,7 +2,6 @@ import test from 'ava'
 import {Map} from 'immutable'
 
 import reducer from './reducer'
-import * as Actions from './actions'
 
 test('unknown action type leaves state unchanged', (t) => {
     const action = {
