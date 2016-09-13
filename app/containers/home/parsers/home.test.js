a79328c912f8aa9268e21140fd778ddcbd0c7b87
fractaltheory <michael@mobify.com>
2016-09-13 15:13:40 -0700

    “Fix” parser test

--- app/containers/home/parsers/home.test.js
+++ app/containers/home/parsers/home.test.js
@@ -1,8 +1,9 @@
+const path = require('path')
 import {jquerifyHtmlFile} from 'progressive-web-sdk/dist/test-utils'
 import homeParser from './home'
 
 test('the title is parsed', () => {
-    const home = homeParser(window.$, jquerifyHtmlFile('home.test.html'))
+    const home = homeParser($, jquerifyHtmlFile(path.join(__dirname, 'home.test.html')))
     expect(home).toBeTruthy()
     expect(home.title).toBeTruthy()
     expect(home.title).toBe('Home page')
