fdf611887b88b92e5c5ada24ae1344b70b3ff582
fractaltheory <michael@mobify.com>
2016-09-14 14:40:25 -0700

    Prevent need for explicitly using global object
    - when referencing jQuery $

--- app/containers/home/parsers/home.test.js
+++ app/containers/home/parsers/home.test.js
@@ -2,7 +2,7 @@ import {jquerifyHtmlFile} from 'progressive-web-sdk/dist/test-utils'
 import homeParser from './home'
 
 test('the title is parsed', () => {
-    const home = homeParser(global.$, jquerifyHtmlFile('app/containers/home/parsers/home.test.html'))
+    const home = homeParser($, jquerifyHtmlFile('app/containers/home/parsers/home.test.html'))
     expect(home).toBeTruthy()
     expect(home.title).toBeTruthy()
     expect(home.title).toBe('Home page')
