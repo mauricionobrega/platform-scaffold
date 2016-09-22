8a8e087657e5915a7aaee583d07f9b4564fd9796
Jeff Kamo <kamojeffrey@gmail.com>
2016-09-22 14:21:31 -0700

    Add tests (especially night watch tests) for SkipLinks

--- tests/system/page-objects/home.js
+++ tests/system/page-objects/home.js
@@ -1,5 +1,9 @@
 const selectors = {
-    wrapper: '.t-home'
+    wrapper: '.t-home',
+    skipLinks:    '.c-skip-links',
+    skipToMain:   '.c-skip-links__anchor:first-of-type',
+    skipToNav:    '.c-skip-links__anchor:nth-child(2n)',
+    skipToFooter: '.c-skip-links__anchor:last-of-type',
 }
 
 const Home = function(browser) {
