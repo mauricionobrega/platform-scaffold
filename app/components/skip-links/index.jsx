e28a6b737eb0bbfb91b7112be20369c3b8b4d110
Jeff Kamo <kamojeffrey@gmail.com>
2016-09-22 11:39:02 -0700

    Add skip links to the scaffold

---
+++ app/components/skip-links/index.jsx
@@ -0,0 +1,23 @@
+import React, {PropTypes} from 'react'
+
+const SkipLinks = ({fragments}) => {
+
+    return (
+        <div className="c-skip-links">
+            <a href="#app-main" className="c-skip-links__anchor">
+                Skip to content
+            </a>
+
+            <a href="#app-navigation" className="c-skip-links__anchor">
+                Skip to main navigation
+            </a>
+
+            <a href="#app-footer" className="c-skip-links__anchor">
+                Skip to footer
+            </a>
+
+        </div>
+    )
+}
+
+export default SkipLinks
