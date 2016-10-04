81048a6e553ab345e9aed12a4a3864b4f7ea4725
fractaltheory <michael@mobify.com>
2016-10-04 14:03:50 -0700

    Get tests working when SVGs are involved

--- app/components/logo/index.test.js
+++ app/components/logo/index.test.js
@@ -2,10 +2,16 @@ import {mount} from 'enzyme'
 import React from 'react'
 
 import Logo from './index'
+import {Icon} from 'progressive-web-sdk/dist/components/icon'
 
 /* eslint-disable newline-per-chained-call */
 
 test('Logo renders without errors', () => {
     const wrapper = mount(<Logo />)
+    expect(wrapper.length).toBe(1)
+})
+
+test('Icon renders without errors', () => {
+    const wrapper = mount(<Icon />)
     expect(wrapper.length).toBe(1)
 })
