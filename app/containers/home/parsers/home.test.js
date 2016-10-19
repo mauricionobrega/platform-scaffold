f3a972d1d6367469849eeb894dd65af93732f6ef
Crystal To <crystal@mobify.com>
2016-10-19 16:46:13 -0700

    Update homepage test

--- app/containers/home/parsers/home.test.js
+++ app/containers/home/parsers/home.test.js
@@ -1,9 +1,9 @@
 import {jquerifyHtmlFile} from 'progressive-web-sdk/dist/test-utils'
 import homeParser from './home'
 
-test('the title is parsed', () => {
+test('homepage is parsed', () => {
     const home = homeParser($, jquerifyHtmlFile('app/containers/home/parsers/home.test.html'))
     expect(home).toBeTruthy()
-    expect(home.title).toBeTruthy()
-    expect(home.title).toBe('Home page')
+    expect(home.categories).toBeTruthy()
+    expect(home.banners).toBeTruthy()
 })
