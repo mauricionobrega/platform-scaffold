cb5783153ff281a046719525cc8b5c64079a5e83
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-10-20 11:34:14 -0700

    Add the utility function.

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,4 +1,5 @@
 import {createAction as actionCreator} from 'redux-act'
+import Immutable from 'immutable'
 
 // simplify redux-act createAction method.
 // usage: createAction('Update Campaign', 'id', 'update')
@@ -35,4 +36,18 @@ export const formEncode = (data) => {
         pairs.push(`${encodeURIComponent(k)}=${encodeURIComponent(data[k])}`)
     })
     return pairs.join('&').replace(/%20/g, '+')
+}
+
+/**
+ * Extract all of the JSON pieces in 'text/x-magento-init' script
+ * elements, and merge them together into a single configuration object
+ */
+export const extractMagentoJson = ($html) => {
+    return $html
+        .find('script[x-type="text/x-magento-init"]')
+        .map((_, item) => item.innerHTML)
+        .get()
+        .map(JSON.parse)
+        .map((item) => Immutable.fromJS(item))
+        .reduce((summary, item) => summary.mergeDeep(item), Immutable.Map())
 }
