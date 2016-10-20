98173a6b136bb30f37c86f7a2efb4a1ef2cf63c5
Crystal To <crystal@mobify.com>
2016-10-20 10:33:44 -0700

    Update homepage test

--- app/containers/home/parsers/home.test.js
+++ app/containers/home/parsers/home.test.js
@@ -1,9 +1,31 @@
 import {jquerifyHtmlFile} from 'progressive-web-sdk/dist/test-utils'
 import homeParser from './home'
 
-test('homepage is parsed', () => {
-    const home = homeParser($, jquerifyHtmlFile('app/containers/home/parsers/home.test.html'))
-    expect(home).toBeTruthy()
-    expect(home.categories).toBeTruthy()
-    expect(home.banners).toBeTruthy()
+describe('homepage', () => {
+    let home
+
+    beforeAll(() => {
+        home = homeParser($, jquerifyHtmlFile('app/containers/home/parsers/home.test.html'))
+    })
+
+
+    it('is parsed', () => {
+        expect(home).toBeTruthy()
+        expect(home.categories).toBeTruthy()
+        expect(home.banners).toBeTruthy()
+    })
+
+    it('contains well-structured categories', () => {
+        const category = home.categories[0]
+        expect(category.href).toBeTruthy()
+        expect(category.text).toBeTruthy()
+    })
+
+    it('contains well-structured banners', () => {
+        const banner = home.banners[0]
+        expect(banner.src).toBeTruthy()
+        expect(banner.href).toBeTruthy()
+        expect(banner.alt).toBeTruthy()
+    })
+
 })
