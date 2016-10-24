c88d912b66156d1e8218378602de11820b40c052
Jeff Kamo <kamojeffrey@gmail.com>
2016-10-24 12:10:38 -0700

    Increase size of newsletter heading

--- app/containers/footer/container.js
+++ app/containers/footer/container.js
@@ -84,7 +84,7 @@ const Footer = (props) => {
             <div className="t-footer__newsletter u-padding-md u-padding-top-lg u-padding-bottom-lg">
                 {newsletter ? (
                     <div>
-                        <h2 className="u-h3 u-margin-bottom-md">
+                        <h2 className="u-h2 u-margin-bottom-md">
                             Subscribe to Merlin's Newsletter
                         </h2>
 
