80869cacee411cefdefe8681d625d45b05907b47
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-10-24 15:17:44 -0700

    Pass the data and event to the partial.

--- app/containers/pdp/container.js
+++ app/containers/pdp/container.js
@@ -5,6 +5,7 @@ import {connect} from 'react-redux'
 import PDPHeading from './partials/pdp-heading'
 import PDPCarousel from './partials/pdp-carousel'
 import PDPDescription from './partials/pdp-description'
+import * as pdpActions from './actions'
 
 const PDPAddToCart = () => false
 
@@ -18,7 +19,9 @@ class PDP extends React.Component {
     render() {
         const {
             contentsLoaded,
-            product
+            product,
+            itemQuantity,
+            setQuantity
         } = this.props
 
         if (!contentsLoaded) {
@@ -30,7 +33,7 @@ class PDP extends React.Component {
                 <PDPHeading {...product} />
                 <PDPCarousel items={product.carouselItems} />
                 <PDPDescription description={product.description} />
-                <PDPAddToCart />
+                <PDPAddToCart quantity={itemQuantity} setQuantity={setQuantity} />
             </div>
         )
     }
@@ -38,12 +41,15 @@ class PDP extends React.Component {
 
 PDP.propTypes = {
     contentsLoaded: PropTypes.bool,
-    product: PropTypes.object
+    itemQuantity: PropTypes.number,
+    product: PropTypes.object,
+    setQuantity: PropTypes.func
 }
 
 PDP.defaultProps = {
     contentsLoaded: false,
-    product: {}
+    product: {},
+    itemQuantity: 1
 }
 
 export const mapStateToProps = (state) => {
@@ -52,12 +58,13 @@ export const mapStateToProps = (state) => {
     }
 }
 
-// export const mapDispatchToProps = (dispatch, props) => {
-//     return {
-//         fetchContents: () => dispatch(pdpActions.fetchContents())
-//     }
-// }
+export const mapDispatchToProps = (dispatch, props) => {
+    return {
+        setQuantity: (quantity) => dispatch(pdpActions.setItemQuantity(quantity))
+    }
+}
 
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(PDP)
