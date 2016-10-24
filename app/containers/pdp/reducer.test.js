8797aee5983bfa329ffde56a3356dbb9fed0d725
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-10-24 14:59:09 -0700

    Test the existing reducer.

--- app/containers/pdp/reducer.test.js
+++ app/containers/pdp/reducer.test.js
@@ -1,6 +1,11 @@
-import {Map} from 'immutable'
+import {Map, is} from 'immutable'
 
 import reducer from './reducer'
+
+import * as appActions from '../app/actions'
+
+jest.mock('./pdp-parser')
+import pdpParser from './pdp-parser'
 
 test('unknown action type leaves state unchanged', () => {
     const action = {
@@ -12,4 +17,34 @@ test('unknown action type leaves state unchanged', () => {
     })
 
     expect(reducer(inputState, action)).toEqual(inputState)
+})
+
+test('appActions.onPageReceived does nothing when pageType !== PDP', () => {
+    const action = appActions.onPageReceived($, $(), 'Home')
+    const inputState = Map()
+
+    expect(reducer(inputState, action)).toEqual(inputState)
+})
+
+test('appActions.onPageReceived sets contentsLoaded to true', () => {
+    const action = appActions.onPageReceived($, $(), 'PDP')
+    const inputState = Map()
+
+    expect(reducer(inputState, action).get('contentsLoaded')).toBe(true)
+})
+
+test('appActions.onPageReceived causes the page to be parsed into the state', () => {
+    const $html = $('<body><hr /></body')
+    const action = appActions.onPageReceived($, $html, 'PDP')
+    const inputState = Map()
+
+    pdpParser.mockClear()
+    const parsedPage = {test: true, product: {title: 'Parsed Product'}}
+    pdpParser.mockReturnValueOnce(parsedPage)
+
+    const result = reducer(inputState, action)
+
+    expect(pdpParser).toBeCalledWith($, $html)
+    expect(result.get('test')).toBe(true)
+    expect(is(result.get('product'), Map(parsedPage.product))).toBe(true)
 })
