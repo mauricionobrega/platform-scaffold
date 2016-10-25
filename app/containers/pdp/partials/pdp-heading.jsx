69c2991cbe580ef9a3a83f73fda175ac7a920503
Maggie Caspar <mcaspar@mobify.com>
2016-10-25 14:20:21 -0700

    adding fonts and font styles

--- app/containers/pdp/partials/pdp-heading.jsx
+++ app/containers/pdp/partials/pdp-heading.jsx
@@ -1,9 +1,9 @@
 import React, {PropTypes} from 'react'
 
 const PDPHeading = ({title, price}) => (
-    <div className="c-pdp-heading">
-        <h1 className="c-pdp-heading__title u-text-uppercase u-text-large">{title}</h1>
-        <p className="c-pdp-heading__price u-text-accent u-text-medium">{price}</p>
+    <div className="c-pdp-heading u-margin-start-md u-margin-end-md">
+        <h1 className="c-pdp-heading__title u-text-uppercase u-margin-bottom">{title}</h1>
+        <span className="c-pdp-heading__price u-text-accent u-text-medium u-text-normal u-text-header-font">{price}</span>
     </div>
 )
 
