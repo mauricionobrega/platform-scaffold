b76031022b93ccce3765b6078d7dd9fbac0cfcfb
Maggie Caspar <mcaspar@mobify.com>
2016-10-25 11:14:49 -0700

    adding utilities and variables for pdp headers

--- app/containers/pdp/partials/pdp-heading.jsx
+++ app/containers/pdp/partials/pdp-heading.jsx
@@ -2,8 +2,8 @@ import React, {PropTypes} from 'react'
 
 const PDPHeading = ({title, price}) => (
     <div className="c-pdp-heading">
-        <h1 className="c-pdp-heading__title">{title}</h1>
-        <p className="c-pdp-heading__price">{price}</p>
+        <h1 className="c-pdp-heading__title u-text-uppercase u-text-large">{title}</h1>
+        <p className="c-pdp-heading__price u-text-accent u-text-medium">{price}</p>
     </div>
 )
 
