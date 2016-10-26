d11b2da13727690eb80f3d422efe821a31cac025
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-10-26 11:13:33 -0700

    Parse the add to cart form on the desktop page.

--- app/containers/pdp/parsers/pdp.js
+++ app/containers/pdp/parsers/pdp.js
@@ -7,6 +7,21 @@ const parseCarouselItems = (magentoObject) => {
     return carouselSetup
 }
 
+const parseAddToCartForm = ($, $form) => {
+    return {
+        submitUrl: $form.attr('action'),
+        hiddenInputs: $form.find('input[type="hidden"]')
+            .map((idx, input) => {
+                const $input = $(input)
+                return {
+                    name: $input.attr('name'),
+                    value: $input.val()
+                }
+            })
+            .get()
+    }
+}
+
 const pdpParser = ($, $html) => {
     const $mainContent = $html.find('.page-main')
 
@@ -18,6 +33,7 @@ const pdpParser = ($, $html) => {
             price: $mainContent.find('.product-info-price .price-wrapper .price').text(),
             carouselItems: parseCarouselItems(magentoObject),
             description: $mainContent.find('.product.info.detailed .product.attibute.description p').text(),
+            ...parseAddToCartForm($, $mainContent.find('#product_addtocart_form'))
         }
     }
 }
