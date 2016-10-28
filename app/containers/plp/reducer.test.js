9fae0c08aaf3c268fcb4d13053aa19b715f4f5d1
fractaltheory <michael@mobify.com>
2016-10-28 14:09:46 -0700

    Test that reducer properly stores page using href
    - needed to mock window.location.href, see inline comments

--- app/containers/plp/reducer.test.js
+++ app/containers/plp/reducer.test.js
@@ -3,11 +3,37 @@ import {onPageReceived} from '../app/actions'
 import {jquerifyHtmlFile} from 'progressive-web-sdk/dist/test-utils'
 
 describe('The PLP reducer', () => {
+    const $content = jquerifyHtmlFile('app/containers/plp/parsers/plp.test.html')
+
     test('parses the page contents onPageReceived and updates the store', () => {
-        const $content = jquerifyHtmlFile('app/containers/plp/parsers/plp.test.html')
         const newState = reducer(initialState, onPageReceived($, $content, 'PLP'))
 
         expect(newState).not.toBe(initialState)
+    })
+
+    test('stores plp state using the current window.location.href as the key name', () => {
+        const firstHref = 'http://www.foo.com'
+        const secondHref = 'http://www.foo.com/home.html'
+
+        /**
+         * We don't have the ability to change window.location.href, so we mock it
+         * as a workaround
+         * @url - https://github.com/tmpvar/jsdom#changing-the-url-of-an-existing-jsdom-window-instance
+         * @url - https://github.com/facebook/jest/issues/890#issuecomment-209698782
+         */
+        Object.defineProperty(window.location, 'href', {
+            writable: true,
+            value: firstHref
+        })
+
+        const oldState = reducer(initialState, onPageReceived($, $content, 'PLP'))
+        expect(oldState.has(firstHref)).toBeTruthy()
+
+        // Mock changing the URL
+        window.location.href = secondHref
+
+        const newState = reducer(initialState, onPageReceived($, $content, 'PLP'))
+        expect(newState.has(secondHref)).toBeTruthy()
     })
 
     test('does nothing for unknown action types', () => {
