3189e7ecc803ab4a36ecd5fd824142377a5a20f0
Maggie Caspar <mcaspar@mobify.com>
2016-11-04 15:04:15 -0700

    edit font utility classes, edit button icon

--- app/containers/pdp/partials/pdp-heading.jsx
+++ app/containers/pdp/partials/pdp-heading.jsx
@@ -5,7 +5,7 @@ const PDPHeading = ({title, price}) => (
     <div className="u-padding-md u-box-shadow u-position-relative u-z-index-1">
         {title ? <h1 className="u-text-uppercase u-margin-bottom">{title}</h1>
         : <SkeletonBlock width="50%" height="28px" className="u-margin-bottom" />}
-        {price ? <span className="u-text-accent u-text-medium u-text-normal u-text-header-font">{price}</span>
+        {price ? <span className="u-color-accent u-text-size-medium u-text-normal u-text-header-font-family">{price}</span>
         : <SkeletonBlock width="25%" height="24px" />}
     </div>
 )
