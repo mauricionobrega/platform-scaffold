a0ca8f13454a27d1a5a1c0d16a7b4c445edd775a
Karl Schmidt <karl@mobify.com>
2016-11-08 16:44:27 -0800

    Fix linting issue with test

--- app/containers/mini-cart/container.test.js
+++ app/containers/mini-cart/container.test.js
@@ -5,7 +5,7 @@ describe('Computing the subtotal', () => {
     test('Should not modify with qty of 1', () => {
         const originalPrice = '$12.99'
         const computedPriceQty = productSubtotal(originalPrice, 1)
-        expect('$' + computedPriceQty).toEqual(originalPrice)
+        expect(`$${computedPriceQty}`).toEqual(originalPrice)
     })
 
     test('Should correctly multiply by qty', () => {
