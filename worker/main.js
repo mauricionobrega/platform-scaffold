bca1a5be2d1b9e6cf8c68a266a2e16c712707219
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-11-22 15:01:55 -0800

    Add a stub manifest to be returned.

--- worker/main.js
+++ worker/main.js
@@ -6,12 +6,29 @@ import toolbox from 'sw-toolbox'
 // Configuration options
 const version = '0.1.0'
 const precacheUrls = []
+const manifest = {}
 
 // Derived constants
 const baseCacheName = `${PROJECT_SLUG}-v${version}`
 toolbox.options.cache.name = baseCacheName
 toolbox.options.cache.maxAgeSeconds = 86400
 toolbox.debug = DEBUG
+
+// Utility functions
+const jsonResponse = (data) => {
+    return new Response(
+        new Blob(
+            [JSON.stringify(data)],
+            {type: 'application/json'}
+        ),
+        {
+            status: 200,
+            statusText: 'OK'
+        }
+    )
+}
+
+
 // Lifecycle Handlers
 
 self.addEventListener('install', (e) => {
@@ -32,3 +49,7 @@ self.addEventListener('activate', (e) => {
     )
 })
 
+// Path Handlers
+toolbox.router.get('/manifest.json', () => {
+    return jsonResponse(manifest)
+})
