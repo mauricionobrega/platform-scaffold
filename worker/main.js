79de09055e34a22d6d1e8437b954b932aac8335b
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-11-22 10:28:14 -0800

    Add the very beginning of a worker.

--- worker/main.js
+++ worker/main.js
@@ -1,1 +1,14 @@
-console.log('Worker running')
+/* global PROJECT_SLUG, DEBUG */
+/* eslint-env worker, serviceworker */
+
+import toolbox from 'sw-toolbox'
+
+// Configuration options
+const version = '0.1.0'
+const precacheUrls = []
+
+// Derived constants
+const baseCacheName = `${PROJECT_SLUG}-v${version}`
+toolbox.options.cache.name = baseCacheName
+toolbox.options.cache.maxAgeSeconds = 86400
+toolbox.debug = DEBUG
