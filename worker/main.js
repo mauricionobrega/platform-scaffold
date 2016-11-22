a3512caa40ec0649e9a3e88b075559234e8d3caf
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-11-22 14:39:33 -0800

    Add the lifecycle methods including cache cleanup.

--- worker/main.js
+++ worker/main.js
@@ -12,3 +12,23 @@ const baseCacheName = `${PROJECT_SLUG}-v${version}`
 toolbox.options.cache.name = baseCacheName
 toolbox.options.cache.maxAgeSeconds = 86400
 toolbox.debug = DEBUG
+// Lifecycle Handlers
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        self.skipWaiting()
+            .then(() => console.log('[ServiceWorker] Installed version', version))
+    )
+})
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        self.clients.claim()
+            .then(() => caches.keys())
+            .then((cacheKeys) => cacheKeys.filter(
+                (key) => !key.startsWith(baseCacheName) && !key.endsWith('$$$inactive$$$')))
+            .then((keysToDelete) => keysToDelete.map((key) => caches.delete(key)))
+            .then((promises) => Promise.all(promises))
+    )
+})
+
