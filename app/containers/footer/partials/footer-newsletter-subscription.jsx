b92c73784050e7add490b4cd533165cd4d5fb835
Jeff Kamo <kamojeffrey@gmail.com>
2016-11-24 17:34:55 -0800

    Remove unneeded component

--- app/containers/footer/partials/footer-newsletter-subscription.jsx
+++ app/containers/footer/partials/footer-newsletter-subscription.jsx
@@ -1,11 +1,8 @@
 import React, {PropTypes} from 'react'
 
 import NewsletterForm from './newsletter-form'
-import SkeletonText from 'progressive-web-sdk/dist/components/skeleton-text'
 
 const FooterNewsletterSubscription = ({newsletter, onSubmit}) => {
-    const skeleton = <SkeletonText lines={8} width="100%" style={{lineHeight: '2em'}} />
-
     return (
         <div className="t-footer__newsletter u-padding-md u-padding-top-lg u-padding-bottom-lg">
             <div>
