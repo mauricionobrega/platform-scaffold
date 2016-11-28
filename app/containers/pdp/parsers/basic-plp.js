0c4fdf414de2e3065456ed9978bbbb4696e7ceaa
Crystal To <crystal@mobify.com>
2016-11-28 13:53:44 -0800

    Add image to shared PLP-PDP state

--- app/containers/pdp/parsers/basic-plp.js
+++ app/containers/pdp/parsers/basic-plp.js
@@ -1,5 +1,5 @@
 import Immutable from 'immutable'
-import {parseTextLink} from '../../../utils/parser-utils'
+import {parseTextLink, parseImage} from '../../../utils/parser-utils'
 import {initialState} from '../reducer'
 
 export const basicPlpParser = ($, $html) => {
@@ -8,10 +8,17 @@ export const basicPlpParser = ($, $html) => {
     $products.each((_, product) => {
         const $product = $(product)
         const {href, text} = parseTextLink($product.find('.product-item-link'))
+        const img = parseImage($product.find('.product-image-photo'))
         productMap[href] = initialState.mergeDeep({
             product: Immutable.Map({
                 title: text.trim(),
-                price: $product.find('.price').text()
+                price: $product.find('.price').text(),
+                carouselItems: [
+                    {
+                        img: img.src,
+                        position: "0"
+                    }
+                ]
             }),
             contentsLoaded: false,
             isPlaceholder: false
