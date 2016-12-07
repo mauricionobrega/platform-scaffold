0b4d142555b9bd10e08b9b677f6b900698b2b8fb
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2016-12-07 15:45:35 -0800

    Actually avoid loading on production.

--- service-worker-loader.js
+++ service-worker-loader.js
@@ -1,5 +1,6 @@
+// THIS IS A STRING VALUE NOT A BOOLEAN
 const isPreview = /preview=(true|false)/.exec(self.location.search)[1]
 
-if (isPreview) {
+if (isPreview === 'true') {
     self.importScripts('https://localhost:8443/worker.js')
 }
