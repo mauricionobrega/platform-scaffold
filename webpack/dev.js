485751379e78501ef480a81dc6adce3e19766a84
Mike Nikles <mnikles@mobify.com>
2016-12-11 12:26:46 -0800

    Upgrade all webpack config files to v2

--- webpack/dev.js
+++ webpack/dev.js
@@ -8,9 +8,10 @@ const mainConfig = require('./base.main')
 const workerConfig = require('./base.worker')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-mainConfig.module.loaders = mainConfig.module.loaders.concat({
+// TODO: Why is this here instead of base.main.js?
+mainConfig.module.rules = mainConfig.module.rules.concat({
     test: /\.scss$/,
-    loader: ExtractTextPlugin.extract(['css?-autoprefixer&-url', 'postcss', 'sass']),
+    loader: ExtractTextPlugin.extract(['css-loader?-autoprefixer&-url', 'postcss-loader', 'sass-loader']),
     include: [
         /node_modules\/progressive-web-sdk/,
         /app/
@@ -20,7 +21,6 @@ mainConfig.module.loaders = mainConfig.module.loaders.concat({
 mainConfig.output.publicPath = `https://${ip.address()}:8443/`
 
 mainConfig.plugins = mainConfig.plugins.concat([
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin()
 ])
 
