4cae051c22a6ee1d5d20e750e6c9bc49143c0964
Karl Schmidt <karl@mobify.com>
2016-12-13 15:38:05 -0800

    Header matching spec (except for font weights and lock icon)

--- app/containers/checkout-header/container.js
+++ app/containers/checkout-header/container.js
@@ -1,12 +1,8 @@
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
-import classnames from 'classnames'
 import Immutable from 'immutable'
 
-import Button from 'progressive-web-sdk/dist/components/button'
-import IconLabel from 'progressive-web-sdk/dist/components/icon-label'
-import {HeaderBar, HeaderBarActions, HeaderBarTitle} from 'progressive-web-sdk/dist/components/header-bar'
-import Link from 'progressive-web-sdk/dist/components/link'
+import {HeaderBar, HeaderBarTitle} from 'progressive-web-sdk/dist/components/header-bar'
 
 class CheckoutHeader extends React.Component {
     shouldComponentUpdate(nextProps) {
@@ -16,32 +12,14 @@ class CheckoutHeader extends React.Component {
     render() {
         const {header} = this.props
 
-        const innerButtonClassName = classnames('t-checkout-header__inner-button', 'u-padding-0', {
-            't--hide-label': false
-        })
-
-        const linkClassName = classnames('t-checkout-header__link', {
-            't--fade-sparkles': false
-        })
-
         return (
             <header className="t-checkout-header">
                 <HeaderBar className="t-checkout-header__bar">
-                    <div className="t-checkout-header__placeholder" />
-
-                    <div className="u-flex">
-                        <HeaderBarTitle>
-                            <Link href="/" className={linkClassName}>
-                                <h1 className="u-visually-hidden">Merlin's Potions</h1>
-                            </Link>
-                        </HeaderBarTitle>
-                    </div>
-
-                    <HeaderBarActions>
-                        <Button innerClassName={innerButtonClassName}>
-                            <IconLabel label="Stores" iconName="map" iconSize="medium" />
-                        </Button>
-                    </HeaderBarActions>
+                    <HeaderBarTitle className="u-flex u-text-align-start">
+                        <h2 className="t-checkout-header__title u-heading-family u-text-uppercase">
+                            <span className="u-text-lighter">MERLIN'S</span> CHECKOUT
+                        </h2>
+                    </HeaderBarTitle>
                 </HeaderBar>
             </header>
         )
