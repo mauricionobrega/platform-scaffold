62a58957978fb805a685c3c8baa37f3765f4f210
Karl Schmidt <karl@mobify.com>
2016-12-14 14:17:16 -0800

    Address PR feedback

--- app/containers/checkout-header/container.js
+++ app/containers/checkout-header/container.js
@@ -6,11 +6,12 @@ const CheckoutHeader = function(props) {
     return (
         <header className="t-checkout-header">
             <HeaderBar className="t-checkout-header__bar">
-                <HeaderBarTitle className="u-flex u-text-align-start">
+                <HeaderBarTitle className="u-flex u-padding-start u-text-align-start">
                     <h2 className="t-checkout-header__title u-heading-family u-text-uppercase">
                         <span className="u-text-lighter">MERLIN'S</span> CHECKOUT
                     </h2>
                 </HeaderBarTitle>
+                <div className="t-checkout-header__icon-lock" />
             </HeaderBar>
         </header>
     )
