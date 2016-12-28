af58e8a668886f2e601cbda7bdf1e026854aa1b8
Jeremy Wiebe <jeremy@mobify.com>
2016-12-28 13:40:29 -0800

    Initial import of iOS shell and assets

---
+++ native/app/app.js
@@ -0,0 +1,25 @@
+// Astro
+import Application from 'astro/application'
+import AnchoredLayoutPlugin from 'astro/plugins/anchoredLayoutPlugin'
+import TabBarPlugin from 'astro/plugins/tabBarPlugin'
+
+import TabBarConfig from './config/tabBarConfig'
+
+window.run = async function() {
+    // Build all the main plugins we need for the layout
+    const [
+        tabBar,
+        mainLayout
+    ] = await Promise.all(
+        TabBarPlugin.init(),
+        AnchoredLayoutPlugin.init()
+    )
+
+    await tabBar.setItems(TabBarConfig.items)
+    await mainLayout.addBottomView(tabBar)
+    await Application.setMainViewPlugin(mainLayout)
+    Application.dismissLaunchImage()
+}
+
+// Comment out next line for JS debugging
+window.run()
---
+++ native/app/config/tabBarConfig.js
@@ -0,0 +1,30 @@
+const tabConfig = {
+    items: [
+        {
+            id: 'shop',
+            title: 'Shop',
+            imageUrl: '',
+            selectedImageUrl: ''
+        },
+        {
+            id: 'stores',
+            title: 'Stores',
+            imageUrl: '',
+            selectedImageUrl: ''
+        },
+        {
+            id: 'account',
+            title: 'Account',
+            imageUrl: '',
+            selectedImageUrl: ''
+        },
+        {
+            id: 'more',
+            title: 'More',
+            imageUrl: '',
+            selectedImageUrl: ''
+        }
+    ]
+}
+
+export default tabConfig
