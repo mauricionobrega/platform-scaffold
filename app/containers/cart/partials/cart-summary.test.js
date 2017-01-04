f0c5e2aec86db14c6fa4a91f817d6aa23bad2ed4
Karl Schmidt <karl@mobify.com>
2017-01-04 12:57:18 -0800

    Fix checkout shipping form tests Remove undefined thing from cart summary tests

--- app/containers/cart/partials/cart-summary.test.js
+++ app/containers/cart/partials/cart-summary.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import CartSummary from './cart-summary'
 import {Provider} from 'react-redux'
-import {mount, shallow, mockStore} from 'enzyme'
+import {mount, shallow} from 'enzyme'
 
 test('renders without errors', () => {
     const store = {
@@ -21,7 +21,7 @@ test('renders without errors', () => {
 const ROOT_CLASS = 't-cart__summary'
 
 test('renders the component class correctly', () => {
-    const wrapper = shallow(<CartSummary cart={{summary_count: '0', subtotal_excl_tax: '0', subtotal_incl_tax: '0'}} store={mockStore} />)
+    const wrapper = shallow(<CartSummary cart={{summary_count: '0', subtotal_excl_tax: '0', subtotal_incl_tax: '0'}} />)
 
     expect(wrapper.hasClass(ROOT_CLASS)).toBe(true)
 })
--- app/containers/checkout-shipping/partials/checkout-shipping-form.test.js
+++ app/containers/checkout-shipping/partials/checkout-shipping-form.test.js
@@ -1,23 +1,19 @@
 import React from 'react'
-import CartProductList from './cart-product-list'
-import {mount, shallow} from 'enzyme'
+import CheckoutShippingReduxForm from './checkout-shipping-form'
+import {Provider} from 'react-redux'
+import {mount} from 'enzyme'
 
 test('renders without errors', () => {
-    const wrapper = mount(<CartProductList cart={{items: []}} />)
+    const store = {
+        subscribe: () => {},
+        dispatch: () => {},
+        getState: () => ({})
+    }
+
+    const wrapper = mount(
+        <Provider store={store}>
+            <CheckoutShippingReduxForm />
+        </Provider>)
 
     expect(wrapper.length).toBe(1)
-})
-
-test('renders without errors with one item', () => {
-    const wrapper = mount(<CartProductList cart={{items: [{product_name: 'TestName', product_image: {alt: 'TestAlt', src: 'TestSrc'}}], summary_count: 1}} />)
-
-    expect(wrapper.length).toBe(1)
-})
-
-const ROOT_CLASS = 't-cart__product-list'
-
-test('renders the component class correctly', () => {
-    const wrapper = shallow(<CartProductList cart={{items: []}} />)
-
-    expect(wrapper.hasClass(ROOT_CLASS)).toBe(true)
 })
