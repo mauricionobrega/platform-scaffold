203a433a16f8589bcbaf8490d95b8de86f5a527a
sshin-mobify <sshin@mobify.com>
2017-01-09 11:45:22 -0800

    Revert "Created hook for cart press"
    This reverts commit 183b759ce3ddb9e4b61bf141c68e4e867a603db1.

--- native/app/controllers/tabController.js
+++ native/app/controllers/tabController.js
@@ -43,10 +43,6 @@ TabController.init = async function(tabItem) {
         navigationView.back()
     })
 
-    headerBar.on('click:cart', () => {
-        console.log('Cart has been clicked')
-    })
-
     return new TabController(tabItem, layout, headerBar, navigationView)
 }
 
