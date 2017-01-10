d0ee73548296a93ae734bb0cd4b8eb2886a265cd
sshin-mobify <sshin@mobify.com>
2017-01-10 15:17:40 -0800

    Got rid of titles for icons

--- native/app/config/cartConfig.js
+++ native/app/config/cartConfig.js
@@ -1,20 +1,10 @@
 import BaseConfig from './baseConfig'
 
-const url = 'https://merlinspotions.com'            // must get url
-
-const closeTitle = {
-    id: 'closeTitle_id',
-    title: 'close'
-}
+const url = 'https://www.merlinspotions.com/checkout/cart/'         // must get url
 
 const closeIcon = {
     id: 'closeCart_id',
     imageUrl: 'file:///close.png'            // need to change this to correct url
-}
-
-const cartTitle = {                         // this title must be under the icon
-    id: 'cartTitle_id',
-    title: 'cart'
 }
 
 const cartIcon = {
@@ -27,4 +17,4 @@ const colors = {
     backgroundColor: BaseConfig.colors.primaryColor
 }
 
-export default {url, closeTitle, closeIcon, cartTitle, cartIcon, colors}
+export default {url, closeIcon, cartIcon, colors}
