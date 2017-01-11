7d70a99cc14c4b6250fadc723379864ad0d2e28a
sshin-mobify <sshin@mobify.com>
2017-01-11 11:06:01 -0800

    Implemented navigationView in cartModalController

--- native/app/controllers/cartModalController.js
+++ native/app/controllers/cartModalController.js
@@ -2,33 +2,38 @@ import Promise from 'bluebird'
 
 import AnchoredLayoutPlugin from 'astro/plugins/anchoredLayoutPlugin'
 import ModalViewPlugin from 'astro/plugins/modalViewPlugin'
-import CartController from './cartController'
-import CartHeaderController from './cartHeaderController'
+import NavigationPlugin from 'astro/plugins/navigationPlugin'
 
-const CartModalController = function(modalView, cartController) {
+// import CartController from './cartController'
+import CartHeaderController from './cartHeaderController'
+import CartConfig from '../config/cartConfig'
+
+const CartModalController = function(modalView, navigationView) {
     this.isShowing = false
     this.viewPlugin = modalView
-    this.cartController = cartController
+    this.navigationView = navigationView
 }
 
 CartModalController.init = async function() {
     const [
         modalView,
         anchoredLayout,
-        cartController,
+        navigationView,
         cartHeaderController,
     ] = await Promise.all([
         ModalViewPlugin.init(),
         AnchoredLayoutPlugin.init(),
-        CartController.init(),
+        NavigationPlugin.init(),
         CartHeaderController.init(),
     ])
 
     await anchoredLayout.addTopView(cartHeaderController.viewPlugin)
-    await anchoredLayout.setContentView(cartController.viewPlugin)
+    await anchoredLayout.setContentView(navigationView)
+    await navigationView.setHeaderBar(cartHeaderController.viewPlugin)
+
     await modalView.setContentView(anchoredLayout)                // load the view in to the modal
 
-    const cartModalController = new CartModalController(modalView, cartController)
+    const cartModalController = new CartModalController(modalView, navigationView)
 
     cartHeaderController.registerCloseEvents(() => {
         cartModalController.hide()
@@ -42,13 +47,13 @@ CartModalController.prototype.show = function() {
         return
     }
     this.isShowing = true
-    this.cartController.reload()
+    this.navigationView.navigate(CartConfig.url)
     this.viewPlugin.show({animated: true})
 }
 
 CartModalController.prototype.hide = function() {
     this.isShowing = false
-    this.cartController.navigate('about:blank')
+    this.navigationView.popToRoot()
     this.viewPlugin.hide({animated: true})
 }
 
@@ -57,7 +62,7 @@ CartModalController.prototype.isActiveItem = function() {
 }
 
 CartModalController.prototype.back = function() {
-    this.cartController.back()
+    this.navigationView.back()
 }
 
 export default CartModalController
