a60350e79823bdf7dfe4e7c07fc2402ff6f5588c
Karl Schmidt <karl@mobify.com>
2017-01-10 16:51:01 -0800

    Route the charms page as a PLP

--- app/app-provider.jsx
+++ app/app-provider.jsx
@@ -78,6 +78,7 @@ const AppProvider = ({store}) => {
                     <Route component={PLP} path="ingredients.html" routeName="productListPage" />
                     <Route component={PLP} path="supplies.html" routeName="productListPage" />
                     <Route component={PLP} path="new-arrivals.html" routeName="productListPage" />
+                    <Route component={PLP} path="charms.html" routeName="productListPage" />
                     <Route component={PDP} path="*.html" routeName="productDetailsPage" />
                     <Route component={CheckoutShipping} path="checkout/shipping/" routeName="checkingShipping" fetchPage="false" Header={CheckoutHeader} Footer={CheckoutFooter} />
                     <Route component={CheckoutPayment} path="checkout/payment/" routeName="checkingPayment" fetchPage="false" Header={CheckoutHeader} Footer={CheckoutFooter} />
