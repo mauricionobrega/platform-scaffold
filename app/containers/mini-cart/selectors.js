a569de9c68a77b6f7549e38b649b6b4496cc839f
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-01-16 13:39:23 -0800

    Split up MiniCart

--- app/containers/mini-cart/selectors.js
+++ app/containers/mini-cart/selectors.js
@@ -1,7 +1,12 @@
 import {createSelector} from 'reselect'
+import {createGetSelector} from '../../utils/selector-utils'
 import * as globalSelectors from '../../store/selectors'
 
 export const getMiniCart = createSelector(
     globalSelectors.getUi,
     ({miniCart}) => miniCart
 )
+
+export const getCartObject = createGetSelector(getMiniCart, 'cart')
+export const getMiniCartContentsLoaded = createGetSelector(getMiniCart, 'contentsLoaded')
+export const getMiniCartIsOpen = createGetSelector(getMiniCart, 'isOpen')
