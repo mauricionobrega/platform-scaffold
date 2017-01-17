377fd23c0243b6ce786fb10a5798ef530cfd0441
Kerr Marin Miller <kmarinmiller@mobify.com>
2017-01-17 14:52:29 -0800

    Install astro before building

--- native/scripts/build-dependencies.sh
+++ native/scripts/build-dependencies.sh
@@ -4,6 +4,7 @@ npm i
 
 # Remove this once we release Astro version 0.19 that will include the `dist` folder in the release 
 pushd node_modules/mobify-progressive-app-sdk
+    npm i
     npm run build:astro_client
 popd
 
