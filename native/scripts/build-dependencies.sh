54c6889fb45e49937680d3d91c097f9b2f54c926
Kerr Marin Miller <kmarinmiller@mobify.com>
2017-01-17 14:19:17 -0800

    Add build step for astro client

--- native/scripts/build-dependencies.sh
+++ native/scripts/build-dependencies.sh
@@ -2,7 +2,14 @@
 
 npm i
 
+# Remove this once we release Astro version 0.19 that will include the `dist` folder in the release 
+pushd node_modules/mobify-progressive-app-sdk
+    npm run build:astro_client
+popd
+
 pushd onboarding
     npm i
     npm run build
 popd
+
+
