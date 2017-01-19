29dbccb9899df243d3c26ac7c1ccfe45f9b382f5
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-01-19 13:58:55 -0800

    Split up the MiniCart template.

--- app/containers/mini-cart/container.js
+++ app/containers/mini-cart/container.js
@@ -25,70 +25,107 @@ export const productSubtotal = (price, quantity) => {
     return (priceNumber * quantity).toFixed(2)
 }
 
+const MiniCartProductList = ({cart}) => {
+    const subtotalClasses = classNames(
+        't-mini-cart__subtotal',
+
+        'u-flexbox',
+        'u-justify-between',
+        'u-margin-bottom-lg',
+        'u-padding-top-lg',
+
+        'u-h4',
+        'u-heading-family'
+    )
+
+    return (
+        <div className="u-padding-md">
+            <Button href="#" className="c--tertiary u-width-full u-margin-bottom u-text-capitalize">
+                View and edit cart
+            </Button>
+
+            <List>
+                {cart.items.map((product, idx) =>
+                    <ProductItem
+                        className="u-padding-top-lg u-padding-bottom-lg u-padding-start u-padding-end"
+                        title={<h2 className="u-h3">{product.product_name}</h2>}
+                        price={product.product_price}
+                        key={idx}
+                        image={<Image src={product.product_image.src} alt={product.product_image.alt} width="64px" height="64px" />}
+                        >
+                        <div>
+                            <p className="u-margin-bottom-sm">Qty: {product.qty}</p>
+                            <p>Sub-Total: ${productSubtotal(product.product_price, product.qty)}</p>
+                        </div>
+                    </ProductItem>
+                )}
+            </List>
+
+            <div className={subtotalClasses}>
+                <div className="u-flex u-text-uppercase">Subtotal:</div>
+                <div className="u-flex-none">{cart.subtotal}</div>
+            </div>
+        </div>
+    )
+}
+
+MiniCartProductList.propTypes = {
+    cart: PropTypes.object
+}
+
+const MiniCartEmpty = () => (
+    <div className="t-mini-cart__empty-content u-flexbox u-flex u-column">
+        <Image
+            className="u-margin-bottom-md"
+            height="140px"
+            width="140px"
+            alt="Illustrated upside-down top hat with a bug flying out"
+            src={getAssetUrl(`static/img/cart/empty-cart@2x.png`)} />
+
+        <p className="t-mini-cart__empty-message u-text-align-center">
+            You have no items in your shopping cart.
+        </p>
+    </div>
+)
+
+const MiniCartHeader = ({closeMiniCart}) => (
+    <HeaderBar>
+        <HeaderBarTitle className="u-flex u-padding-start u-text-align-start">
+            <h2 className="t-mini-cart__title u-heading-family u-text-uppercase">
+                <span className="u-text-lighter">Shopping</span> Cart
+            </h2>
+        </HeaderBarTitle>
+
+        <HeaderBarActions>
+            <IconLabelButton iconName="close" label="close" onClick={closeMiniCart}>Close</IconLabelButton>
+        </HeaderBarActions>
+    </HeaderBar>
+)
+
+MiniCartHeader.propTypes = {
+    closeMiniCart: PropTypes.func
+}
+
+const MiniCartMain = ({hasItems, cart}) => (
+    <div className="t-mini-cart__content u-flexbox u-column u-padding-md">
+        {hasItems ? <MiniCartProductList cart={cart} /> : <MiniCartEmpty />}
+
+        <div className="u-padding-top-lg u-flex-none">
+            <Button href="#" className="c--primary u-width-full u-text-uppercase">
+                {hasItems ? 'Go To Checkout' : 'Continue Shopping'}
+            </Button>
+        </div>
+    </div>
+)
+
+MiniCartMain.propTypes = {
+    cart: PropTypes.object,
+    hasItems: PropTypes.bool
+}
+
 class MiniCart extends React.Component {
     componentDidMount() {
         this.props.fetchContents()
-    }
-
-    renderList(cart) {
-        const subtotalClasses = classNames(
-            't-mini-cart__subtotal',
-
-            'u-flexbox',
-            'u-justify-between',
-            'u-margin-bottom-lg',
-            'u-padding-top-lg',
-
-            'u-h4',
-            'u-heading-family'
-        )
-
-        return (
-            <div className="u-padding-md">
-                <Button href="#" className="c--tertiary u-width-full u-margin-bottom u-text-capitalize">
-                    View and edit cart
-                </Button>
-
-                <List>
-                    {cart.items.map((product, idx) =>
-                        <ProductItem
-                            className="u-padding-top-lg u-padding-bottom-lg u-padding-start u-padding-end"
-                            title={<h2 className="u-h3">{product.product_name}</h2>}
-                            price={product.product_price}
-                            key={idx}
-                            image={<Image src={product.product_image.src} alt={product.product_image.alt} width="64px" height="64px" />}
-                        >
-                            <div>
-                                <p className="u-margin-bottom-sm">Qty: {product.qty}</p>
-                                <p>Sub-Total: ${productSubtotal(product.product_price, product.qty)}</p>
-                            </div>
-                        </ProductItem>
-                    )}
-                </List>
-
-                <div className={subtotalClasses}>
-                    <div className="u-flex u-text-uppercase">Subtotal:</div>
-                    <div className="u-flex-none">{cart.subtotal}</div>
-                </div>
-            </div>
-        )
-    }
-
-    renderEmpty() {
-        return (
-            <div className="t-mini-cart__empty-content u-flexbox u-flex u-column">
-                <Image
-                    className="u-margin-bottom-md"
-                    height="140px"
-                    width="140px"
-                    alt="Illustrated upside-down top hat with a bug flying out"
-                    src={getAssetUrl(`static/img/cart/empty-cart@2x.png`)} />
-
-                <p className="t-mini-cart__empty-message u-text-align-center">
-                    You have no items in your shopping cart.
-                </p>
-            </div>
-        )
     }
 
     render() {
@@ -97,31 +134,9 @@ class MiniCart extends React.Component {
 
         return (
             <Sheet className="t-mini-cart" open={isOpen} onDismiss={closeMiniCart} maskOpacity={0.7} effect="slide-right">
-                <HeaderBar>
-                    <HeaderBarTitle className="u-flex u-padding-start u-text-align-start">
-                        <h2 className="t-mini-cart__title u-heading-family u-text-uppercase">
-                            <span className="u-text-lighter">Shopping</span> Cart
-                        </h2>
-                    </HeaderBarTitle>
+                <MiniCartHeader closeMiniCart={closeMiniCart} />
 
-                    <HeaderBarActions>
-                        <IconLabelButton iconName="close" label="close" onClick={closeMiniCart}>Close</IconLabelButton>
-                    </HeaderBarActions>
-                </HeaderBar>
-
-                {contentsLoaded ?
-                    (
-                        <div className="t-mini-cart__content u-flexbox u-column u-padding-md">
-                            {hasItems ? this.renderList(cart) : this.renderEmpty()}
-
-                            <div className="u-padding-top-lg u-flex-none">
-                                <Button href="#" className="c--primary u-width-full u-text-uppercase">
-                                    {hasItems ? 'Go To Checkout' : 'Continue Shopping'}
-                                </Button>
-                            </div>
-                        </div>
-                    )
-                : false}
+                {contentsLoaded && <MiniCartMain hasItems={hasItems} cart={cart} />}
             </Sheet>
         )
     }
