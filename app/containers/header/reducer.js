793139b3c0c0be185c525e05b1602d1cb63e27eb
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-01-24 12:53:22 -0800

    Use the common cart info (still in MiniCart for now) in Header

--- app/containers/header/reducer.js
+++ app/containers/header/reducer.js
@@ -1,20 +1,14 @@
 import Immutable from 'immutable'
 import {handleActions} from 'redux-actions'
 import * as headerActions from './actions'
-import * as cartActions from '../cart/actions'
 import {mergePayload} from '../../utils/reducer-utils'
 
 export const initialState = Immutable.fromJS({
-    isCollapsed: false,
-    itemCount: 0
+    isCollapsed: false
 })
 
 const header = handleActions({
     [headerActions.toggleHeader]: mergePayload,
-
-    [cartActions.receiveCartContents]: (state, {payload}) => {
-        return state.set('itemCount', payload.cart.summary_count)
-    },
 }, initialState)
 
 
