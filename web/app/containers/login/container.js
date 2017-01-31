879b85cc796d2931a5f9647cd05102359349b0e2
Steven Shin <sshin@mobify.com>
2017-01-31 10:11:37 -0800

    remove default header

--- web/app/containers/login/container.js
+++ web/app/containers/login/container.js
@@ -9,6 +9,8 @@ import SkeletonBlock from 'progressive-web-sdk/dist/components/skeleton-block'
 import SkeletonText from 'progressive-web-sdk/dist/components/skeleton-text'
 import {Tabs, TabsPanel} from 'progressive-web-sdk/dist/components/tabs'
 
+import {isRunningInAstro} from '../../utils/astro-integration'
+
 import * as actions from './actions'
 
 class Login extends React.Component {
@@ -54,6 +56,7 @@ class Login extends React.Component {
 
         return (
             <div className="t-login">
+                {!isRunningInAstro &&
                 <div className="u-bg-color-neutral-20 u-padding-md u-padding-top-lg u-padding-bottom-lg u-box-shadow-inset">
                     {title ?
                         <h1 className="u-text-uppercase u-text-normal">
@@ -65,7 +68,8 @@ class Login extends React.Component {
                         </div>
                     }
                 </div>
-                
+                }   
+
                 <Tabs activeIndex={this.indexForSection(routeName)} className="t-login__navigation" onChange={(index) => navigateToSection(router, routes, this.sectionForIndex(index))}>
                     <TabsPanel title={Login.SECTION_NAMES[Login.SIGN_IN_SECTION]}>
                         <LoginSection signinSection={signinSection} submitSignInForm={submitSignInForm} openSignInModal={openSignInModal} closeSignInModal={closeSignInModal}/>
@@ -127,6 +131,7 @@ function RegisterSection(props) {
 }
 
 function LoginSection(props) {
+    const item =
     <div>
         <div className="u-padding-start-md u-padding-end-md u-padding-top-lg u-padding-bottom-lg u-box-shadow">
             <div className="u-margin-bottom">
@@ -164,6 +169,7 @@ function LoginSection(props) {
             />
         </div>
     </div>
+    return item
 }
 
 const mapDispatchToProps = {
