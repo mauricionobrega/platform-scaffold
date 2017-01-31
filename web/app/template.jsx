fab57aae5cf9313a765858ec5e5ab31c5a27bf4c
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-01-31 14:19:42 -0800

    Add a Template higher-order component for handling route changes.

---
+++ web/app/template.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {onRouteChanged} from './containers/app/actions'
+
+const getDisplayName = (WrappedComponent) => {
+    return WrappedComponent.displayName || WrappedComponent.name || 'Component'
+}
+
+const getPath = ({pathname, search}) => pathname + search
+
+/**
+ * Given the current router state, get the corresponding URL on the
+ * desktop site. Ignores #fragments in the router state.
+ */
+const getURL = (routerLocation) => {
+    return [
+        window.location.protocol,
+        '//',
+        window.location.host,
+        getPath(routerLocation)
+    ].join('')
+}
+
+
+const template = (WrappedComponent) => {
+    class Template extends React.Component {
+        constructor(props) {
+            super(props)
+
+            this.WrappedComponent = WrappedComponent
+        }
+
+        dispatchRouteChange(targetProps) {
+            const {dispatch, location} = targetProps
+            dispatch(onRouteChanged(getURL(location), Template))
+        }
+
+        componentWillMount() {
+            console.log('mounting', Template.displayName)
+
+            this.dispatchRouteChange(this.props)
+        }
+
+        componentWillUnmount() {
+            console.log('unmounting', Template.displayName)
+        }
+
+        componentWillReceiveProps(nextProps) {
+            if (getPath(this.props.location) !== getPath(nextProps.location)) {
+                console.log('changing', Template.displayName)
+                this.dispatchRouteChange(nextProps)
+            }
+        }
+
+        render() {
+            return (<WrappedComponent {...this.props} />)
+        }
+    }
+    Template.displayName = `Template(${getDisplayName(WrappedComponent)})`
+
+    return connect()(Template)
+}
+
+export default template
