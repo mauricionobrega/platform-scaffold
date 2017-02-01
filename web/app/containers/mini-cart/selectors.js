a2cc3a81a2a2f98ce012149a4bb34a3996ea642b
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-02-01 14:13:09 -0800

    Clean up selectors a little.

--- web/app/containers/mini-cart/selectors.js
+++ web/app/containers/mini-cart/selectors.js
@@ -6,15 +6,18 @@ export const getMiniCart = createSelector(
     globalSelectors.getUi,
     ({miniCart}) => miniCart
 )
-export const getMiniCartContentsLoaded = createGetSelector(getMiniCart, 'contentsLoaded')
 
-export const getCartObject = globalSelectors.getCart
-export const getMiniCartItems = createGetSelector(getCartObject, 'items')
-export const getMiniCartSubtotal = createGetSelector(getCartObject, 'subtotal')
+export const getMiniCartContentsLoaded = createSelector(
+    globalSelectors.getCart,
+    (cart) => cart.has('items')
+)
+
+export const getMiniCartItems = createGetSelector(globalSelectors.getCart, 'items')
+export const getMiniCartSubtotal = createGetSelector(globalSelectors.getCart, 'subtotal')
 export const getMiniCartHasItems = createSelector(
     getMiniCartItems,
-    (items) => items.size > 0
+    (items) => items && items.size > 0
 )
-export const getMiniCartSummaryCount = createGetSelector(getCartObject, 'summary_count')
-export const getSubtotalExcludingTax = createGetSelector(getCartObject, 'subtotal_excl_tax')
-export const getSubtotalIncludingTax = createGetSelector(getCartObject, 'subtotal_incl_tax')
+export const getMiniCartSummaryCount = createGetSelector(globalSelectors.getCart, 'summary_count')
+export const getSubtotalExcludingTax = createGetSelector(globalSelectors.getCart, 'subtotal_excl_tax')
+export const getSubtotalIncludingTax = createGetSelector(globalSelectors.getCart, 'subtotal_incl_tax')
--- web/app/store/cart/reducer.js
+++ web/app/store/cart/reducer.js
@@ -3,9 +3,7 @@ import {handleActions} from 'redux-actions'
 import {receiveCartContents} from './actions'
 import {mergePayloadForActions} from '../../utils/reducer-utils'
 
-const initialState = Immutable.fromJS({
-    items: []
-})
+const initialState = Immutable.fromJS({})
 
 const cartReducer = handleActions({
     ...mergePayloadForActions(receiveCartContents)
