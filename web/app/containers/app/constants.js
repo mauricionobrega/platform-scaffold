06fabdecfff0969c2d3ce4311c3e27e5224365c8
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-01-31 16:36:24 -0800

    Remove the router-utils.

--- web/app/containers/app/constants.js
+++ web/app/containers/app/constants.js
@@ -2,5 +2,4 @@
  * This file contains constants for use in the application.
  */
 export const CURRENT_URL = 'currentURL'
-export const SELECTOR = 'selector'
 export const PLACEHOLDER = 'placeholder'
