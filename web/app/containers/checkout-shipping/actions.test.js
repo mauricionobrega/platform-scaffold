71a92d0b22bb4220e1b8d656d91607bf5b136b51
Jennifer Bandelin <jennifer@mobify.com>
2017-02-07 16:13:32 -0800

    Fix checkout shipping action tests

--- web/app/containers/checkout-shipping/actions.test.js
+++ web/app/containers/checkout-shipping/actions.test.js
@@ -11,26 +11,13 @@ afterAll(() => {
     global.fetch = realFetch
 })
 
-jest.mock('progressive-web-sdk/dist/jquery-response')
-import {jqueryResponse} from 'progressive-web-sdk/dist/jquery-response'
 jest.mock('./checkout-shipping-parser')
 import checkoutShippingParser from './checkout-shipping-parser'
 
 
 test('process parses the response and dispatches receiveData', () => {
-    jqueryResponse.mockClear()
-    jqueryResponse.mockReturnValue(Promise.resolve(['$', '$response']))
 
-    const thunk = process('page contents!')
-    expect(typeof thunk).toBe('function')
-
-    const mockDispatch = jest.fn()
-
-    return thunk(mockDispatch)
-        .then(() => {
-            expect(jqueryResponse).toBeCalledWith('page contents!')
-
-            expect(mockDispatch).toBeCalled()
-            expect(checkoutShippingParser).toBeCalledWith('$', '$response')
-        })
+    const thunk = process({payload: {$: '$', $response: '$response'}})
+    expect(typeof thunk).toBe('object')
+    expect(checkoutShippingParser).toBeCalledWith('$', '$response')
 })
