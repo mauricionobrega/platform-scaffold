18a17e4fd6e90036e5312f7e725004fcbbaf4041
Mike Lee <mlee@mobify.com>
2017-02-07 17:45:32 -0800

    fix build

--- web/app/containers/checkout-payment/reducer.test.js
+++ web/app/containers/checkout-payment/reducer.test.js
@@ -2,7 +2,6 @@
 import {Map} from 'immutable'
 
 import reducer from './reducer'
-import {receiveContents} from './actions'
 
 test('unknown action type leaves state unchanged', () => {
     const action = {
@@ -14,20 +13,4 @@ test('unknown action type leaves state unchanged', () => {
     })
 
     expect(reducer(inputState, action)).toEqual(inputState)
-})
-
-test('checkoutPaymentActions.receiveContents sets contentsLoaded flag', () => {
-    const action = receiveContents({})
-
-    const initialState = Map({
-        contentsLoaded: false,
-        bystander: 'data'
-    })
-
-    const finalState = Map({
-        contentsLoaded: true,
-        bystander: 'data'
-    })
-
-    expect(reducer(initialState, action).equals(finalState)).toBe(true)
 })
