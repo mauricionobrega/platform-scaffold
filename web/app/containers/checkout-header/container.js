847550b669fa1ca499b57e8ff3ed2932cee2b213
Steven Shin <sshin@mobify.com>
2017-02-10 09:05:57 -0800

    Remove web header in astro

--- web/app/containers/checkout-header/container.js
+++ web/app/containers/checkout-header/container.js
@@ -3,34 +3,39 @@ import React from 'react'
 import Button from 'progressive-web-sdk/dist/components/button'
 import {HeaderBar, HeaderBarTitle} from 'progressive-web-sdk/dist/components/header-bar'
 import {Icon} from 'progressive-web-sdk/dist/components/icon'
+import {isRunningInAstro} from '../../utils/astro-integration'
 
 const CheckoutHeader = function(props) {
     const canSignIn = true
-    return (
-        <header className="t-checkout-header">
-            <HeaderBar className="t-checkout-header__bar">
-                <HeaderBarTitle className="u-flex-none u-padding-start u-text-align-start">
-                    <h2 className="t-checkout-header__title u-heading-family u-text-uppercase">
-                        <span className="u-text-lighter">MERLIN'S</span> CHECKOUT
-                    </h2>
-                </HeaderBarTitle>
+    if (isRunningInAstro) {
+        return null
+    } else {
+        return (
+            <header className="t-checkout-header">
+                <HeaderBar className="t-checkout-header__bar">
+                    <HeaderBarTitle className="u-flex-none u-padding-start u-text-align-start">
+                        <h2 className="t-checkout-header__title u-heading-family u-text-uppercase">
+                            <span className="u-text-lighter">MERLIN'S</span> CHECKOUT
+                        </h2>
+                    </HeaderBarTitle>
 
-                <Icon name="lock" size="medium" className="u-flex-none" />
+                    <Icon name="lock" size="medium" className="u-flex-none" />
 
-                {canSignIn &&
-                    <div className="u-flex u-text-align-end">
-                        <Button
-                            href="/customer/account/login/"
-                            innerClassName="u-color-neutral-10"
-                        >
-                            <Icon name="user" className="u-margin-end-sm" />
-                            <span>Sign in</span>
-                        </Button>
-                    </div>
-                }
-            </HeaderBar>
-        </header>
-    )
+                    {canSignIn &&
+                        <div className="u-flex u-text-align-end">
+                            <Button
+                                href="/customer/account/login/"
+                                innerClassName="u-color-neutral-10"
+                            >
+                                <Icon name="user" className="u-margin-end-sm" />
+                                <span>Sign in</span>
+                            </Button>
+                        </div>
+                    }
+                </HeaderBar>
+            </header>
+        )
+    }
 }
 
 export default CheckoutHeader
