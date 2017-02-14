4c970709294ad48a8850430438220177f0e80084
Mike Lee <mlee@mobify.com>
2017-02-14 15:12:40 -0800

    [Payment] Update selectors for payment form

--- web/app/containers/checkout-payment/actions.js
+++ web/app/containers/checkout-payment/actions.js
@@ -2,7 +2,7 @@ import {createAction, makeRequest} from '../../utils/utils'
 import {jqueryResponse} from 'progressive-web-sdk/dist/jquery-response'
 import checkoutPaymentParser from './checkout-payment-parser'
 
-import {getShippingFormValues} from '../../store/form/selectors'
+import {getPaymentBillingFormValues} from '../../store/form/selectors'
 import {getCustomerEntityID} from './selectors'
 import {getIsLoggedIn} from '../app/selectors'
 
@@ -41,18 +41,18 @@ export const submitPayment = () => {
             city,
             region_id,
             postcode,
-            telephone,
-            shipping_method
-        } = getShippingFormValues(currentState)
+            // telephone,
+            // shipping_method
+        } = getPaymentBillingFormValues(currentState)
         const entityID = getCustomerEntityID(currentState)
         const isLoggedIn = getIsLoggedIn(currentState)
         const names = name.split(' ')
-        const shippingSelections = shipping_method.split('_')
+        // const shippingSelections = shipping_method.split('_')
         const addressData = {
             firstname: names.slice(0, -1).join(' '),
             lastname: names.slice(-1).join(' '),
             company: company || '',
-            telephone,
+            // telephone,
             postcode,
             city,
             street: addressLine2 ? [addressLine1, addressLine2] : [addressLine1],
@@ -60,19 +60,20 @@ export const submitPayment = () => {
             countryId: country_id,
             save_in_address_book: true
         }
-        const addressInformation = {
-            addressInformation: {
-                shippingAddress: addressData,
-                billingAddress: {
-                    ...addressData,
-                    saveInAddressBook: false
-                },
-                shipping_carrier_code: shippingSelections[0],
-                shipping_method_code: shippingSelections[1]
+        const paymentInformation = {
+            billingAddress: {
+                ...addressData
+            },
+            cartId: entityID,
+            email: ,
+            paymentMethod: {
+                additional_data: null,
+                method: "checkmo",
+                po_number: null
             }
         }
         const persistShippingURL = `https://www.merlinspotions.com/rest/default/V1/${isLoggedIn ? 'carts/mine' : `guest-carts/${entityID}`}/shipping-information`
-        makeJsonEncodedRequest(persistShippingURL, addressInformation, {method: 'POST'})
+        makeJsonEncodedRequest(persistShippingURL, paymentInformation, {method: 'POST'})
             .then((response) => response.json())
             .then((responseJSON) => {
                 if (responseJSON.payment_methods) {
--- web/app/store/form/selectors.js
+++ web/app/store/form/selectors.js
@@ -4,3 +4,7 @@ import {getForm} from '../selectors'
 export const getShippingForm = createSelector(getForm, (form) => form.shippingForm)
 
 export const getShippingFormValues = createSelector(getShippingForm, (shippingForm) => shippingForm.values)
+
+export const getPaymentBillingForm = createSelector(getForm, (form) => form.paymentBillingAddressForm)
+
+export const getPaymentBillingFormValues = createSelector(getPaymentBillingForm, (paymentBillingAddressForm) => paymentBillingAddressForm.values)
