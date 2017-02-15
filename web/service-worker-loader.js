8c536bf9ca1526838fd0f313433ead063777bfea
Kerr Marin Miller <kmarinmiller@mobify.com>
2017-02-15 11:55:10 -0800

    Revert change to service-worker-loader

--- web/service-worker-loader.js
+++ web/service-worker-loader.js
@@ -1,18 +1,8 @@
-import {getBuildOrigin} from 'progressive-web-sdk/dist/asset-utils'
-import {isRunningInAstro} from './app/utils/astro-integration'
+// THIS IS A STRING VALUE NOT A BOOLEAN, BE CAREFUL
+const isPreview = /preview=(true|false)/.exec(self.location.search)[1]
 
-try {
-    if (isRunningInAstro) {
-        throw 'Service worker not supported in Astro'
-    }
-    // THIS IS A STRING VALUE NOT A BOOLEAN, BE CAREFUL
-    const isPreview = /(true|false)/.exec(self.location.search)[1]
-    if (isPreview === 'true') {
-        const buildOrigin = getBuildOrigin()
-        self.importScripts(`${buildOrigin}/worker.js`)
-    } else {
-        self.importScripts('https://cdn.mobify.com/sites/progressive-web-scaffold/production/worker.js')
-    }
-} catch (e) {
-    console.log(e)
+if (isPreview === 'true') {
+    self.importScripts('https://localhost:8443/worker.js')
+} else {
+    self.importScripts('https://cdn.mobify.com/sites/progressive-web-scaffold/production/worker.js')
 }
