78e93fbdc2c84f91d7deb5cb1827f2d224ae2ba8
Jennifer Bandelin <jennifer@mobify.com>
2017-02-15 16:06:03 -0800

    Add comments to listMerger

--- web/app/utils/reducer-utils.js
+++ web/app/utils/reducer-utils.js
@@ -8,6 +8,9 @@ export const mergePayloadForActions = (...actions) => {
 }
 
 const isList = Immutable.List.isList
+// Ensures the new list is always used when merging Maps that contain lists
+// Otherwise we would end up with something like this:
+// a = {test: [1, 2, 3]}, b = {test: [1, 2]}, a.mergeDeep(b) => {test: [1, 2, 3]}
 export const listMerger = (a, b) => {
     if (a && a.mergeWith && !isList(a) && !isList(b)) {
         return a.mergeWith(listMerger, b)
