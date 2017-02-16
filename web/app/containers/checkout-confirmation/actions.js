165dffa7f2bfa74af7e7e46e8da52b20776a40f8
Jeff Kamo <kamojeffrey@gmail.com>
2017-02-16 14:28:29 -0800

    Replace success method with fail method

--- web/app/containers/checkout-confirmation/actions.js
+++ web/app/containers/checkout-confirmation/actions.js
@@ -82,9 +82,9 @@ export const updateShippingAndBilling = (parsedFormData) => {
                 Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
                 'Upgrade-Insecure-Requests': 1
             },
-            success: (response) => {
-                console.log('response for updating customer address', response)
-                history.pushState(null, null, 'https://www.merlinspotions.com/checkout/confirmation/')
+            error: (response) => {
+                console.error('Updating the user Shipping/Billing address failed. Response log:')
+                console.error(response)
             }
         })
     }
