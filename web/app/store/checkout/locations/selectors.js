2ebd8238d67adab54777e8e1949957083876c1cc
fractaltheory <michael@mobify.com>
2017-02-17 15:47:23 -0800

    Use existing shippingForm selectors

--- web/app/store/checkout/locations/selectors.js
+++ web/app/store/checkout/locations/selectors.js
@@ -2,15 +2,14 @@ import Immutable from 'immutable'
 import {createSelector} from 'reselect'
 
 import {createGetSelector} from '../../../utils/selector-utils'
-import {getCheckout, getForm} from '../../selectors'
+import {getShippingFormValues} from '../../form/selectors'
+import {getCheckout} from '../../selectors'
 
 export const getLocations = createGetSelector(getCheckout, 'locations', Immutable.Map())
 export const getCountries = createGetSelector(getLocations, 'countries', Immutable.List())
 export const getRegions = createGetSelector(getLocations, 'regions', Immutable.List())
 
-export const getShippingForm = createSelector(getForm, (form) => Immutable.fromJS(form.shippingForm))
-export const getShippingFormValues = createGetSelector(getShippingForm, 'values', Immutable.Map())
-export const getSelectedCountryID = createGetSelector(getShippingFormValues, 'country_id')
+export const getSelectedCountryID = createSelector(getShippingFormValues, (values) => { return values ? values.country_id : null })
 export const getDefaultRegionEntry = createGetSelector(getRegions, 0)
 
 // Filter list of available regions based on what user has selected as country
