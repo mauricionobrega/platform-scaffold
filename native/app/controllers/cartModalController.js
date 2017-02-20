11acbf54ff3d80a43b0598ffc4f1d79968f97651
Steven Shin <sshin@mobify.com>
2017-02-20 10:23:32 -0800

    Created 2 events that triggers alerView for Astro checkout

--- native/app/controllers/cartModalController.js
+++ native/app/controllers/cartModalController.js
@@ -3,6 +3,7 @@ import Promise from 'bluebird'
 import AnchoredLayoutPlugin from 'progressive-app-sdk/plugins/anchoredLayoutPlugin'
 import ModalViewPlugin from 'progressive-app-sdk/plugins/modalViewPlugin'
 import NavigationPlugin from 'progressive-app-sdk/plugins/navigationPlugin'
+import AlertViewPlugin from 'progressive-app-sdk/plugins/alertViewPlugin'
 
 import AppEvents from '../global/app-events'
 import CartHeaderController from './cartHeaderController'
@@ -17,6 +18,7 @@ const CartModalController = function(modalView, navigationView) {
     this.isShowing = false
     this.viewPlugin = modalView
     this.navigationView = navigationView
+    this.alertEnabled = false
 
     this.navigationView.on('sign-in:clicked', () => {
         this.hide()
@@ -26,6 +28,14 @@ const CartModalController = function(modalView, navigationView) {
     this.navigationView.on('continue:clicked', () => {
         this.hide()
         AppEvents.trigger(Events.shopShow)
+    })
+
+    this.navigationView.on('checkout:disable-alert', () => {
+        this.alertEnabled = false
+    })
+
+    this.navigationView.on('checkout:enable-alert', () => {
+        this.alertEnabled = true
     })
 }
 
@@ -51,6 +61,13 @@ CartModalController.init = async function() {
         }
     }
 
+    const alertViewOptions = {
+        title: 'Are you sure you want to exit checkout?',
+        text: 'You will lose all progress, but the items will remain in your cart for later!',
+        okButton: 'Exit',
+        cancelButton: 'Cancel'
+    }
+
     await anchoredLayout.addTopView(cartHeaderController.viewPlugin)
     await anchoredLayout.setContentView(navigationView)
     await navigationView.navigateToUrl(CartConfig.url, headerOptions, {})
@@ -60,12 +77,19 @@ CartModalController.init = async function() {
 
     const cartModalController = new CartModalController(modalView, navigationView)
 
-    cartHeaderController.on('close', () => {
-        cartModalController.hide()
-    })
-
     cartHeaderController.on('back', () => {
         navigationView.back()
+    })
+
+    cartHeaderController.on('close', async () => {
+        if (cartModalController.alertEnabled) {
+            const okButtonPressed = await AlertViewPlugin.alert(alertViewOptions)
+            if (okButtonPressed) {
+                cartModalController.hide()
+            }
+        } else {
+            cartModalController.hide()
+        }
     })
 
     return cartModalController
