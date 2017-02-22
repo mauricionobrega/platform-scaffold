ee7c32807b1240d6a3d5c552736b724e63e7c253
Ellen Wong <ellen@mobify.com>
2017-02-22 12:59:32 -0800

    Comments

--- web/scripts/lighthouse-ci.sh
+++ web/scripts/lighthouse-ci.sh
@@ -1,8 +1,10 @@
-# OUTPUT_PATH=./lighthouse/audit-local.html
-# URL=https://localhost
+# Run the Lighthouse test against the dev build with continuous integration.
 
-
-
+# Start the dev server
+# Spin up Chrome with the option to load a site with insecure certificates
+# Wait a bit for the dev server to be ready	then run Lighthouse against 
+# the proxied site
+# Parse the HTML report from Lighthouse
 concurrently --kill-others --success first --raw \
 	'npm run dev' \
 	'chrome-debug --allow-insecure-localhost' \
