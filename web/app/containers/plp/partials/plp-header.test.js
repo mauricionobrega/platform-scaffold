f88070eb1934f5a9047ced4a86c09762a300d233
Kerr Marin Miller <kmarinmiller@mobify.com>
2017-02-22 10:18:51 -0800

    Rename variable

--- web/app/containers/plp/partials/plp-header.test.js
+++ web/app/containers/plp/partials/plp-header.test.js
@@ -7,10 +7,10 @@ import {mount, shallow} from 'enzyme'
 
 import * as AstroIntegration from '../../../utils/astro-integration'
 
-const PLPHeading = ConnectedPLPHeading.WrappedComponent
+const PLPHeader = ConnectedPLPHeading.WrappedComponent
 
 test('renders without errors', () => {
-    const wrapper = mount(<PLPHeading />)
+    const wrapper = mount(<PLPHeader />)
 
     expect(wrapper.length).toBe(1)
 })
@@ -20,7 +20,7 @@ const ROOT_CLASS = 't-plp'
 test('doesnt render the breadcrumbs if running in Astro', () => {
     AstroIntegration.isRunningInAstro = true
 
-    const wrapper = shallow(<PLPHeading />)
+    const wrapper = shallow(<PLPHeader />)
     const breadcrumbs = wrapper.find(`.${ROOT_CLASS}__breadcrumb`)
     expect(breadcrumbs.length).toBe(0)
 })
@@ -28,7 +28,7 @@ test('doesnt render the breadcrumbs if running in Astro', () => {
 test('renders the breadcrumbs if not running in Astro', () => {
     AstroIntegration.isRunningInAstro = false
 
-    const wrapper = shallow(<PLPHeading />)
+    const wrapper = shallow(<PLPHeader />)
     const breadcrumbs = wrapper.find(`.${ROOT_CLASS}__breadcrumb`)
     expect(breadcrumbs.length).toBe(1)
 })
