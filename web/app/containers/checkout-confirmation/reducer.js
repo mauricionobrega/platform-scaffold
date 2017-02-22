caa7648b45cca575771cc393f285bab58f336843
Jennifer Bandelin <jennifer@mobify.com>
2017-02-21 16:54:21 -0800

    Remove un-used reducers

--- web/app/containers/checkout-confirmation/reducer.js
+++ web/app/containers/checkout-confirmation/reducer.js
@@ -5,12 +5,6 @@ import {mergePayloadForActions} from '../../utils/reducer-utils'
 
 export default handleActions({
     ...mergePayloadForActions(checkoutConfirmationActions.receiveData),
-    [checkoutConfirmationActions.showSuccessModal]: (state) => {
-        return state.mergeDeep({isModalShown: true})
-    },
-    [checkoutConfirmationActions.hideModal]: (state) => {
-        return state.mergeDeep({isModalShown: false})
-    },
     [checkoutConfirmationActions.hideRegistrationForm]: (state) => {
         return state.mergeDeep({isRegistrationFormHidden: true})
     },
