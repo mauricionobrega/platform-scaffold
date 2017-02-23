49e780ce6d5211a3968ff4dad166f07497abe607
Ellen Wong <ellen@mobify.com>
2017-02-22 17:41:32 -0800

    CI script uses variables again

--- web/scripts/lighthouse-ci.sh
+++ web/scripts/lighthouse-ci.sh
@@ -1,5 +1,8 @@
 # Run the Lighthouse test against the dev build with continuous integration.
 
+OUTPUT_PATH=./lighthouse/audit-local.html
+URL=https://localhost
+
 # Start the dev server
 # Spin up Chrome with the option to load a site with insecure certificates
 # Wait a bit for the dev server to be ready	then run Lighthouse against 
@@ -8,10 +11,10 @@
 concurrently --kill-others --success first --raw \
 	'npm run dev' \
 	'chrome-debug --allow-insecure-localhost' \
-	'sleep 40 && lighthouse \
+	"sleep 40 && lighthouse \
 		--skip-autolaunch \
 		--output=html \
-		--output-path=./lighthouse/audit-local.html \
-		https://localhost'
+		--output-path=${OUTPUT_PATH} \
+		${URL}"
 
 node ./lighthouse/check-score.js
