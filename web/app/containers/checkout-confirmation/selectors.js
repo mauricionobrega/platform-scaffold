c34c66ee4be6a5f252bedf988006305c5198492d
Jennifer Bandelin <jennifer@mobify.com>
2017-02-23 16:52:46 -0800

    Remove un-used selectors

--- web/app/containers/checkout-confirmation/selectors.js
+++ web/app/containers/checkout-confirmation/selectors.js
@@ -4,8 +4,6 @@ import {getUi} from '../../store/selectors'
 
 export const getCheckoutConfirmation = createSelector(getUi, ({checkoutConfirmation}) => checkoutConfirmation)
 
-export const getEmailAddress = createGetSelector(getCheckoutConfirmation, 'emailAddress')
 export const getIsRegistrationFormHidden = createGetSelector(getCheckoutConfirmation, 'isRegistrationFormHidden')
-export const getIsModalShown = createGetSelector(getCheckoutConfirmation, 'isModalShown')
 export const getOrderNumber = createGetSelector(getCheckoutConfirmation, 'orderNumber')
 export const getOrderUrl = createGetSelector(getCheckoutConfirmation, 'orderUrl')
