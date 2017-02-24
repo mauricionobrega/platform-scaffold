e7b11b5a9d22f56b05fd91cf4e9231afc921cfba
Cole Sanderson <cole@mobify.me>
2017-02-24 13:54:12 -0800

    PLATT-43: refactor

--- web/app/containers/home/partials/home-nearest-stores.jsx
+++ web/app/containers/home/partials/home-nearest-stores.jsx
@@ -1,8 +1,11 @@
-/* eslint-disable react/self-closing-comp, max-len */
-
+/* eslint-disable react/self-closing-comp */
 import React, {PropTypes} from 'react'
 import classNames from 'classnames'
 import Button from 'progressive-web-sdk/dist/components/button'
+
+// Merlins Potions Nearby Widget Config
+import merlinsPotionsNearbyConfig from '../../../config/merlins-potions-nearby-config.json'
+
 const componentClass = 'c-nearest-stores'
 
 /**
@@ -12,40 +15,23 @@ const componentClass = 'c-nearest-stores'
 class HomeNearestStores extends React.Component {
 
     componentWillMount() {
-        const content = '<div><%=city%></div> <div class="u-text-bold"><%=distanceKM%>km</div>'
-        const noLocationContent = 'Sorry, No locations near you.'
-
-        const merlinsPotionsTemplateOne = `<% if(id){ %> <div class="pw-list-tile c-list-tile u-border-light-bottom u-text-font-family pw--is-anchor c--is-anchor"><a href="http://locations.merlinspotions.com<%=llpURL%>" class="pw-link c-link pw-list-tile__primary c-list-tile__primary"><div class="pw-list-tile__action c-list-tile__action"><svg aria-hidden="true" class="pw-icon c-icon" title="Store Icon" aria-labelledby="icon-888"><title id="icon-888"></title><use role="img" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#pw-store"></use></svg></div><div class="pw-list-tile__content c-list-tile__content u-padding-0 u-justify-between u-row"> ${content} </div><div class="pw-list-tile__action c-list-tile__action"><button class="pw-button c-button c--blank pw--icon-only c--icon-only" type="button"><div class="pw-button__inner c-button__inner"><svg aria-hidden="true" class="pw-icon c-icon pw-button__icon c-button__icon" title="Chevron Right Icon" aria-labelledby="icon-999"><title id="icon-999"></title><use role="img" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#pw-chevron-right"></use></svg></div></button></div></a></div> <% } else { %> <div class="u-padding-md u-text-align-center">${noLocationContent}</div> <% } %>`
-
-        const merlinsPotionsTemplateTwo = `<% if(id){ %> <div class="pw-list-tile c-list-tile u-border-light-bottom u-text-font-family pw--is-anchor c--is-anchor"><a href="http://locations.merlinspotions.com<%=llpURL%>" class="pw-link c-link pw-list-tile__primary c-list-tile__primary"><div class="pw-list-tile__action c-list-tile__action"><svg aria-hidden="true" class="pw-icon c-icon" title="Store Icon" aria-labelledby="icon-888"><title id="icon-888"></title><use role="img" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#pw-store"></use></svg></div><div class="pw-list-tile__content c-list-tile__content u-padding-0 u-justify-between u-row"> ${content} </div><div class="pw-list-tile__action c-list-tile__action"><button class="pw-button c-button c--blank pw--icon-only c--icon-only" type="button"><div class="pw-button__inner c-button__inner"><svg aria-hidden="true" class="pw-icon c-icon pw-button__icon c-button__icon" title="Chevron Right Icon" aria-labelledby="icon-999"><title id="icon-999"></title><use role="img" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#pw-chevron-right"></use></svg></div></button></div></a></div> <% } else { %><% } %>`
-
-        const merlinsPotionsData = {
-            business_id: '56c3681586312e000e000001', // Merlins Potions customer ID
-            configs: [
-                {
-                    selector: '#donde-closest-locations-widget-1', // render in this element
-                    nthNearest: 1, // 1st nearest store
-                    maxDistance: '100', // look in 100 mi ra3dius
-                    showSpinner: true,
-                    template: merlinsPotionsTemplateOne // output template
-                },
-                {
-                    selector: '#donde-closest-locations-widget-2', // render in this element
-                    nthNearest: 2, // 2nd nearest store
-                    maxDistance: '100', // look in 100 mi radius
-                    template: merlinsPotionsTemplateTwo // output template
-                },
-                {
-                    selector: '#donde-closest-locations-widget-3', // render in this element
-                    nthNearest: 3, // 3rd nearest store
-                    maxDistance: '100', // look in 100 mi radius
-                    template: merlinsPotionsTemplateTwo // output template
-                }
-            ]
-        }
-
-        const merlinsPotionsAsync = !function(a){var b=document.createElement("script");b.type="text/javascript",b.src="https://dtopnrgu570sp.cloudfront.net/nearby-widget/nearby.min.js",b.setAttribute("async",!0),b.addEventListener?b.addEventListener("load",function(b){a(null,b)},!1):b.onreadystatechange=function(){b.readyState in{loaded:1,complete:1}&&(b.onreadystatechange=null,a())}, document.head.appendChild(b)}(function(){
-            window.DondeNearby.load({ ...merlinsPotionsData })
+        // Nearby widget async script
+        const merlinsPotionsAsync = !function(a) { // eslint-disable-line wrap-iife
+            const b = document.createElement('script')
+            b.type = 'text/javascript'
+            b.src = 'https://dtopnrgu570sp.cloudfront.net/nearby-widget/nearby.min.js'
+            b.setAttribute('async', !0)
+            b.addEventListener ? b.addEventListener('load', (b) => { // eslint-disable-line no-unused-expressions
+                a(null, b)
+            }, !1) : b.onreadystatechange = function() {
+                b.readyState in { // eslint-disable-line no-unused-expressions
+                    loaded: 1,
+                    complete: 1
+                } && (b.onreadystatechange = null, a())
+            }
+            document.head.appendChild(b)
+        }(() => {
+            window.DondeNearby.load({...merlinsPotionsNearbyConfig})
         })
 
         const merlinsPotionsScript = document.createElement('script')
@@ -64,15 +50,23 @@ class HomeNearestStores extends React.Component {
             // 'c--modifier': bool ? true : false
         })
 
+        const closestLocations = merlinsPotionsNearbyConfig.configs
+
         return (
             <div className={classes}>
                 <div className="u-card u-padding-md u-padding-top-lg u-padding-bottom-lg">
                     <h2 className="u-padding-bottom-md u-border-light-bottom u-text-uppercase">{title}</h2>
 
                     <div className="u-margin-bottom-md">
-                        <div id="donde-closest-locations-widget-1"></div>
-                        <div id="donde-closest-locations-widget-2"></div>
-                        <div id="donde-closest-locations-widget-3"></div>
+
+                        {closestLocations.map((id, idx) => {
+                            let selectorString = id.selector
+                            selectorString = selectorString.slice(1, selectorString.length)
+
+                            return (
+                                <div id={selectorString} key={idx}></div>
+                            )
+                        })}
                     </div>
 
                     <Button className="c--tertiary u-text-uppercase u-width-full" href="https://locations.merlinspotions.com">{viewAllStoresText}</Button>
