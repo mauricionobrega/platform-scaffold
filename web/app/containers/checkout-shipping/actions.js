97350de85e449c722e87b5dd1d670ce0feca92b5
Jennifer Bandelin <jennifer@mobify.com>
2017-02-28 14:18:11 -0800

    Fix lint

--- web/app/containers/checkout-shipping/actions.js
+++ web/app/containers/checkout-shipping/actions.js
@@ -1,6 +1,3 @@
 import {createAction} from '../../utils/utils'
-import CheckoutShipping from './container'
-import {addNotification, fetchPage, removeAllNotifications} from '../app/actions'
-import {getShippingFormValues} from '../../store/form/selectors'
 
 export const showCompanyAndApt = createAction('Showing the "Company" and "Apt #" fields')
