e9407e4c489341a6bb5010ab34f8d78db97a8b19
Jennifer Bandelin <jennifer@mobify.com>
2017-02-28 11:33:05 -0800

    Fix lint

--- web/app/store/checkout/actions.js
+++ web/app/store/checkout/actions.js
@@ -1,11 +1,29 @@
 import {createAction} from '../../utils/utils'
-import {getCheckoutEntityID, extractMagentoJson} from '../../utils/magento-utils'
+import {extractMagentoShippingStepData, getCheckoutEntityID, extractMagentoJson} from '../../utils/magento-utils'
 
 import parseLocations from './locations/parser'
+import {parseShippingInitialValues} from './shipping/parser'
 
 export const receiveCheckoutData = createAction('Receive Checkout Data')
 
 export const receiveShippingMethodInitialValues = createAction('Receive Shipping Method Initial Values', 'shipping')
+
+export const processCheckoutData = ({payload: {$response}}) => {
+    return (dispatch) => {
+        const customerEntityID = getCheckoutEntityID($response)
+        const magentoFieldData = extractMagentoShippingStepData($response).getIn(['children', 'shipping-address-fieldset', 'children'])
+        const initialValues = parseShippingInitialValues(magentoFieldData)
+        const locationsData = parseLocations(magentoFieldData)
+
+        dispatch(receiveCheckoutData({
+            // entity_id is used for API calls
+            customerEntityID,
+            ...locationsData,
+            shipping: {initialValues}
+        }))
+    }
+}
+
 
 const ESTIMATE_FIELD_PATH = ['#block-summary', 'Magento_Ui/js/core/app', 'components', 'block-summary', 'children', 'block-shipping', 'children', 'address-fieldsets', 'children']
 
