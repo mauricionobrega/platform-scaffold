5a09ac68587fd0ada5a3c557a7bbba9dd8362f15
Ellen Wong <ellen@mobify.com>
2017-03-01 16:16:08 -0800

    bash: the great escape

--- web/scripts/lighthouse-local.sh
+++ web/scripts/lighthouse-local.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 OUTPUT_PATH=./lighthouse/audit-local.html
-URL=https://localhost
+URL="https://www.merlinspotions.com/#mobify-override\&mobify-path=true\&mobify-url=https://localhost:8443/loader.js\&mobify-global=true\&mobify-domain=\&mobify-all=true\&mobify=1\&mobify-debug=1\&mobify-js=1"
 
 concurrently --kill-others --success first --raw \
-	'npm run proxy' \
-	'chrome-debug --allow-insecure-localhost' \
+	'chrome-debug --allow-insecure-localhost --user-agent="Chrome MobifyPreview"' \
 	"sleep 5 && lighthouse \
 		--skip-autolaunch \
 		--output=html \
