969a6999008a57c985137ea45646ab1486a01052
Ellen Wong <ellen@mobify.com>
2017-03-03 15:26:34 -0800

    Try lighthouse score with MobifyPreview in user agent

--- web/scripts/lighthouse-ci.sh
+++ web/scripts/lighthouse-ci.sh
@@ -4,18 +4,11 @@
 OUTPUT_PATH=./lighthouse/audit-local.html
 URL=https://localhost
 
-# Start the dev server
-# Spin up Chrome with the option to load a site with insecure certificates
-# Wait a bit for the dev server to be ready	then run Lighthouse against 
-# the proxied site
-# Parse the HTML report from Lighthouse
 concurrently --kill-others --success first --raw \
-	'npm run dev' \
-	'chrome-debug --allow-insecure-localhost' \
-	"sleep 40 && lighthouse \
-		--skip-autolaunch \
-		--output=html \
-		--output-path=${OUTPUT_PATH} \
-		${URL}"
+	'chrome-debug --allow-insecure-localhost --user-agent="Chrome MobifyPreview"' \
+	"sleep 5 && lighthouse --skip-autolaunch --output=html \
+		--output-path=./lighthouse/audit-local.html \
+		https://www.merlinspotions.com/#mobify-override&mobify-path=true&mobify-url=https://localhost:8443/loader.js&mobify-global=true&mobify-domain=&mobify-all=true&mobify=1&mobify-debug=1&mobify-js=1"
+
 
 node ./lighthouse/check-score.js
