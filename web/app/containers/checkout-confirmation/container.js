81bb571e2030e472118a1751eb142c437f76ec1f
Kerr Marin Miller <kmarinmiller@mobify.com>
2017-03-07 11:32:59 -0800

    Trigger events for astro in checkout confirmation

--- web/app/containers/checkout-confirmation/container.js
+++ web/app/containers/checkout-confirmation/container.js
@@ -5,13 +5,26 @@ import CheckoutConfirmationDetails from './partials/checkout-confirmation-detail
 import CheckoutConfirmationModal from './partials/checkout-confirmation-modal'
 import CheckoutConfirmationQuestions from './partials/checkout-confirmation-questions'
 
-const CheckoutConfirmation = () => (
-    <div className="t-checkout-confirmation">
-        <CheckoutConfirmationSplash />
-        <CheckoutConfirmationDetails />
-        <CheckoutConfirmationQuestions />
-        <CheckoutConfirmationModal />
-    </div>
-)
+import {trigger} from '../../vendor/astro-client'
+
+class CheckoutConfirmation extends React.Component {
+    componentDidMount() {
+        trigger('checkout:disable-alert')
+        trigger('cart-updated', {
+            count: 0
+        })
+    }
+
+    render() {
+        return (
+            <div className="t-checkout-confirmation">
+                <CheckoutConfirmationSplash />
+                <CheckoutConfirmationDetails />
+                <CheckoutConfirmationQuestions />
+                <CheckoutConfirmationModal />
+            </div>
+        )
+    }
+}
 
 export default CheckoutConfirmation
