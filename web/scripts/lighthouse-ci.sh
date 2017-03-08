a782746753c0e789c99a8a86b2b1215acdee6b28
Ellen Wong <ellen@mobify.com>
2017-03-08 13:10:11 -0800

    forgot to start the server, oops

--- web/scripts/lighthouse-ci.sh
+++ web/scripts/lighthouse-ci.sh
@@ -6,7 +6,6 @@ OUTPUT_PATH=./reports/audit-local.html
 # Change www.merlinspotions.com to the project's URL. 
 URL=https://www.merlinspotions.com/#mobify-override\&mobify-path=true\&mobify-url=https://localhost:8443/loader.js\&mobify-global=true\&mobify-domain=\&mobify-all=true\&mobify=1\&mobify-debug=1\&mobify-js=1
 
-# The development server should already be running via 'npm run dev'
 # Lighthouse uses your local installation of Chrome, which should be at least 
 # version 54.0. Use a custom user agent containing "MobifyPreview" so that 
 # Preview will accept our requests, and disable device emulation so that the
@@ -16,6 +15,8 @@ URL=https://www.merlinspotions.com/#mobify-override\&mobify-path=true\&mobify-ur
 # CI will fail the build if the score is below a threshold.
 # See min_lighthouse_score in package.json
 
+npm run dev &
+sleep 20
 lighthouse \
 	--chrome-flags='--user-agent="MobifyPreview" --allow-insecure-localhost' \
 	--output=html \
@@ -23,4 +24,5 @@ lighthouse \
 	--disable-device-emulation=true \
 	"${URL}"
 
+kill %1
 node ./lighthouse/check-score.js
