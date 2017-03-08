bd68e5688e2a40686b1fbeebfb71bfd1cda976dd
Ellen Wong <ellen@mobify.com>
2017-03-08 13:29:04 -0800

    kill background processes

--- web/scripts/lighthouse-ci.sh
+++ web/scripts/lighthouse-ci.sh
@@ -6,6 +6,8 @@ OUTPUT_PATH=./reports/audit-local.html
 # Change www.merlinspotions.com to the project's URL. 
 URL=https://www.merlinspotions.com/#mobify-override\&mobify-path=true\&mobify-url=https://localhost:8443/loader.js\&mobify-global=true\&mobify-domain=\&mobify-all=true\&mobify=1\&mobify-debug=1\&mobify-js=1
 
+trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
+
 # Lighthouse uses your local installation of Chrome, which should be at least 
 # version 54.0. Use a custom user agent containing "MobifyPreview" so that 
 # Preview will accept our requests, and disable device emulation so that the
@@ -24,5 +26,4 @@ lighthouse \
 	--disable-device-emulation=true \
 	"${URL}"
 
-kill %1
 node ./lighthouse/check-score.js
