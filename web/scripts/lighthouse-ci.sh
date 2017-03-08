b43dee617d638372c08fc650c8c2fbdf004376af
Ellen Wong <ellen@mobify.com>
2017-03-08 09:54:55 -0800

    Put Lighthouse reports under reports

--- web/scripts/lighthouse-ci.sh
+++ web/scripts/lighthouse-ci.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 # Run the Lighthouse test against the dev build with continuous integration.
 
+# Location to save the generated HTML report.
+OUTPUT_PATH=./reports/audit-local.html
+# Change www.merlinspotions.com to the project's URL. 
+URL=https://www.merlinspotions.com/#mobify-override\&mobify-path=true\&mobify-url=https://localhost:8443/loader.js\&mobify-global=true\&mobify-domain=\&mobify-all=true\&mobify=1\&mobify-debug=1\&mobify-js=1
+
 # The development server should already be running via 'npm run dev'
-# chrome-debug is available via the Lighthouse module.
-# Use a custom user agent containing "MobifyPreview" so that Preview will
-# accept our requests. 
+# Lighthouse uses your local installation of Chrome, which should be at least 
+# version 54.0. Use a custom user agent containing "MobifyPreview" so that 
+# Preview will accept our requests, and disable device emulation so that the
+# "MobifyPreview" user agent does not get overridden. 
 
 # Finally, parse the HTML report for the Lighthouse score. 
 # CI will fail the build if the score is below a threshold.
 # See min_lighthouse_score in package.json
 
-# Change www.merlinspotions.com to the project's URL. 
-
-concurrently --kill-others --success --raw \
-	'chrome-debug --allow-insecure-localhost --user-agent="Chrome MobifyPreview"' \
-	"sleep 5 && lighthouse --skip-autolaunch --output=html \
-		--output-path=./lighthouse/audit-local.html \
-		https://www.merlinspotions.com/#mobify-override&mobify-path=true&mobify-url=https://localhost:8443/loader.js&mobify-global=true&mobify-domain=&mobify-all=true&mobify=1&mobify-debug=1&mobify-js=1"
+lighthouse \
+	--chrome-flags='--user-agent="MobifyPreview" --allow-insecure-localhost' \
+	--output=html \
+	--output-path=${OUTPUT_PATH} \
+	--disable-device-emulation=true \
+	"${URL}"
 
 node ./lighthouse/check-score.js
--- web/scripts/lighthouse-local.sh
+++ web/scripts/lighthouse-local.sh
@@ -1,33 +1,22 @@
 #!/usr/bin/env bash
 
-OUTPUT_PATH=./lighthouse/audit-local.html
+# Location to save the generated HTML report.
+OUTPUT_PATH=./reports/audit-local.html
+# Change www.merlinspotions.com to the project's URL. 
 URL=https://www.merlinspotions.com/#mobify-override\&mobify-path=true\&mobify-url=https://localhost:8443/loader.js\&mobify-global=true\&mobify-domain=\&mobify-all=true\&mobify=1\&mobify-debug=1\&mobify-js=1
-# URL="https://www.merlinspotions.com/#mobify-override&mobify-path=true&mobify-url=https://localhost:8443/loader.js&mobify-global=true&mobify-domain=&mobify-all=true&mobify=1&mobify-debug=1&mobify-js=1"
 
 # The development server should already be running via 'npm run dev'
-# chrome-debug is available via the Lighthouse module. This uses your local
-# installation of Chrome, which should be at least version 54.0. 
-# Use a custom user agent containing "MobifyPreview" so that Preview will
-# accept our requests. 
-
-# Change www.merlinspotions.com to the project's URL. 
-
-# concurrently --kill-others --success --raw \
-# 	'chrome-debug --allow-insecure-localhost --user-agent="Chrome MobifyPreview"' \
-# 	"sleep 5 && lighthouse --skip-autolaunch --output=html \
-# 		--output-path=./lighthouse/audit-local.html \
-# 		https://www.merlinspotions.com/#mobify-override&mobify-path=true&mobify-url=https://localhost:8443/loader.js&mobify-global=true&mobify-domain=&mobify-all=true&mobify=1&mobify-debug=1&mobify-js=1"
+# Lighthouse uses your local installation of Chrome, which should be at least 
+# version 54.0. Use a custom user agent containing "MobifyPreview" so that 
+# Preview will accept our requests, and disable device emulation so that the
+# "MobifyPreview" user agent does not get overridden. 
 
 lighthouse \
+	--view \
 	--chrome-flags='--user-agent="MobifyPreview" --allow-insecure-localhost' \
 	--output=html \
 	--output-path=${OUTPUT_PATH} \
-	--view=true \
 	--disable-device-emulation=true \
-	"https://www.merlinspotions.com/#mobify-override&mobify-path=true&mobify-url=https://localhost:8443/loader.js&mobify-global=true&mobify-domain=&mobify-all=true&mobify=1&mobify-debug=1&mobify-js=1"
-	# "${URL}"
-	# ${URL}
-
-# open ${OUTPUT_PATH}
+	"${URL}"
 
  
