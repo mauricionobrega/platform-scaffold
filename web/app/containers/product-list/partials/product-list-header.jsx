7dca86eb73cb4309fbf4e739c41167a8d59306c2
Jennifer Bandelin <jennifer@mobify.com>
2017-03-09 13:21:36 -0800

    Add correct breadcrumbs to PLP

--- web/app/containers/product-list/partials/product-list-header.jsx
+++ web/app/containers/product-list/partials/product-list-header.jsx
@@ -10,12 +10,12 @@ import Image from 'progressive-web-sdk/dist/components/image'
 
 import {isRunningInAstro} from '../../../utils/astro-integration'
 
-const ProductListHeader = ({title, contentsLoaded}) => (
+const ProductListHeader = ({title, contentsLoaded, parentName, parentHref}) => (
     <div className="u-flexbox u-align-bottom">
         <div className="u-flex u-padding-top-lg u-padding-bottom-lg u-padding-start-md">
             {!isRunningInAstro &&
                 <div className="t-product-list__breadcrumb">
-                    <Link href="/" className="u-text-small">Home</Link>
+                    <Link href={parentHref} className="u-text-small">{parentName}</Link>
                 </div>
             }
             <div className="u-margin-top-md">
@@ -43,11 +43,15 @@ const ProductListHeader = ({title, contentsLoaded}) => (
 
 ProductListHeader.propTypes = {
     contentsLoaded: PropTypes.bool,
+    parentHref: PropTypes.string,
+    parentName: PropTypes.string,
     title: PropTypes.string
 }
 
 const mapStateToProps = createStructuredSelector({
     contentsLoaded: selectors.getProductListContentsLoaded,
+    parentHref: selectors.getProductListParentHref,
+    parentName: selectors.getProductListParentName,
     title: selectors.getProductListTitle
 })
 
