1e2805e127d544f6a298176230d3a82e956e9094
Jeremy Wiebe <jeremy@mobify.com>
2017-03-13 09:10:02 -0700

    Move things out of 'store' into merlin's connector

--- web/app/integration-manager/_merlins-connector/product-details/commands.js
+++ web/app/integration-manager/_merlins-connector/product-details/commands.js
@@ -1,11 +1,10 @@
 import {urlToPathKey} from '../../../utils/utils'
-import {productDetailsParser} from '../../../store/products/parser'
 import {receiveFormInfo} from '../../actions'
 import {receivePdpProductData} from '../../responses'
 
 import {fetchPageData} from '../utils'
 
-import {pdpAddToCartFormParser} from './parsers'
+import {productDetailsParser, pdpAddToCartFormParser} from './parsers'
 
 export const fetchPdpData = (url) => (dispatch) => {
     return dispatch(fetchPageData(url))
--- web/app/integration-manager/_merlins-connector/product-details/parsers.js
+++ web/app/integration-manager/_merlins-connector/product-details/parsers.js
@@ -1,3 +1,24 @@
+import {extractMagentoJson} from '../../../utils/magento-utils'
+import {getTextFrom} from '../../../utils/parser-utils'
+
+const parseCarouselItems = (magentoObject) => {
+    const carouselSetup = magentoObject
+          .getIn(['[data-gallery-role=gallery-placeholder]', 'mage/gallery/gallery', 'data'])
+          .sortBy((item) => item.get('position'))
+    return carouselSetup.toJS()
+}
+
+export const productDetailsParser = ($, $html) => {
+    const $mainContent = $html.find('.page-main')
+    const magentoObject = extractMagentoJson($html)
+    return {
+        title: getTextFrom($mainContent, '.page-title-wrapper.product .page-title > span'),
+        price: getTextFrom($mainContent, '.product-info-price .price-wrapper .price'),
+        carouselItems: parseCarouselItems(magentoObject),
+        description: getTextFrom($mainContent, '.product.info.detailed .product.attibute.description p')
+    }
+}
+
 export const pdpAddToCartFormParser = ($, $html) => {
     const $mainContent = $html.find('.page-main')
     const $form = $mainContent.find('#product_addtocart_form')
