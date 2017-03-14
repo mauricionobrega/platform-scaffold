4a86d6ae82ae645af25fef0b137df0973c584130
Jeremy Wiebe <jeremy@mobify.com>
2017-03-14 08:00:20 -0700

    Fix product details actions tests

--- web/app/containers/product-details/actions.js
+++ web/app/containers/product-details/actions.js
@@ -43,7 +43,7 @@ export const submitCartForm = () => (dispatch, getStore) => {
         })
         .catch((error) => {
             // TODO?? How do we communicate errors to the user?? Modal?
-            console.error('Error adding to cart:', error)
+            console.error(`Error adding to cart: ${error}`)
         })
         .then(() => {
             dispatch(addToCartComplete())
--- web/app/containers/product-details/actions.test.js
+++ web/app/containers/product-details/actions.test.js
@@ -2,7 +2,9 @@
 import Immutable from 'immutable'
 import * as fetchUtils from 'progressive-web-sdk/dist/utils/fetch-utils'
 
-jest.mock('./')
+import {openModal} from '../../store/modals/actions'
+
+import {PRODUCT_DETAILS_ITEM_ADDED_MODAL} from './constants'
 import {addToCartStarted, submitCartForm} from './actions'
 
 jest.mock('../../integration-manager/commands')
@@ -10,23 +12,11 @@ import {addToCart} from '../../integration-manager/commands'
 
 /* eslint-disable import/namespace */
 
-let realMakeFormEncodedRequest
+describe('Add to Cart', () => {
+    const mockDispatch = jest.fn()
+    mockDispatch.mockImplementation((...args) => args[0])
 
-beforeAll(() => {
-    realMakeFormEncodedRequest = fetchUtils.makeFormEncodedRequest
-    fetchUtils.makeFormEncodedRequest = jest.fn()
-    fetchUtils.makeFormEncodedRequest.mockReturnValue(Promise.resolve())
-
-    addToCart.mockImplementation(() => Promise.resolve())
-})
-
-afterAll(() => {
-    fetchUtils.makeFormEncodedRequest = realMakeFormEncodedRequest
-})
-
-test('submitCartForm marks operation as in progress and adds to cart', () => {
-    const submitCartFormThunk = submitCartForm()
-    expect(typeof submitCartFormThunk).toBe('function')
+    let realMakeFormEncodedRequest
 
     const getStore = () => ({
         ui: {
@@ -37,16 +27,49 @@ test('submitCartForm marks operation as in progress and adds to cart', () => {
         }
     })
 
-    const mockDispatch = jest.fn()
-    mockDispatch.mockImplementation((...args) => args[0])
-
-    submitCartFormThunk(mockDispatch, getStore).then(() => {
-        expect(mockDispatch).toBeCalled()
-        expect(mockDispatch.mock.calls[0][0]).toEqual(addToCartStarted())
-        expect(mockDispatch.mock.calls[1][0]).toEqual(addToCart('/', 1))
+    beforeAll(() => {
+        realMakeFormEncodedRequest = fetchUtils.makeFormEncodedRequest
+        fetchUtils.makeFormEncodedRequest = jest.fn()
+        fetchUtils.makeFormEncodedRequest.mockReturnValue(Promise.resolve())
     })
-})
 
-test('submitCartForm shows the "added to cart" modal if it succeeds', () => {
+    afterAll(() => {
+        fetchUtils.makeFormEncodedRequest = realMakeFormEncodedRequest
+    })
 
+    beforeEach(() => {
+        mockDispatch.mockClear()
+
+        addToCart.mockReset()
+        addToCart.mockImplementation(() => Promise.resolve())
+    })
+
+    test('submitCartForm marks operation as in progress and adds to cart', () => {
+        const submitCartFormThunk = submitCartForm()
+        expect(typeof submitCartFormThunk).toBe('function')
+
+        submitCartFormThunk(mockDispatch, getStore).then(() => {
+            expect(mockDispatch).toBeCalled()
+            expect(mockDispatch.mock.calls[0][0]).toEqual(addToCartStarted())
+            expect(mockDispatch.mock.calls[1][0]).toEqual(addToCart('/', 1))
+        })
+    })
+
+    test('submitCartForm shows the "added to cart" modal if it succeeds', () => {
+        const submitCartFormThunk = submitCartForm()
+
+        return submitCartFormThunk(mockDispatch, getStore).then(() => {
+            expect(mockDispatch).toHaveBeenCalledWith(openModal(PRODUCT_DETAILS_ITEM_ADDED_MODAL))
+        })
+    })
+
+    test('submitCartForm does not show the "added to cart" modal if it fails', () => {
+        const submitCartFormThunk = submitCartForm()
+        addToCart.mockReset()
+        addToCart.mockImplementation(() => Promise.reject('Network error adding to cart'))
+
+        return submitCartFormThunk(mockDispatch, getStore).then(() => {
+            expect(mockDispatch).not.toHaveBeenCalledWith(openModal(PRODUCT_DETAILS_ITEM_ADDED_MODAL))
+        })
+    })
 })
