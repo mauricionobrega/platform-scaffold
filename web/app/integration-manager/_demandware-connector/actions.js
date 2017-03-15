cb0ffe84d2d0f9c5164dfbcd71a55898f55dad2e
Jeremy Wiebe <jeremy@mobify.com>
2017-03-15 13:01:37 -0700

    Adding an example action for the Demandware reducer

--- web/app/integration-manager/_demandware-connector/actions.js
+++ web/app/integration-manager/_demandware-connector/actions.js
@@ -1,8 +1,8 @@
-// import {createAction} from '../../utils/utils'
+import {createAction} from '../../utils/utils'
 
 // Add other actions here that are specific to this connector.
 // Actions that are returned out of the connector and reduced
 // by the app should go into ./responses.js
 
 // Example.
-// export const myAction = createAction('Some Demandware-specific action')
+export const exampleAction = createAction('Some Demandware-specific action')
--- web/app/integration-manager/_demandware-connector/reducer.js
+++ web/app/integration-manager/_demandware-connector/reducer.js
@@ -2,13 +2,14 @@ import {handleActions} from 'redux-actions'
 
 import Immutable from 'immutable'
 
-// import {someAction} from './actions'
+// This is an example action. It can be deleted.
+import {exampleAction} from './actions'
 
 const initialState = Immutable.Map()
 
 // Add Demandware-specific actions here
 const reducer = handleActions({
-    // [someAction]: (state, {payload}) => state.mergeDeep(payload)
+    [exampleAction]: (state, {payload}) => state.mergeDeep(payload)
 }, initialState)
 
 export default reducer
