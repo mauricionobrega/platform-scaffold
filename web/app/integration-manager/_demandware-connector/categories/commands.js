6c60e0eac1b16063e3fcb0e5a66a06b21357fb51
Jennifer Bandelin <jennifer@mobify.com>
2017-03-17 08:47:12 -0700

    Add missing headers to PLP fetch

--- web/app/integration-manager/_demandware-connector/categories/commands.js
+++ web/app/integration-manager/_demandware-connector/categories/commands.js
@@ -1,6 +1,6 @@
 import {makeRequest} from 'progressive-web-sdk/dist/utils/fetch-utils'
 import {urlToPathKey} from '../../../utils/utils'
-import {initDemandWareSession, requestHeaders} from '../app/commands'
+import {initDemandWareSession} from '../app/commands'
 import {receiveCategory} from '../../categories/responses'
 import {receiveProductListProductData} from '../../products/responses'
 import {parseProductListData} from '../parsers'
@@ -12,19 +12,19 @@ export const fetchProductListData = (url) => (dispatch) => {
     const categoryID = categoryIDMatch ? categoryIDMatch[1] : ''
     const urlPathKey = urlToPathKey(url)
     return initDemandWareSession()
-        .then(() => {
-            const options = {
+        .then((headers) => {
+            const requestOptions = {
                 method: 'GET',
-                headers: requestHeaders
+                headers
             }
-            makeRequest(`${API_END_POINT_URL}/categories/${categoryID}`, options)
+            makeRequest(`${API_END_POINT_URL}/categories/${categoryID}`, requestOptions)
                 .then((response) => response.json())
                 .then((responseJSON) => {
                     dispatch(receiveCategory({
                         [urlPathKey]: {title: responseJSON.name}
                     }))
                     if (responseJSON.parent_category_id !== 'root') {
-                        makeRequest(`${API_END_POINT_URL}/categories/${responseJSON.parent_category_id}`, options)
+                        makeRequest(`${API_END_POINT_URL}/categories/${responseJSON.parent_category_id}`, requestOptions)
                             .then((response) => response.json())
                             .then((responseJSON) => {
                                 dispatch(receiveCategory({
@@ -34,7 +34,7 @@ export const fetchProductListData = (url) => (dispatch) => {
                     }
                 })
                 .then(() => {
-                    makeRequest(`${API_END_POINT_URL}/product_search?expand=images,prices&q=&refine_1=cgid=${categoryID}`, options)
+                    makeRequest(`${API_END_POINT_URL}/product_search?expand=images,prices&q=&refine_1=cgid=${categoryID}`, requestOptions)
                         .then((response) => response.json())
                         .then(({hits}) => {
                             const productListData = parseProductListData(hits)
