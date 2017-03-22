a108569ce4a49778f5c2afeb877863b4c5bacedf
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-03-22 12:29:13 -0700

    Remove some straggling usages.

--- web/app/containers/footer/actions.js
+++ web/app/containers/footer/actions.js
@@ -1,18 +1,16 @@
+import {createAction} from 'progressive-web-sdk/dist/utils/action-creation'
 import * as parser from './parsers/parser'
 import * as constants from './constants'
-import * as utils from '../../utils/utils'
 import * as commands from '../../integration-manager/commands'
 
-export const receiveData = utils.createAction('Receive footer data')
+export const receiveData = createAction('Receive footer data')
 
 export const process = ({payload: {$, $response}}) => receiveData({
     newsletter: parser.parseNewsLetter($response),
     navigation: parser.parseNavigation($, $response)
 })
 
-export const newsletterSignupComplete = utils.createAction('Newsletter signup complete',
-    'signupStatus'
-)
+export const newsletterSignupComplete = createAction('Newsletter signup complete', ['signupStatus'])
 
 export const signUpToNewsletter = (data) => {
     return (dispatch) => {
