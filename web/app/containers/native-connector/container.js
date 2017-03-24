5a07942e497b4f905f309dcdb1d05e8e3982de5e
Kerr Marin Miller <kmarinmiller@mobify.com>
2017-03-23 17:08:26 -0700

    Remove console logs from native connector

--- web/app/containers/native-connector/container.js
+++ web/app/containers/native-connector/container.js
@@ -7,15 +7,12 @@ import {getCart} from '../../store/cart/actions'
 
 class NativeConnector extends React.Component {
     componentDidMount() {
-        console.log('Mounting native component, listening to cart:needs-update')
         onAstroEvent('cart:needs-update', () => {
-            console.log('cart:needs-update in NativeConnector')
             this.props.refreshCart()
         })
     }
 
     componentWillUnmount() {
-        console.log('Removing Astro event for cart:needs-update')
         disableAstroEvent('cart:needs-update')
     }
 
