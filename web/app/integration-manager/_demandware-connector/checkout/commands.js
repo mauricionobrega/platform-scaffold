8d7e0885cb399527f5435b9a17720af88dae919b
Jennifer Bandelin <jennifer@mobify.com>
2017-03-27 14:42:05 -0700

    Return makeRequest promise that fetches shipping methods

--- web/app/integration-manager/_demandware-connector/checkout/commands.js
+++ web/app/integration-manager/_demandware-connector/checkout/commands.js
@@ -9,28 +9,38 @@ import {noop} from 'progressive-web-sdk/dist/utils/utils'
 export const fetchShippingMethodsEstimate = () => (dispatch) => {
     return getBasketID()
         .then((basketID) => {
-            makeDemandwareRequest(`${API_END_POINT_URL}/baskets/${basketID}/shipments/me/shipping_methods`, {method: 'GET'})
-            .then((response) => response.json())
-            .then((responseJSON) => {
-                const initialValues = {
-                    shipping_method: responseJSON.default_shipping_method_id
-                }
-                const shippingMethods = responseJSON.applicable_shipping_methods.map((shippingMethod) => {
-                    return {
-                        label: `${shippingMethod.name} - ${shippingMethod.description}`,
-                        cost: `$${shippingMethod.price.toFixed(2)}`,
-                        value: shippingMethod.id
+            return makeDemandwareRequest(`${API_END_POINT_URL}/baskets/${basketID}/shipments/me/shipping_methods`, {method: 'GET'})
+                .then((response) => response.json())
+                .then((responseJSON) => {
+                    const initialValues = {
+                        shipping_method: responseJSON.default_shipping_method_id
                     }
-                })
+                    const shippingMethods = responseJSON.applicable_shipping_methods.map((shippingMethod) => {
+                        return {
+                            label: `${shippingMethod.name} - ${shippingMethod.description}`,
+                            cost: `$${shippingMethod.price.toFixed(2)}`,
+                            value: shippingMethod.id
+                        }
+                    })
 
-                dispatch(receiveCheckoutData({shipping: {shippingMethods}}))
-                return dispatch(receiveShippingMethodInitialValues({initialValues}))
-            })
+                    dispatch(receiveCheckoutData({shipping: {shippingMethods}}))
+                    return dispatch(receiveShippingMethodInitialValues({initialValues}))
+                })
         })
 }
 
 export const fetchCheckoutShippingData = () => (dispatch) => {
-    return dispatch(fetchShippingMethodsEstimate())
+    // return dispatch(fetchShippingMethodsEstimate())
+
+    return getBasketID()
+        .then((basketID) => {
+            return makeDemandwareRequest(`${API_END_POINT_URL}/baskets/${basketID}`, {method: 'GET'})
+                .then((response) => response.json())
+                .then((responseJSON) => {
+                    console.log(responseJSON) // What should we populate here? anything? skip this step maybe?
+                })
+                .then(() => dispatch(fetchShippingMethodsEstimate()))
+        })
 }
 
 export const submitShipping = noop
