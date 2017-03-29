1df5d37b1f55caf09be99af4019fbbcde283310c
Jennifer Bandelin <jennifer@mobify.com>
2017-03-29 13:19:56 -0700

    Use promise.resolve instead of constructing a promise

--- web/app/integration-manager/_demandware-connector/login/commands.js
+++ web/app/integration-manager/_demandware-connector/login/commands.js
@@ -9,31 +9,56 @@ import {SubmissionError} from 'redux-form'
 import {SIGN_IN_URL, API_END_POINT_URL, REQUEST_HEADERS} from '../constants'
 
 export const fetchLoginData = () => (dispatch) => {
-    return new Promise(() => {
-        return dispatch(receiveLoginPageData({
-            signinSection: {
-                heading: 'RETURNING CUSTOMERS',
-                description: 'If you are a registered user, please enter your email and password.',
-                requiredText: '* Required Fields',
-                isFormLoaded: true,
-                href: SIGN_IN_URL
-            },
-            registerSection: {
-                heading: 'NEW CUSTOMERS',
-                description: 'Creating an account is easy. Just fill out the form below and enjoy the benefits of being a registered customer.',
-                requiredText: '* Required Fields',
-                href: `${SIGN_IN_URL}/create`,
-                isFormLoaded: true,
-                form: {
-                    sections: [{
-                        heading: 'Personal Information',
-                    }, {
-                        heading: 'Sign-in Information',
-                    }]
-                }
+    return Promise.resolve(dispatch(receiveLoginPageData({
+        signinSection: {
+            heading: 'RETURNING CUSTOMERS',
+            description: 'If you are a registered user, please enter your email and password.',
+            requiredText: '* Required Fields',
+            isFormLoaded: true,
+            href: SIGN_IN_URL
+        },
+        registerSection: {
+            heading: 'NEW CUSTOMERS',
+            description: 'Creating an account is easy. Just fill out the form below and enjoy the benefits of being a registered customer.',
+            requiredText: '* Required Fields',
+            href: `${SIGN_IN_URL}/create`,
+            isFormLoaded: true,
+            form: {
+                sections: [{
+                    heading: 'Personal Information',
+                }, {
+                    heading: 'Sign-in Information',
+                }]
             }
-        }))
-    })
+        }
+    })))
+
+
+    // new Promise(() => {
+    //     return dispatch(receiveLoginPageData({
+    //         signinSection: {
+    //             heading: 'RETURNING CUSTOMERS',
+    //             description: 'If you are a registered user, please enter your email and password.',
+    //             requiredText: '* Required Fields',
+    //             isFormLoaded: true,
+    //             href: SIGN_IN_URL
+    //         },
+    //         registerSection: {
+    //             heading: 'NEW CUSTOMERS',
+    //             description: 'Creating an account is easy. Just fill out the form below and enjoy the benefits of being a registered customer.',
+    //             requiredText: '* Required Fields',
+    //             href: `${SIGN_IN_URL}/create`,
+    //             isFormLoaded: true,
+    //             form: {
+    //                 sections: [{
+    //                     heading: 'Personal Information',
+    //                 }, {
+    //                     heading: 'Sign-in Information',
+    //                 }]
+    //             }
+    //         }
+    //     }))
+    // })
 }
 
 export const navigateToSection = () => (dispatch) => noop()
