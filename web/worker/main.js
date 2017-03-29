0051e9e4e2172956837ae615fdeb660d81c893cf
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-03-29 15:54:02 -0700

    Conform the worker import to the final SDK version.

--- web/worker/main.js
+++ web/worker/main.js
@@ -3,10 +3,7 @@
 
 import worker from 'progressive-web-sdk/dist/worker/main'
 
-const CAPTURING_URL = 'https://cdn.mobify.com/capturejs/capture-latest.min.js'
-
 worker({
-    capturingURL: CAPTURING_URL,
     slug: PROJECT_SLUG,
-    debug: DEBUG
+    isDebug: DEBUG
 })
