10c16363493c755048b9a3c44eb881b9cf33ea86
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-03-29 16:49:29 -0700

    Typecheck the receiveProductDetailsUIData response.

--- web/app/integration-manager/products/responses.js
+++ web/app/integration-manager/products/responses.js
@@ -1,6 +1,41 @@
 import {createAction} from 'progressive-web-sdk/dist/utils/action-creation'
+import {createAction as createReduxAction} from 'redux-actions'
 
-export const receiveProductDetailsUIData = createAction('Receive Product Details UI data')
+import * as Runtypes from 'runtypes'
+
+const Breadcrumb = Runtypes.Record({
+    href: Runtypes.String,
+    text: Runtypes.String,
+    title: Runtypes.String
+})
+
+const ProductUIData = Runtypes.Record({
+    breadcrumbs: Runtypes.Array(Breadcrumb),
+    uenc: Runtypes.String,
+    formInfo: Runtypes.Always,
+    itemQuantity: Runtypes.Number,
+    ctaText: Runtypes.String
+})
+
+const typecheck = (type, value) => {
+    try {
+        type.check(value)
+    } catch (e) {
+        console.info(e)
+        console.log(value)
+    }
+    return value
+}
+
+const createTypedAction = (description, type) => createReduxAction(
+    description,
+    (payload) => typecheck(type, payload)
+)
+
+export const receiveProductDetailsUIData = createTypedAction(
+    'Receive Product Details UI data',
+    Runtypes.Dictionary(ProductUIData)
+)
 
 export const receiveProductDetailsProductData = createAction('Receive Product Details product data')
 
