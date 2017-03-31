5e088370e0da9039ea42b0bdc3f6846ef7e7b762
Jennifer Bandelin <jennifer@mobify.com>
2017-03-30 17:30:52 -0700

    Remove un-used params

--- web/app/integration-manager/_demandware-connector/login/commands.js
+++ web/app/integration-manager/_demandware-connector/login/commands.js
@@ -75,7 +75,7 @@ export const login = ({login}) => (dispatch) => {
         })
 }
 
-export const registerUser = (href, {firstname, lastname, email, password}, resolve, reject) => (dispatch) => {
+export const registerUser = ({firstname, lastname, email, password}, resolve, reject) => (dispatch) => {
     const requestOptions = {
         method: 'POST',
         body: JSON.stringify({
