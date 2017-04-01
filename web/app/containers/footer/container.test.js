3bd35c245800ce8616b4ea7e3cc0dfef2a5d2df6
Jeff Kamo <kamojeffrey@gmail.com>
2017-03-31 17:02:36 -0700

    Fix broken tests
    …and Stop passing isRunningInAstro to footer as a prop. Reference it
    directly, instead!

--- web/app/containers/footer/container.test.js
+++ web/app/containers/footer/container.test.js
@@ -1,23 +1,35 @@
 /* eslint-env jest */
+/* eslint-disable import/namespace */
+
 import {shallow} from 'enzyme'
 import React from 'react'
+import * as AstroIntegration from '../../utils/astro-integration'
 
 import Footer from './container'
 
 describe('The footer', () => {
+    const originalisRunningInAstro = AstroIntegration.isRunningInAstro
+
+    afterEach(() => {
+        // Value of `isRunningInAstro` might have been overwritten in test
+        AstroIntegration.isRunningInAstro = originalisRunningInAstro
+    })
+
     test('Should not render footer at all if running in an Astro app', () => {
-        const footer = shallow(<Footer isRunningInAstro={true} />)
+        AstroIntegration.isRunningInAstro = true
+        const footer = shallow(<Footer />)
         expect(footer.children().length).toBe(0)
     })
 
     test('Should render footer if not running in an Astro app', () => {
+        AstroIntegration.isRunningInAstro = false
         const footerData = {
             get: (key) => {
                 return {key}
             }
         }
 
-        const footer = shallow(<Footer isRunningInAstro={false} footer={footerData} />)
+        const footer = shallow(<Footer footer={footerData} />)
         expect(footer.children().length).not.toBe(0)
     })
 })
