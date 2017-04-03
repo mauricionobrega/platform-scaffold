6047ace566b48bf1de5bab671400887f260eba87
Cole Sanderson <cole@mobify.me>
2017-04-03 11:57:13 -0700

    PLATT-43: nearby widget to PDP

--- web/app/containers/product-details/container.js
+++ web/app/containers/product-details/container.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
 import ProductDetailsHeading from './partials/product-details-heading'
 import ProductDetailsCarousel from './partials/product-details-carousel'
 import ProductDetailsDescription from './partials/product-details-description'
 import ProductDetailsAddToCart from './partials/product-details-add-to-cart'
 import ProductDetailsItemAddedModal from './partials/product-details-item-added-modal'
+import ProductNearestStores from './partials/product-nearest-stores'
 
 const ProductDetails = ({route: {routeName}}) => {
     return (
@@ -14,6 +14,9 @@ const ProductDetails = ({route: {routeName}}) => {
             <ProductDetailsDescription />
             <ProductDetailsAddToCart />
             <ProductDetailsItemAddedModal />
+            <div className="u-padding-md u-bg-color-neutral-10">
+                <ProductNearestStores title="The Product is sold in the store" viewAllStoresText="Check all stores" class="u-margin-all" />
+            </div>
         </div>
     )
 }
