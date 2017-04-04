c1959b590c99c9a04d0031613bb2ee42f343a490
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-04 15:06:03 -0700

    No longer use the login heading from the store.

--- web/app/containers/login/partials/signin-panel.jsx
+++ web/app/containers/login/partials/signin-panel.jsx
@@ -1,23 +1,27 @@
 import React, {PropTypes} from 'react'
-import {createPropsSelector} from 'reselect-immutable-helpers'
 import {connect} from 'react-redux'
-import * as selectors from '../selectors'
 import * as actions from '../actions'
 
 import SignInForm from './signin-form'
-import {PanelHeading, PanelDescription, PanelRequiredText} from './common'
+import {PanelHeading} from './common'
 
-const SignInPanel = ({description, heading, requiredText, submitForm}) => (
+const signinDescription = 'If you have an account, sign in with your email address.'
+
+const requiredText = '* Required Fields'
+
+const SignInPanel = ({submitForm}) => (
     <div className="t-login__signin-panel">
         <div className="u-padding-start-md u-padding-end-md u-padding-top-lg u-padding-bottom-lg u-box-shadow">
             <div className="u-margin-bottom">
-                <PanelHeading heading={heading} />
+                <PanelHeading heading="Registered Customers" />
             </div>
 
-            <PanelDescription description={description} />
+            <p>
+                {signinDescription}
+            </p>
 
             <div className="u-margin-top">
-                <PanelRequiredText requiredText={requiredText} />
+                {requiredText}
             </div>
         </div>
 
@@ -28,20 +32,11 @@ const SignInPanel = ({description, heading, requiredText, submitForm}) => (
 )
 
 SignInPanel.propTypes = {
-    description: PropTypes.string,
-    heading: PropTypes.string,
-    requiredText: PropTypes.string,
     submitForm: PropTypes.func
 }
-
-const mapStateToProps = createPropsSelector({
-    description: selectors.signin.getDescription,
-    heading: selectors.signin.getHeading,
-    requiredText: selectors.signin.getRequiredText
-})
 
 const mapDispatchToProps = {
     submitForm: actions.submitSignInForm
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SignInPanel)
+export default connect(null, mapDispatchToProps)(SignInPanel)
