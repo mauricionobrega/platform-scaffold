892efe6d4f3a5e87d68ec3500281a541a89265d7
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-04 15:40:37 -0700

    Fix up the tests.

--- web/app/integration-manager/_merlins-connector/login/parsers/register.test.js
+++ web/app/integration-manager/_merlins-connector/login/parsers/register.test.js
@@ -9,14 +9,9 @@ describe('the Registration parser', () => {
 
     const headings = ['Personal Information', 'Sign-in Information']
 
-    test('extracts the href from the page', () => {
-        expect(isURL(parsedContent.href)).toBe(true)
-    })
-
     test('extracts forms from the page', () => {
         const form = parsedContent.form
         expect(isURL(form.href)).toBe(true)
-        expect(form.submitText).toBe('Create an Account')
         form.sections.forEach((section) => {
             expect(headings.includes(section.heading)).toBe(true)
             section.fields.forEach((field) => { // eslint-disable-line max-nested-callbacks
--- web/app/integration-manager/_merlins-connector/login/parsers/signin.test.js
+++ web/app/integration-manager/_merlins-connector/login/parsers/signin.test.js
@@ -7,30 +7,9 @@ describe('the Login parser', () => {
     const $content = jquerifyHtmlFile(`${__dirname}/signin-example.html`)
     const parsedContent = loginParser($, $content)
 
-    test('extracts the title from the page', () => {
-        expect(parsedContent.title).toBe('Customer Login')
-    })
-
-    test('extracts the heading from the page', () => {
-        expect(parsedContent.heading).toBe('Registered Customers')
-    })
-
-    test('extracts the description from the page', () => {
-        expect(typeof parsedContent.description).toBe('string')
-    })
-
-    test('extracts the href from the page', () => {
-        expect(isURL(parsedContent.href)).toBe(true)
-    })
-
-    test('extracts the required text from the page', () => {
-        expect(parsedContent.requiredText).toBe('* Required Fields')
-    })
-
     test('extracts form info from the page', () => {
         const form = parsedContent.form
         expect(isURL(form.href)).toBe(true)
-        expect(form.submitText).toBe('Login')
         expect(isURL(form.forgotPassword.href)).toBe(true)
         expect(form.forgotPassword.title).toBe('Forgot Your Password?')
         form.fields.forEach((field) => {
