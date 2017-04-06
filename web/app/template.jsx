024edc1d744522fc098c48c8091391d3fadc013d
gsaray <gsaray@mobify.com>
2017-04-06 12:01:34 -0700

    update to address feedback

--- web/app/template.jsx
+++ web/app/template.jsx
@@ -26,7 +26,7 @@ const template = (WrappedComponent) => {
             if (!route.suppressFetch) {
                 dispatch(fetchPage(url, WrappedComponent, route.routeName, route.fetchUrl))
             }
-            this.props.dispatch(removeAllNotifications())
+            dispatch(removeAllNotifications())
         }
 
         componentWillMount() {
