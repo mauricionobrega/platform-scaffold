4729a4387d230617f89a8ac94bac50f67ec7f399
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-10 16:30:06 -0700

    Wrap some container components in their own files.

--- web/app/containers/templates.js
+++ web/app/containers/templates.js
@@ -5,51 +5,55 @@ import Loadable from 'react-loadable'
 import template from '../template'
 
 // Don't split the Home container out from the main app, so that we can have instant page transitions
-import UnwrappedHome from './home/container'
-import UnwrappedOffline from './offline/container'
+import Home from './home/container'
+import Offline from './offline/container'
 import ContainerPlaceholder from '../components/container-placeholder'
 
-export const UnwrappedCart = Loadable({
-    loader: () => import('./cart/container'),
-    LoadingComponent: ContainerPlaceholder
-})
+// export const UnwrappedCart = Loadable({
+//     loader: () => import('./cart/container'),
+//     LoadingComponent: ContainerPlaceholder
+// })
 
-export const UnwrappedCheckoutConfirmation = Loadable({
-    loader: () => import('./checkout-confirmation/container'),
-    LoadingComponent: ContainerPlaceholder
-})
+// export const UnwrappedCheckoutConfirmation = Loadable({
+//     loader: () => import('./checkout-confirmation/container'),
+//     LoadingComponent: ContainerPlaceholder
+// })
 
-export const UnwrappedCheckoutPayment = Loadable({
-    loader: () => import('./checkout-payment/container'),
-    LoadingComponent: ContainerPlaceholder
-})
+// export const UnwrappedCheckoutPayment = Loadable({
+//     loader: () => import('./checkout-payment/container'),
+//     LoadingComponent: ContainerPlaceholder
+// })
 
-export const UnwrappedCheckoutShipping = Loadable({
-    loader: () => import('./checkout-shipping/container'),
-    LoadingComponent: ContainerPlaceholder
-})
+// export const UnwrappedCheckoutShipping = Loadable({
+//     loader: () => import('./checkout-shipping/container'),
+//     LoadingComponent: ContainerPlaceholder
+// })
 
-export const UnwrappedLogin = Loadable({
-    loader: () => import('./login/container'),
-    LoadingComponent: ContainerPlaceholder
-})
+// export const UnwrappedLogin = Loadable({
+//     loader: () => import('./login/container'),
+//     LoadingComponent: ContainerPlaceholder
+// })
 
-export const UnwrappedProductDetails = Loadable({
+export const ProductDetails = Loadable({
     loader: () => import('./product-details/container'),
     LoadingComponent: ContainerPlaceholder
 })
 
-export const UnwrappedProductList = Loadable({
+export const ProductList = Loadable({
     loader: () => import('./product-list/container'),
     LoadingComponent: ContainerPlaceholder
 })
+
+import UnwrappedCart from './cart/container'
+import UnwrappedCheckoutConfirmation from './checkout-confirmation/container'
+import UnwrappedCheckoutPayment from './checkout-payment/container'
+import UnwrappedCheckoutShipping from './checkout-shipping/container'
+import UnwrappedLogin from './login/container'
 
 export const Cart = template(UnwrappedCart)
 export const CheckoutConfirmation = template(UnwrappedCheckoutConfirmation)
 export const CheckoutPayment = template(UnwrappedCheckoutPayment)
 export const CheckoutShipping = template(UnwrappedCheckoutShipping)
-export const Home = template(UnwrappedHome)
 export const Login = template(UnwrappedLogin)
-export const ProductDetails = template(UnwrappedProductDetails)
-export const ProductList = template(UnwrappedProductList)
-export const Offline = template(UnwrappedOffline)
+
+export {Home, Offline}
