bf3db0d761d4ac9a98f79b492a3390d3c2824344
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-11 16:26:58 -0700

    Fix up form validation for add to cart.

--- web/app/containers/product-details/actions.js
+++ web/app/containers/product-details/actions.js
@@ -37,40 +37,39 @@ export const goToCheckout = () => (dispatch, getState) => {
     }
 }
 
+const submitCartFormSelector = createPropsSelector({
+    key: appSelectors.getCurrentPathKey,
+    qty: selectors.getItemQuantity,
+    variations: selectors.getProductVariationCategories
+})
 
 export const submitCartForm = (formValues) => (dispatch, getStore) => {
-    const currentState = getStore()
-    const key = appSelectors.getCurrentPathKey(currentState)
-    const qty = selectors.getItemQuantity(currentState)
-    const variations = selectors.getProductVariationCategories(currentState)
-    dispatch(addToCartStarted())
+    const {key, qty, variations} = submitCartFormSelector(getStore())
 
-    return new Promise((resolve, reject) => {
+    if (variations) {
         const errors = {}
-        if (variations) {
-            variations.toJS().forEach(({id, name}) => {
-                if (!formValues[id]) {
-                    errors[id] = `Please select a ${name}.`
-                }
-            })
+        variations.forEach(({id, label}) => {
+            if (!formValues[id]) {
+                errors[id] = `Please select a ${label}`
+            }
+        })
+        if (Object.keys(errors).length > 0) {
+            return Promise.reject(new SubmissionError(errors))
         }
-        if (Object.keys(errors).length) {
-            return reject(new SubmissionError(errors))
-        }
-        return resolve(true)
-    })
-    .then(() => dispatch(addToCart(key, qty)))
-    .then(() => dispatch(openModal(PRODUCT_DETAILS_ITEM_ADDED_MODAL)))
-    .catch((error) => {
-        console.error(`Error adding to cart: ${error}`)
-        return dispatch(addNotification({
-            content: 'Unable to add item to the cart.',
-            id: 'addToCartError',
-            showRemoveButton: true
-        }))
-    })
-    .then(() => dispatch(addToCartComplete()))
+    }
 
+    dispatch(addToCartStarted())
+    return dispatch(addToCart(key, qty))
+        .then(() => dispatch(openModal(PRODUCT_DETAILS_ITEM_ADDED_MODAL)))
+        .catch((error) => {
+            console.error(`Error adding to cart: ${error}`)
+            return dispatch(addNotification({
+                content: 'Unable to add item to the cart.',
+                id: 'addToCartError',
+                showRemoveButton: true
+            }))
+        })
+        .then(() => dispatch(addToCartComplete()))
 }
 
 const variationBlurSelector = createPropsSelector({
