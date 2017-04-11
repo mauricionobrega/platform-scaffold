88b83b99bc2e30affdfd9664d4977d304834f693
Jennifer Bandelin <jennifer@mobify.com>
2017-04-11 16:16:04 -0700

    Remove unnecessary function export

--- web/app/integration-manager/_demandware-connector/cart/commands.js
+++ web/app/integration-manager/_demandware-connector/cart/commands.js
@@ -27,7 +27,7 @@ export const createBasket = () => {
         /* eslint-enable camelcase */
 }
 
-export const getProductImage = (item, currentState) => {
+const getProductImage = (item, currentState) => {
     const productImage = getFirstProductImageByPathKey(getProductHref(item.product_id))(currentState)
 
     if (productImage) {
