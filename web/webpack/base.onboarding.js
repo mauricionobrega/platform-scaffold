c2cc975bfd1a05ee7d4bf3c8df3aeb79a0ede890
Jason Voll <jason@mobify.com>
2017-04-11 16:03:37 -0700

    webpack config fix for app

--- web/webpack/base.onboarding.js
+++ web/webpack/base.onboarding.js
@@ -19,7 +19,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
