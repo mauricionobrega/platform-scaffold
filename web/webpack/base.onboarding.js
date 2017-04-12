1d95a145dc9409829b34fbc4edddac328d39e936
Jason Voll <jason@mobify.com>
2017-04-12 11:08:25 -0700

    make JS/JSX regex in webpack config more explicit

--- web/webpack/base.onboarding.js
+++ web/webpack/base.onboarding.js
@@ -19,7 +19,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
+                test: /\.js(x?)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
