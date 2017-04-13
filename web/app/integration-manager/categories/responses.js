d7260674419ce065babd9f40bf8e2bbb4a483ab7
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-13 16:11:13 -0700

    Create a similar receiveCategoryContents response.

--- web/app/integration-manager/categories/responses.js
+++ web/app/integration-manager/categories/responses.js
@@ -1,11 +1,16 @@
 import {Dictionary} from 'runtypes'
 
 import {createTypedAction} from '../../utils/utils'
-import {CategoryInfo, Categories} from './types'
+import {CategoryInfo, CategoryContents, Categories} from './types'
 
 export const receiveCategoryInformation = createTypedAction(
     'Receive Category Information',
     Dictionary(CategoryInfo)
+)
+
+export const receiveCategoryContents = createTypedAction(
+    'Receive Category Contents',
+    Dictionary(CategoryContents)
 )
 
 export const receiveCategory = createTypedAction(
