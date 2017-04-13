8f44161c87aa3ffbdc469428ed01fa5f304e96c3
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-13 15:09:29 -0700

    Provide the data on the Demandware connector.

--- web/app/integration-manager/_demandware-connector/categories/commands.js
+++ web/app/integration-manager/_demandware-connector/categories/commands.js
@@ -6,6 +6,33 @@ import {parseProductListData} from '../parsers'
 
 import {API_END_POINT_URL, SITE_ID} from '../constants'
 
+const makeCategoryURL = (id) => `${API_END_POINT_URL}/categories/${id}`
+
+/* eslint-disable camelcase, no-use-before-define */
+const processCategory = (dispatch) => ({parent_category_id, id, name}) => {
+    const parentId = parent_category_id !== 'root' ? parent_category_id : null
+    const path = `/s/${SITE_ID}/${id}`
+    dispatch(receiveCategory({
+        [path]: {
+            id,
+            title: name,
+            href: path,
+            parentId
+        }
+    }))
+
+    if (parentId) {
+        dispatch(fetchParentCategoryData(parentId))
+    }
+}
+/* eslint-enable camelcase, no-use-before-define */
+
+const fetchParentCategoryData = (id) => (dispatch) => {
+    makeDemandwareRequest(makeCategoryURL(id), {method: 'GET'})
+        .then((response) => response.json())
+        .then(processCategory(dispatch))
+}
+
 export const fetchProductListData = (url) => (dispatch) => {
     const categoryIDMatch = /\/([^/]+)$/.exec(url)
     const categoryID = categoryIDMatch ? categoryIDMatch[1] : ''
@@ -14,32 +41,21 @@ export const fetchProductListData = (url) => (dispatch) => {
         method: 'GET'
     }
 
-    return makeDemandwareRequest(`${API_END_POINT_URL}/categories/${categoryID}`, requestOptions)
+    return makeDemandwareRequest(makeCategoryURL(categoryID), requestOptions)
         .then((response) => response.json())
-        .then((responseJSON) => {
-            dispatch(receiveCategory({
-                [urlPathKey]: {title: responseJSON.name}
-            }))
+        .then(processCategory(dispatch))
 
-            if (responseJSON.parent_category_id !== 'root') {
-                makeDemandwareRequest(`${API_END_POINT_URL}/categories/${responseJSON.parent_category_id}`, requestOptions)
-                    .then((response) => response.json())
-                    .then((responseJSON) => {
-                        dispatch(receiveCategory({
-                            [urlPathKey]: {parentName: responseJSON.name, parentHref: `/s/${SITE_ID}/${responseJSON.id}`}
-                        }))
-                    })
-            }
-        })
         .then(() => makeDemandwareRequest(`${API_END_POINT_URL}/product_search?expand=images,prices&q=&refine_1=cgid=${categoryID}`, requestOptions))
         .then((response) => response.json())
-        .then(({hits}) => {
+        .then(({hits, total}) => {
             const productListData = parseProductListData(hits)
+            const products = Object.keys(productListData)
 
             dispatch(receiveProductListProductData(productListData))
             dispatch(receiveCategory({
                 [urlPathKey]: {
-                    products: Object.keys(productListData)
+                    products,
+                    itemCount: total
                 }
             }))
         })
