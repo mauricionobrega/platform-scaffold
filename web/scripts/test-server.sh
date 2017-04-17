b06d2cb40f7fa6327a0970b64fdf0554085c44d0
Ellen Wong <ellen@mobify.com>
2017-04-17 13:22:52 -0700

    Use the local development cert from dev-server

--- web/scripts/test-server.sh
+++ web/scripts/test-server.sh
@@ -2,4 +2,6 @@
 # Start test server on build folder
 
 http-server --ssl --cors --p=8443 \
-	--key lighthouse/server.pem --cert lighthouse/server.pem build
+	--key dev-server/localhost.pem \
+	--cert dev-server/localhost.pem \
+	build
