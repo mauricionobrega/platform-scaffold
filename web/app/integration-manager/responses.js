8718325b3c05698bb7c9bfed81361d006aad666d
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-17 14:40:58 -0700

    Don't parse in the connector either.

--- web/app/integration-manager/responses.js
+++ web/app/integration-manager/responses.js
@@ -2,7 +2,6 @@ import {createAction} from 'progressive-web-sdk/dist/utils/action-creation'
 
 export const receiveHomeData = createAction('Receive Home Data')
 export const receiveNavigationData = createAction('Receive Navigation Data')
-export const receiveFooterData = createAction('Receive Footer Data')
 export const receiveCategory = createAction('Receive Category Data')
 export const setPageFetchError = createAction('Set page fetch error', ['fetchError'])
 export const receiveAppData = createAction('Receive App Data')
