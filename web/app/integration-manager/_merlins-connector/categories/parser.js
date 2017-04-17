bb8d99ff67c108449eaa8566622a7fb994aa9e22
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-17 09:47:09 -0700

    Convert the Merlin's connector to the new responses.

--- web/app/integration-manager/_merlins-connector/categories/parser.js
+++ web/app/integration-manager/_merlins-connector/categories/parser.js
@@ -3,8 +3,10 @@ import {urlToPathKey} from 'progressive-web-sdk/dist/utils/utils'
 
 /* eslint-disable newline-per-chained-call */
 
+export const parseCategoryTitle = ($, $html) => getTextFrom($html, '.page-title')
+
 const categoryProductsParser = ($, $html) => {
-    const $numItems = $html.find('#toolbar-amount .toolbar-number').first()
+    const $numItems = $html.find('#toolbar-amount .tolbar-number').first()
 
     const products = $
           .makeArray($html.find('.item.product-item'))
@@ -13,8 +15,7 @@ const categoryProductsParser = ($, $html) => {
 
     return {
         itemCount: $numItems.length > 0 ? parseInt($numItems.text(), 10) : 0,
-        products,
-        title: getTextFrom($html, '.page-title')
+        products
     }
 }
 
