1180f98aeb64db33bf732ac70d5d7c891bf4198c
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-17 15:35:45 -0700

    Fix typo and test.

--- web/app/integration-manager/_merlins-connector/categories/parser.js
+++ web/app/integration-manager/_merlins-connector/categories/parser.js
@@ -6,7 +6,7 @@ import {urlToPathKey} from 'progressive-web-sdk/dist/utils/utils'
 export const parseCategoryTitle = ($, $html) => getTextFrom($html, '.page-title')
 
 const categoryProductsParser = ($, $html) => {
-    const $numItems = $html.find('#toolbar-amount .tolbar-number').first()
+    const $numItems = $html.find('#toolbar-amount .toolbar-number').first()
 
     const products = $
           .makeArray($html.find('.item.product-item'))
