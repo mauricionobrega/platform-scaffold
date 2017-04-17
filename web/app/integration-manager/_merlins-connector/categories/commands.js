c546b9e9d1c31be5eeead5b73bfcfcfe096bbc7c
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-17 09:47:09 -0700

    Convert the Merlin's connector to the new responses.

--- web/app/integration-manager/_merlins-connector/categories/commands.js
+++ web/app/integration-manager/_merlins-connector/categories/commands.js
@@ -1,7 +1,7 @@
 import {urlToPathKey} from 'progressive-web-sdk/dist/utils/utils'
-import {receiveCategory} from '../../categories/responses'
+import {receiveCategoryContents, receiveCategoryInformation} from '../../categories/responses'
 import {receiveProductListProductData} from '../../products/responses'
-import categoryProductsParser from './parser'
+import categoryProductsParser, {parseCategoryTitle} from './parser'
 import {productListParser} from '../products/parsers'
 import {fetchPageData} from '../app/commands'
 
@@ -12,14 +12,13 @@ export const fetchProductListData = (url) => (dispatch) => {
             const pathKey = urlToPathKey(url)
             // Receive page contents
             dispatch(receiveProductListProductData(productListParser($, $response)))
-            dispatch(receiveCategory({
-                [pathKey]: {
-                    ...categoryProductsParser($, $response),
-                    // I can't really find a categoryId in the Merlin's HTML
-                    id: pathKey,
-                    href: pathKey,
-                    parentId: null
-                }
+            dispatch(receiveCategoryInformation(pathKey, {
+                // I can't really find a categoryId in the Merlin's HTML
+                id: pathKey,
+                href: pathKey,
+                parentId: null,
+                title: parseCategoryTitle($, $response)
             }))
+            dispatch(receiveCategoryContents(pathKey, categoryProductsParser($, $response)))
         })
 }
