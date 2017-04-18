3e7a5606dd34a99e9960bd8b2af77a20143aa92a
Jennifer Bandelin <jennifer@mobify.com>
2017-04-18 13:03:26 -0700

    Add util to construct DW order address object

--- web/app/integration-manager/_demandware-connector/checkout/utils.js
+++ web/app/integration-manager/_demandware-connector/checkout/utils.js
@@ -9,3 +9,32 @@ export const populateLocationsData = () => (dispatch) => {
         }
     }))
 }
+
+export const createOrderAddressObject = (formValues) => {
+    const {
+        name,
+        firstname,
+        lastname,
+        company,
+        addressLine1,
+        addressLine2,
+        countryId,
+        city,
+        regionId,
+        postcode,
+        telephone
+    } = formValues
+    return {
+        address1: addressLine1,
+        address2: addressLine2,
+        city,
+        country_code: countryId,
+        first_name: firstname,
+        last_name: lastname,
+        full_name: name,
+        phone: telephone,
+        postal_code: postcode,
+        state_code: regionId,
+        company_name: company
+    }
+}
