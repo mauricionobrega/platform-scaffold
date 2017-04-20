ca865ac028ef977d35f12f80b107fe49fec17ef2
Jeremy Wiebe <jeremy@mobify.com>
2017-04-20 11:36:48 -0700

    Some small cleanups

--- web/app/integration-manager/_merlins-connector/utils.js
+++ web/app/integration-manager/_merlins-connector/utils.js
@@ -2,8 +2,6 @@ import {makeFormEncodedRequest} from 'progressive-web-sdk/dist/utils/fetch-utils
 import {getCookieValue} from '../../utils/utils'
 
 export const submitForm = (url, data, options) => {
-    console.log('Submitting form to ', url, data, options)
-
     // The form_key is a session-level value. If there is
     // a form_key cookie, that trumps all!
     const formKey = getCookieValue('form_key')
