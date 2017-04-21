d2f286587036a4e54dc79c348c68394bb7159120
Jeff Kamo <kamojeffrey@gmail.com>
2017-04-21 14:43:34 -0700

    Add url query string to open filter modal

--- web/app/containers/product-list/partials/product-list-filter-modal.jsx
+++ web/app/containers/product-list/partials/product-list-filter-modal.jsx
@@ -2,7 +2,7 @@ import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
 import {createPropsSelector} from 'reselect-immutable-helpers'
 import {PRODUCT_LIST_FILTER_MODAL} from '../constants'
-import {closeModal} from '../../../store/modals/actions'
+import {closeModal, openModal} from '../../../store/modals/actions'
 import {changeFilterTo} from '../../../store/categories/actions'
 import {isModalOpen} from '../../../store/selectors'
 import * as selectors from '../selectors'
@@ -13,65 +13,79 @@ import Sheet from 'progressive-web-sdk/dist/components/sheet'
 import {HeaderBar, HeaderBarActions, HeaderBarTitle} from 'progressive-web-sdk/dist/components/header-bar'
 import IconLabelButton from '../../../components/icon-label-button'
 
-const ProductListFilterModal = ({closeModal, filters, isOpen, changeFilter}) => (
-    <Sheet
-        className="t-product-list__filter-modal"
-        open={isOpen}
-        onDismiss={closeModal}
-        maskOpacity={0.7}
-        effect="slide-right"
-        shrinkToContent={false}
-        coverage="85%"
-    >
-        <HeaderBar>
-            <HeaderBarTitle className="u-flex u-padding-start u-text-align-start">
-                <h1 className="u-h3 u-text-uppercase">
-                    <span className="u-text-extra-lighter">Filter Results By</span>
-                </h1>
-            </HeaderBarTitle>
+const FILTER_QUERY = 'filter_is_open'
 
-            <HeaderBarActions>
-                <IconLabelButton iconName="close" label="" onClick={closeModal}>Close</IconLabelButton>
-            </HeaderBarActions>
-        </HeaderBar>
+class ProductListFilterModal extends React.Component {
+    componentDidMount() {
+        if (window.location.search.indexOf(FILTER_QUERY) > 0) {
+            this.props.openModal()
+        }
+    }
 
-        <Accordion initialOpenItems={[0]}>
-            {filters.map(({label, ruleset, kinds}) =>
-                <AccordionItem header={label} key={ruleset} className="u-padding-0">
-                    {/* disabling a11y lints because the below handler is
-                        for the bubbled events from the children button elements */}
-                    {/* eslint-disable
-                        jsx-a11y/click-events-have-key-events,
-                        jsx-a11y/onclick-has-focus,
-                        jsx-a11y/onclick-has-role,
-                        jsx-a11y/no-static-element-interactions */}
-                    <div
-                        className="t-product-list__filter-modal-items"
-                        role="presentation"
-                        onClick={(e) => {
-                            changeFilter(e)
-                            closeModal()
-                        }}
-                    >
-                        {kinds.map(({count, label, query}) =>
-                            <Button
-                                key={query}
-                                className="c--link u-width-full"
-                                innerClassName="u-justify-start"
-                                id={query}
+    render() {
+        const {closeModal, filters, isOpen, changeFilter} = this.props
+
+        return (
+            <Sheet
+                className="t-product-list__filter-modal"
+                open={isOpen}
+                onDismiss={closeModal}
+                maskOpacity={0.7}
+                effect="slide-right"
+                shrinkToContent={false}
+                coverage="85%"
+            >
+                <HeaderBar>
+                    <HeaderBarTitle className="u-flex u-padding-start u-text-align-start">
+                        <h1 className="u-h3 u-text-uppercase">
+                            <span className="u-text-extra-lighter">Filter Results By</span>
+                        </h1>
+                    </HeaderBarTitle>
+
+                    <HeaderBarActions>
+                        <IconLabelButton iconName="close" label="" onClick={closeModal}>Close</IconLabelButton>
+                    </HeaderBarActions>
+                </HeaderBar>
+
+                <Accordion initialOpenItems={[0]}>
+                    {filters.map(({label, ruleset, kinds}) =>
+                        <AccordionItem header={label} key={ruleset} className="u-padding-0">
+                            {/* disabling a11y lints because the below handler is
+                                for the bubbled events from the children button elements */}
+                            {/* eslint-disable
+                                jsx-a11y/click-events-have-key-events,
+                                jsx-a11y/onclick-has-focus,
+                                jsx-a11y/onclick-has-role,
+                                jsx-a11y/no-static-element-interactions */}
+                            <div
+                                className="t-product-list__filter-modal-items"
+                                role="presentation"
+                                onClick={(e) => {
+                                    changeFilter(e)
+                                    closeModal()
+                                }}
                             >
-                                <div>
-                                    <span className="u-color-brand">{label}</span>
-                                    <span className="u-color-neutral-40">({count})</span>
-                                </div>
-                            </Button>
-                        )}
-                    </div>
-                </AccordionItem>
-            )}
-        </Accordion>
-    </Sheet>
-)
+                                {kinds.map(({count, label, query}) =>
+                                    <Button
+                                        key={query}
+                                        className="c--link u-width-full"
+                                        innerClassName="u-justify-start"
+                                        id={query}
+                                    >
+                                        <div>
+                                            <span className="u-color-brand">{label}</span>
+                                            <span className="u-color-neutral-40">({count})</span>
+                                        </div>
+                                    </Button>
+                                )}
+                            </div>
+                        </AccordionItem>
+                    )}
+                </Accordion>
+            </Sheet>
+        )
+    }
+}
 
 ProductListFilterModal.propTypes = {
     /**
@@ -80,7 +94,7 @@ ProductListFilterModal.propTypes = {
     changeFilter: PropTypes.func,
 
     /**
-     * A function used to set the navigation-sheet's state to closed
+     * A function used to set the filter sheet's state to closed
      */
     closeModal: PropTypes.func,
 
@@ -93,6 +107,11 @@ ProductListFilterModal.propTypes = {
      * Whether the modal is open or not
      */
     isOpen: PropTypes.bool,
+
+    /**
+     * A function used to set the filter sheet's state to open
+     */
+    openModal: PropTypes.func,
 }
 
 const mapStateToProps = createPropsSelector({
@@ -102,7 +121,8 @@ const mapStateToProps = createPropsSelector({
 
 const mapDispatchToProps = {
     changeFilter: (e) => changeFilterTo(e.target.closest('[id]').id),
-    closeModal: () => closeModal(PRODUCT_LIST_FILTER_MODAL)
+    closeModal: () => closeModal(PRODUCT_LIST_FILTER_MODAL),
+    openModal: () => openModal(PRODUCT_LIST_FILTER_MODAL)
 }
 
 export default connect(
