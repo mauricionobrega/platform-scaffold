b5fd02f827fc6e1f5770e1d51dc35f2821cb4051
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-26 12:02:59 -0700

    Remove unintended old code.

--- web/app/store/categories/actions.js
+++ web/app/store/categories/actions.js
@@ -1,10 +1,7 @@
 import {createAction} from 'progressive-web-sdk/dist/utils/action-creation'
-import {urlToPathKey} from 'progressive-web-sdk/dist/utils/utils'
-import productListParser from './parsers/product-list'
 import {getCurrentPathKey} from '../../containers/app/selectors'
 import {getSelectedCategory} from '../../containers/product-list/selectors'
 
-export const receiveCategory = createAction('Receive Category Data')
 export const changeSortOption = createAction('Change Sort Option')
 
 export const changeSort = (sortValue) => (dispatch, getStore) => {
@@ -15,12 +12,4 @@ export const changeSort = (sortValue) => (dispatch, getStore) => {
     })
     const currentCategory = {[getCurrentPathKey(currentState)]: categoryData}
     dispatch(changeSortOption(currentCategory))
-}
-
-export const process = ({payload}) => {
-    const {$, $response, url} = payload
-
-    return receiveCategory({
-        [urlToPathKey(url)]: productListParser($, $response)
-    })
 }
