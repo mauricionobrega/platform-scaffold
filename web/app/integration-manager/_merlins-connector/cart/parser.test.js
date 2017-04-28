504581f5f4339879191c2fff64c8e33d167df6d3
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-04-28 14:36:50 -0700

    Clear up naming.

--- web/app/integration-manager/_merlins-connector/cart/parser.test.js
+++ web/app/integration-manager/_merlins-connector/cart/parser.test.js
@@ -4,9 +4,9 @@ import parse from './parser'
 describe('Parsing the cart contents', () => {
 
     test('should be able to parse the server response from the cart endpoint', () => {
-        const responseStr = (require('./cart-contents-example.json'))
+        const responseJSON = (require('./cart-contents-example.json'))
         const parsed = require('./cart-contents-parsed.json')
-        expect(parse(JSON.stringify(responseStr))).toEqual(parsed)
+        expect(parse(JSON.stringify(responseJSON))).toEqual(parsed)
     })
 
 })
