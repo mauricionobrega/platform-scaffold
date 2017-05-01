9f217d80d1aa40b1aada644eafaafecb4aa5d750
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-05-01 11:24:18 -0700

    Fix sort logic to sort Immutable objects.

--- web/app/utils/sort-utils.js
+++ web/app/utils/sort-utils.js
@@ -4,8 +4,8 @@ export const sortLib = {
     // sort by name
     name: (a, b) => {
         // getting name of products
-        const nameA = a.title
-        const nameB = b.title
+        const nameA = a.get('title')
+        const nameB = b.get('title')
 
         if (nameA < nameB) {
             return -1
@@ -19,8 +19,8 @@ export const sortLib = {
     // sort by price
     price: (a, b) => {
         // replacing $ sign with empty string to compare the price
-        const priceA = parseFloat(a.price.replace(DOLLAR_SIGN, ''))
-        const priceB = parseFloat(b.price.replace(DOLLAR_SIGN, ''))
+        const priceA = parseFloat(a.get('price').replace(DOLLAR_SIGN, ''))
+        const priceB = parseFloat(b.get('price').replace(DOLLAR_SIGN, ''))
 
         return priceA - priceB
     },
