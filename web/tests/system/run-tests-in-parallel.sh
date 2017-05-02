140ecb93ebc6a488b9510287ce24a262f3b09aab
jasecode <jzhang@mobify.com>
2017-05-02 12:06:17 -0700

    moving unit tests to second node

--- web/tests/system/run-tests-in-parallel.sh
+++ web/tests/system/run-tests-in-parallel.sh
@@ -9,36 +9,37 @@ if [ $CIRCLE_NODE_TOTAL -eq 1 ]; then
   npm run lint
   npm test -- --runInBand
   npm run test:pwa-ci
+  ./tests/system/start-test-server.sh
   npm run test:e2e
 
 else
   #If the node total is greater than 1 assign the first node to lint. Divy up the remaining tests.
   if [ $CIRCLE_NODE_TOTAL -gt 1 ]; then
-    echo $CIRCLE_NODE_TOTAL 'Circle CI nodes. Running tests in parallel.'
+  	echo $CIRCLE_NODE_TOTAL 'Circle CI nodes. Running tests in parallel.'
     echo 'This is Circle CI node' $CIRCLE_NODE_INDEX'.'
 
 
     if [ $CIRCLE_NODE_INDEX -eq 0 ]; then
-	    echo 'Running Lint'
-	    npm run lint
-
-	    echo 'Running Unit Tests'
-	    npm test -- --runInBand
-
+		echo 'Running Lint'
+	  	npm run lint
 	    echo 'Running Lighthouse Test'
 	    npm run test:pwa-ci
   	fi
 
     # The other cirlce_node_index workbers should divide up the tests
-    if [ $CIRCLE_NODE_INDEX -ge 0 ]; then
-      ./tests/system/start-test-server.sh
-      i=0
-      for testfile in $(find ./tests/system/workflows/ -name '*.js'| sort); do
-        if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
-          echo 'Running test: ' ${testfile}
-          npm run test:e2e --test ${testfile}
-        fi
-        ((i=i+1))
+    if [ $CIRCLE_NODE_INDEX -gt 0 ]; then
+		echo 'Running Unit Tests'
+		npm test -- --runInBand
+
+		echo 'start-test-server'
+		./tests/system/start-test-server.sh
+		i=0
+		for testfile in $(find ./tests/system/workflows/ -name '*.js'| sort); do
+			if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
+		  echo 'Running test: ' ${testfile}
+		  npm run test:e2e --test ${testfile}
+		fi
+		((i=i+1))
       done
     fi
   fi
