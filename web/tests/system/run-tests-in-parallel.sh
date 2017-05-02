4c5f5b1469bd3a813fa2121eac92e15626f0841b
jasecode <jzhang@mobify.com>
2017-05-02 10:54:36 -0700

    reorganize lgic

--- web/tests/system/run-tests-in-parallel.sh
+++ web/tests/system/run-tests-in-parallel.sh
@@ -12,26 +12,26 @@ if [ $CIRCLE_NODE_TOTAL -eq 1 ]; then
   npm run test:e2e
 
 else
-  if [ $CIRCLE_NODE_INDEX -eq 0 ]; then
-    echo 'Running Lint'
-    npm run lint
-
-    echo 'Running Unit Tests'
-    npm test -- --runInBand
-
-    echo 'Running Lighthouse Test'
-    npm run test:pwa-ci
-  fi
-
   #If the node total is greater than 1 assign the first node to lint. Divy up the remaining tests.
   if [ $CIRCLE_NODE_TOTAL -gt 1 ]; then
     echo $CIRCLE_NODE_TOTAL 'Circle CI nodes. Running tests in parallel.'
     echo 'This is Circle CI node' $CIRCLE_NODE_INDEX'.'
-    ./tests/system/start-test-server.sh
 
+
+    if [ $CIRCLE_NODE_INDEX -eq 0 ]; then
+	    echo 'Running Lint'
+	    npm run lint
+
+	    echo 'Running Unit Tests'
+	    npm test -- --runInBand
+
+	    echo 'Running Lighthouse Test'
+	    npm run test:pwa-ci
+  	fi
 
     # The other cirlce_node_index workbers should divide up the tests
     if [ $CIRCLE_NODE_INDEX -ge 0 ]; then
+      ./tests/system/start-test-server.sh
       i=0
       for testfile in $(find ./tests/system/workflows/ -name '*.js'| sort); do
         if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
