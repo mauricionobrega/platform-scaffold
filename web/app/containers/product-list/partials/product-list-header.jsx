01202d79779255c0034942c5884abd34c718b6d1
Cole Sanderson <cole@mobify.me>
2017-05-04 16:16:16 -0700

    fix merge error

--- web/app/containers/product-list/partials/product-list-header.jsx
+++ web/app/containers/product-list/partials/product-list-header.jsx
@@ -8,16 +8,9 @@ import Link from 'progressive-web-sdk/dist/components/link'
 import SkeletonText from 'progressive-web-sdk/dist/components/skeleton-text'
 import Image from 'progressive-web-sdk/dist/components/image'
 
-import {isRunningInAstro} from '../../../utils/astro-integration'
-
 const ProductListHeader = ({title, contentsLoaded}) => (
     <div className="u-flexbox u-align-bottom">
         <div className="u-flex u-padding-top-lg u-padding-bottom-lg u-padding-start-md">
-            {!isRunningInAstro &&
-                <div className="t-product-list__breadcrumb">
-                    <Link href="/" className="u-text-size-small">Home</Link>
-                </div>
-            }
             <div className="t-product-list__breadcrumb">
                 <Link href="/" className="u-text-small">Home</Link>
             </div>
