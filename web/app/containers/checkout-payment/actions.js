703225ee6e0b6586cc3dc1fd938135bf5a89470b
Jeff Kamo <kamojeffrey@gmail.com>
2017-05-03 18:10:33 -0700

    Fix broken tests

--- web/app/containers/checkout-payment/actions.js
+++ web/app/containers/checkout-payment/actions.js
@@ -1,6 +1,8 @@
 import {browserHistory} from 'progressive-web-sdk/dist/routing'
 import {createAction} from '../../utils/utils'
-import {makeJsonEncodedRequest} from 'progressive-web-sdk/dist/utils/fetch-utils'
+import {makeRequest, makeJsonEncodedRequest} from 'progressive-web-sdk/dist/utils/fetch-utils'
+import {jqueryResponse} from 'progressive-web-sdk/dist/jquery-response'
+import checkoutPaymentParser from './checkout-payment-parser'
 import {getPaymentBillingFormValues} from '../../store/form/selectors'
 import {getCustomerEntityID, getEmailAddress} from '../../store/checkout/selectors'
 import {getShippingAddress} from '../../store/checkout/shipping/selectors'
@@ -13,6 +15,23 @@ export const toggleCardInputRadio = createAction('Toggled the card method radio 
 export const toggleCompanyAptField = createAction('Showing the "Company" and "Apt #" fields', 'isCompanyOrAptShown')
 export const toggleNewAddressFields = createAction('Toggled new address fields', 'newShippingAddressIsEnabled')
 export const setCvvType = createAction('Setting CVV type', 'cvvType')
+
+export const receiveResponse = (response) => {
+    return (dispatch) => {
+        return jqueryResponse(response)
+            .then(([$, $responseText]) => {
+                dispatch(receiveContents(checkoutPaymentParser($, $responseText)))
+            })
+    }
+}
+
+export const fetchContents = () => {
+    return (dispatch) => {
+        return makeRequest(window.location.href)
+            .then((response) => dispatch(receiveResponse(response)))
+    }
+}
+
 
 export const submitPayment = () => {
     return (dispatch, getState) => {
