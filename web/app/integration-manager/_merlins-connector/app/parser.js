ff0d2a8e4aa3cbbf56ecd78c90f913745efd31ec
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-05-10 13:32:44 -0700

    Switch to a simpler way of parsing the logged in flag.

--- web/app/integration-manager/_merlins-connector/app/parser.js
+++ web/app/integration-manager/_merlins-connector/app/parser.js
@@ -1,11 +1,11 @@
 import {getCheckoutConfigObject} from '../../../utils/magento-utils'
 
-export const appParser = ($html) => {
-    let isLoggedIn = !!$html.find('.customer-welcome').length
-    if (!isLoggedIn) {
-        // We may be on a checkout page so check the checkout config object
-        const config = getCheckoutConfigObject($html)
-        isLoggedIn = (config && config.customerData) ? config.customerData.constructor !== Array : isLoggedIn
+export const parseLoginStatus = ($html) => {
+    const welcomePresent = !!$html.find('.customer-welcome').length
+    if (welcomePresent) {
+        return true
     }
-    return {isLoggedIn}
+    // We may be on a checkout page so check the checkout config object
+    const config = getCheckoutConfigObject($html)
+    return (config && config.customerData) ? config.customerData.constructor !== Array : false
 }
