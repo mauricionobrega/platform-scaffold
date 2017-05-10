614cc8667587d23f445fe9c940d88b5d548f206e
Cdok <cdokurno@mobify.com>
2017-05-10 13:12:48 -0700

    fix to master product variations prepopulating

--- web/app/integration-manager/_demandware-connector/parsers.js
+++ web/app/integration-manager/_demandware-connector/parsers.js
@@ -22,9 +22,19 @@ const parseVariationCategories = (variation_attributes) => {
     }))
 }
 
-const getInitialVariantValues = (variants, id) => {
+const setInitialVariantValues = (variants, id, variationCategories) => {
     const currentVariant = variants.find(({product_id}) => product_id === id)
-    return currentVariant && currentVariant.variation_values
+
+    if (currentVariant) {
+        return currentVariant.variation_values
+    }
+
+    const defaultVariant = {}
+    variationCategories.forEach(({id, values}) => {
+        defaultVariant[id] = values[0].value
+    })
+
+    return defaultVariant
 }
 
 /* eslint-enable camelcase */
@@ -40,7 +50,7 @@ export const parseProductDetails = ({id, name, price, long_description, image_gr
         description: long_description,
         thumbnail: images[0],
         images,
-        initialValues: getInitialVariantValues(variants, id),
+        initialValues: setInitialVariantValues(variants, id, variation_attributes),
         variationCategories: parseVariationCategories(variation_attributes),
         variants: variants.map(({product_id, variation_values}) => {
             return {
@@ -60,13 +70,21 @@ export const getCurrentProductID = (url) => {
     }
 
     if (!productID) {
-    // Cart edit style: https://.../checkout/cart/configure/id/{basket_id}/product_id/{product_id}/
+        // Cart edit style: https://.../checkout/cart/configure/id/{basket_id}/product_id/{product_id}/
         productIDMatch = /product_id\/(\d+)/.exec(url)
         productID = productIDMatch ? productIDMatch[1] : ''
     }
 
     console.log('[getCurrentProductID]', productID)
     return productID
+}
+
+export const getInitialSelectedVariant = (variants, initialValues) => {
+    return variants.find(({values}) => {
+        return Object.keys(values).every((key) => {
+            return values[key] === initialValues[key]
+        })
+    })
 }
 
 export const parseCategories = (categories) => {
--- web/app/integration-manager/_demandware-connector/products/commands.js
+++ web/app/integration-manager/_demandware-connector/products/commands.js
@@ -2,12 +2,13 @@ import {receiveProductDetailsProductData, receiveProductDetailsUIData} from '../
 import {setCurrentURL} from '../../results'
 import {urlToPathKey} from 'progressive-web-sdk/dist/utils/utils'
 import {makeDemandwareRequest} from '../utils'
-import {parseProductDetails, getCurrentProductID, getProductHref} from '../parsers'
+import {parseProductDetails, getCurrentProductID, getProductHref, getInitialSelectedVariant} from '../parsers'
 import {API_END_POINT_URL} from '../constants'
 
 export const fetchPdpData = (url) => (dispatch) => {
     const productURL = `${API_END_POINT_URL}/products/${getCurrentProductID(url)}?expand=prices,images,variations`
     const productPathKey = urlToPathKey(url)
+
     const options = {
         method: 'GET'
     }
@@ -18,28 +19,41 @@ export const fetchPdpData = (url) => (dispatch) => {
                 ...parseProductDetails(responseJSON),
                 href: productPathKey
             }
+
             const productDetailsMap = {
                 [productPathKey]: productDetailsData
             }
             productDetailsData.variants.forEach(({id}) => {
                 productDetailsMap[getProductHref(id)] = productDetailsData
             })
+
             dispatch(receiveProductDetailsProductData(productDetailsMap))
             dispatch(receiveProductDetailsUIData({[productPathKey]: {itemQuantity: responseJSON.step_quantity}}))
+
+            // since the pathname will always be master, the productHref will
+            // only === pathname when landing on master page
+            if (getProductHref(productDetailsData.id) === window.location.pathname) {
+                const {variants, initialValues} = productDetailsData
+                const defaultVariant = getInitialSelectedVariant(variants, initialValues)
+                const currentProductHref = defaultVariant.id
+
+                dispatch(setCurrentURL(getProductHref(currentProductHref)))
+                dispatch(fetchPdpData(getProductHref(currentProductHref)))
+            }
         })
 }
 
 export const getProductVariantData = (selections, variants, categoryIds) => (dispatch) => {
     if (categoryIds.some((id) => !selections[id])) {
-        return
+        return Promise.resolve()
     }
 
     for (const {values, id} of variants) {
         if (categoryIds.every((id) => selections[id] === values[id])) {
             const currentProductHref = getProductHref(id)
             dispatch(setCurrentURL(currentProductHref))
-            dispatch(fetchPdpData(currentProductHref))
-            return
+
+            return dispatch(fetchPdpData(currentProductHref))
         }
     }
 }
