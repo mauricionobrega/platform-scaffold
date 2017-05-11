2870d21a74c82d9ef10bed4178fa03c7d7d345fb
Jeremy Wiebe <jeremy@mobify.com>
2017-05-11 07:46:44 -0700

    Remove dead code

--- web/app/containers/checkout-confirmation/parsers/customer-address.js
+++
@@ -1,7 +0,0 @@
-const customerAddressParser = ($, $html) => {
-    return {
-        form_key: $html.find('.form-address-edit [name="form_key"]').val(),
-    }
-}
-
-export default customerAddressParser
