b2e81b499a201a21cdad39cad480605f2db7e42c
Jeremy Wiebe <jeremy@mobify.com>
2017-05-18 08:20:12 -0700

    Tell Chrome to ignore cert errors on CI Lighthouse tests

--- web/scripts/lighthouse-ci.sh
+++ web/scripts/lighthouse-ci.sh
@@ -10,19 +10,19 @@ PREVIEW=#mobify-override\&mobify-path=true\&mobify-url=https://localhost:8443/lo
 
 trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
 
-# Lighthouse uses your local installation of Chrome, which should be at least 
-# version 54.0. Use a custom user agent containing "MobifyPreview" so that 
+# Lighthouse uses your local installation of Chrome, which should be at least
+# version 54.0. Use a custom user agent containing "MobifyPreview" so that
 # Preview will accept our requests, and disable device emulation so that the
-# "MobifyPreview" user agent does not get overridden. 
+# "MobifyPreview" user agent does not get overridden.
 
-# Finally, parse the HTML report for the Lighthouse score. 
+# Finally, parse the HTML report for the Lighthouse score.
 # CI will fail the build if the score is below a threshold.
 # See min_lighthouse_score in package.json
 
 sudo apt-get install libnss3-tools
 # Initialize database of certificates
 mkdir -p $HOME/.pki/nssdb
-# Pass in a password 
+# Pass in a password
 certutil -d $HOME/.pki/nssdb -N --empty-password
 # Add self-signed SSL certificate
 certutil -d sql:$HOME/.pki/nssdb -A -t "P,," -n lighthouse/server.pem -i lighthouse/server.pem
@@ -37,6 +37,7 @@ lighthouse \
 	--output=html \
 	--output-path=${OUTPUT_PATH} \
 	--disable-device-emulation=true \
+    --chrome-flags "--ignore-certificate-errors"
 	"${URL}${PREVIEW}"
 
 node ./lighthouse/check-score.js
