62fce1a2c416aaf6c78eea8c17215f5f21a7ff9b
Oliver Brook <olibrook@gmail.com>
2017-05-18 15:33:25 -0700

    Quick notes on how to deploy

---
+++ amp/scripts/deploy.js
@@ -0,0 +1,15 @@
+/**
+ * Deploy a built package as a Lambda function.
+ *
+ * See the JS SDK here:
+ * http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#createFunction-property
+ */
+const deploy = (zipFile, environment) => {
+
+}
+
+
+/**
+ * Make changes on the API gateway to expose the lambda function.
+ */
+const configureAPI = () => {}
