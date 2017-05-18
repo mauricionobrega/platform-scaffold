f5b0944cdced3b41555e8fe260ef328bb9e5c727
Richard Sexton <rsexton404@gmail.com>
2017-05-18 15:16:51 -0700

    Further gutting of the reducer

--- web/app/containers/log-me-in/reducer.js
+++ web/app/containers/log-me-in/reducer.js
@@ -27,61 +27,6 @@ const signinFields = [
     },
 ]
 
-const registerPersonalFields = [
-    {
-        label: 'First Name',
-        name: 'firstname',
-        type: 'text',
-        required: true,
-        tooltip: false,
-    },
-    {
-        label: 'Last Name',
-        name: 'lastname',
-        type: 'text',
-        required: true,
-        tooltip: false,
-    },
-    {
-        label: 'Email',
-        name: 'email',
-        type: 'email',
-        required: true,
-        tooltip: false,
-    },
-    {
-        label: 'Sign Up for Newsletter',
-        name: 'is_subscribed',
-        type: 'checkbox',
-        required: false,
-        tooltip: false,
-    },
-]
-
-const registerSigninFields = [
-    {
-        label: 'Password',
-        name: 'password',
-        type: 'password',
-        required: true,
-        tooltip: false,
-    },
-    {
-        label: 'Confirm Password',
-        name: 'password_confirmation',
-        type: 'password',
-        required: true,
-        tooltip: false,
-    },
-    {
-        label: 'Remember Me',
-        name: 'persistent_remember_me',
-        type: 'checkbox',
-        required: false,
-        tooltip: false,
-    },
-]
-
 const initialState = Immutable.fromJS({
     title: 'Customer Login',
     signinSection: {
