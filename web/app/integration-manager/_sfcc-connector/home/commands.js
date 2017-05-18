8b1ba0c36f21dfbf6ebc5b8050210bb54a4fe634
Daniel Brook-Roberge <dbrookroberge@mobify.com>
2017-05-18 15:33:42 -0700

    Miscellaneous cleanups.

--- web/app/integration-manager/_sfcc-connector/home/commands.js
+++ web/app/integration-manager/_sfcc-connector/home/commands.js
@@ -7,7 +7,5 @@ import {receiveHomeData} from '../../results'
 export const initHomePage = () => (dispatch) => {
     // Banners are being pulled from the bundle right now
     // so we just need an array with the correct number of objects
-    return new Promise(() => {
-        return dispatch(receiveHomeData({banners: [{}, {}, {}]}))
-    })
+    return Promise.resolve(dispatch(receiveHomeData({banners: [{}, {}, {}]})))
 }
