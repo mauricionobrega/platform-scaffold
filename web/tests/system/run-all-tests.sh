5fe784a6ce2acc201d73d72c40b61cfe73ff2a85
jasecode <jzhang@mobify.com>
2017-05-19 11:34:56 -0700

    fix bash indentation

--- web/tests/system/run-all-tests.sh
+++ web/tests/system/run-all-tests.sh
@@ -30,19 +30,19 @@ else
       echo 'Running Lint'
       npm run lint
     
-    echo 'Running Unit Tests'
-    npm test -- --runInBand
+      echo 'Running Unit Tests'
+      npm test -- --runInBand
     
-    echo 'Running End to End Tests'
-    #If we have nodes > 2, it will be part of the division to run another test:e2e
-    i=0
-    for testfile in $(find ./tests/system/workflows/ -name '*.js'| sort); do
-      if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
-        echo 'Running test: ' ${testfile}
-        npm run test:e2e --test ${testfile}
-      fi
-      ((i=i+1))
-    done
+      echo 'Running End to End Tests'
+      #If we have nodes > 2, it will be part of the division to run another test:e2e
+      i=0
+      for testfile in $(find ./tests/system/workflows/ -name '*.js'| sort); do
+        if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
+          echo 'Running test: ' ${testfile}
+          npm run test:e2e --test ${testfile}
+        fi
+        ((i=i+1))
+      done
     fi
   fi
 fi
