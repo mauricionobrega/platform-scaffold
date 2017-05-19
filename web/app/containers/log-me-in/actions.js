9863524cd4e7e54744bc00288f8870a48a951002
Richard Sexton <rsexton404@gmail.com>
2017-05-18 17:01:17 -0700

    Pared down the actions

--- web/app/containers/log-me-in/actions.js
+++ web/app/containers/log-me-in/actions.js
@@ -1,100 +1,31 @@
 import {jqueryResponse} from 'progressive-web-sdk/dist/jquery-response'
 import {createAction} from '../../utils/utils'
 import {makeFormEncodedRequest} from 'progressive-web-sdk/dist/utils/fetch-utils'
-import isEmail from 'validator/lib/isEmail'
-import {SubmissionError} from 'redux-form'
 import {getLogin} from './selectors'
-import {SIGN_IN_SECTION} from './constants'
-
-import {isFormResponseInvalid} from './parsers/common'
 import signinParser from './parsers/signin'
-
-import {isRunningInAstro, jsRpcMethod} from '../../utils/astro-integration'
 
 export const receiveData = createAction('Receive Login Data')
 
-export const process = ({payload: {$, $response, routeName}}) => {
-    if (routeName === SIGN_IN_SECTION) {
-        return receiveData({
-            signinSection: signinParser($, $response)
-        })
-    }
-    // This shouldn't happen, but just in case...
-    return receiveData()
+export const process = ({payload: {$, $response}}) => {
+    return receiveData({
+        signinSection: signinParser($, $response)
+    })
 }
 
-const validateSignInForm = (formValues) => {
-    const errors = {
-        login: {}
-    }
-    if (!formValues.login) {
-        return {
-            _error: 'Please fill in the form'
-        }
-    }
-    const email = formValues.login.username
-    if (!email) {
-        errors.login.username = 'Email address is required'
-    } else if (!isEmail(email)) {
-        errors.login.username = 'Email address is invalid'
-    }
-    const password = formValues.login.password
-    if (!password) {
-        errors.login.password = 'Password is required'
-    }
-    return errors
-}
-
-const sendForm = (href, formValues, formSelector, resolve, reject) => {
-    return makeFormEncodedRequest(href, formValues, {method: 'POST'})
-        .then(jqueryResponse)
-        .then((res) => {
-            const [$, $response] = res // eslint-disable-line no-unused-vars
-            if (isFormResponseInvalid($response, formSelector)) {
-                const error = {
-                    _error: 'Username or password is incorrect'
-                }
-                return reject(new SubmissionError(error))
-            }
-            if (isRunningInAstro) {
-                jsRpcMethod('user:loggedIn', [])()
-            }
-            window.location.href = '/customer/account'
-            return resolve(true)
-        })
-        .catch((error) => {
-            if (error.name !== SubmissionError) {
-                reject(new SubmissionError({_error: 'Failed to login due to network error.'}))
-            }
-        })
-}
-
-export const submitSignInForm = (formValues, resolve, reject) => {
+export const submitSignInForm = (formValues, resolve) => {
     return (dispatch, getStore) => {
-        const errors = validateSignInForm(formValues)
-        if (errors._error || Object.keys(errors.login).length) {
-            return reject(new SubmissionError(errors))
-        }
         const loginData = getLogin(getStore()).toJS()
         const {href, hiddenInputs} = loginData.signinSection.form
 
         hiddenInputs.forEach((input) => {
             formValues[input.name] = input.value
         })
-        return sendForm(href, formValues, '.form-login', resolve, reject)
-    }
-}
-
-const findPathForRoute = (routes, routeName) => {
-    const path = routes[0].childRoutes.find((route) => route.routeName === routeName).path
-    return `/${path}`
-}
-
-/**
- * Uses React router to navigate between different pages. Takes care of browser history, etc.
- */
-export const navigateToSection = (router, routes, sectionName) => {
-    return () => {
-        router.push(findPathForRoute(routes, sectionName))
+        return makeFormEncodedRequest(href, formValues, {method: 'POST'})
+            .then(jqueryResponse)
+            .then((res) => {
+                const [$, $response] = res // eslint-disable-line no-unused-vars
+                window.location.href = '/customer/account'
+                return resolve(true)
+            })
     }
 }
