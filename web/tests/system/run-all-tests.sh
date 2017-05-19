569d5d81f5e4a85b4a62841d02285a2c55fe00c0
Jason Zhang <jzhang@mobify.com>
2017-05-19 11:01:00 -0700

    Update comments

--- web/tests/system/run-all-tests.sh
+++ web/tests/system/run-all-tests.sh
@@ -2,6 +2,7 @@
 set -o pipefail
 set -o nounset
 
+#If the node total is 1, run all the tests sequentially. 
 if [ $CIRCLE_NODE_TOTAL -eq 1 ]; then
   echo 'Running lint'
   npm run lint
@@ -13,7 +14,7 @@ if [ $CIRCLE_NODE_TOTAL -eq 1 ]; then
   npm run test:e2e
 
 else
-  #If the node total is greater than 1 assign the first node to lint. Divy up the remaining tests.
+  #If the node total is greater than 1 assign the first node to running lighthouse. Divy up the remaining tests.
   if [ $CIRCLE_NODE_TOTAL -gt 1 ]; then
     echo $CIRCLE_NODE_TOTAL 'Circle CI nodes. Running tests in parallel.'
     echo 'This is Circle CI node' $CIRCLE_NODE_INDEX'.'
@@ -24,14 +25,16 @@ else
       npm run test:pwa-ci
     fi
 
-    # The other cirlce_node_index workers should divide up the tests
+    # The other cirlce_node_index worker will run the rest of the tests
     if [ $CIRCLE_NODE_INDEX -gt 0 ]; then
       echo 'Running Lint'
       npm run lint
     
     echo 'Running Unit Tests'
     npm test -- --runInBand
-
+    
+    #If there are ever more than 2 nodes, node 3 will be part of the division to run another test:e2e
+    echo 'Running End to End Tests"
     i=0
     for testfile in $(find ./tests/system/workflows/ -name '*.js'| sort); do
       if [ $(expr $i % $(expr $CIRCLE_NODE_TOTAL - 1)) -eq $(expr $CIRCLE_NODE_INDEX - 1) ]; then
