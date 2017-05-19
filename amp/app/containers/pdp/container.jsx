0058931466fc43e5f0b2c2e585a08dae076c3d03
Karl Schmidt <karl@mobify.com>
2017-05-19 16:35:00 -0700

    Refactor out the page component since there isn’t really anything like that in the PWA scaffold

--- amp/app/containers/pdp/container.jsx
+++ amp/app/containers/pdp/container.jsx
@@ -1,22 +1,43 @@
-import Page from '../../components/page/'
+import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
+import AmpImage from 'mobify-amp-sdk/dist/components/amp-image'
+import AmpLightbox from '../../components/amp-lightbox'
 
-const containerClass = 't-pdp'
+const containerClass = 't-home'
+
+const PDP = ({links, title}) => {
+    return (
+        <div className={containerClass}>
+            <div dangerouslySetInnerHTML={{__html: '<button on="tap:my-lightbox">Open lightbox</button>'}} />
+            <AmpLightbox id="my-lightbox">
+                <amp-img src="https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif" width="500" height="500" />
+            </AmpLightbox>
+            <AmpImage src="/static/mobify.png" width="252" height="64" layout="fixed" />
+
+            <h1>{title}</h1>
+            {links.map((linkText, i) => <p key={i}>{ linkText }</p>)}
+        </div>
+    )
+}
+
+PDP.propTypes = {
+    /**
+     * An array of links
+     */
+    links: PropTypes.array,
+    /**
+     * A title
+     */
+    title: PropTypes.string
+}
 
 const mapStateToProps = (state) => ({
     links: state.links,
-    title: `PDP! - ${state.title}` || '',
+    title: `Home! - ${state.title}` || '',
     className: containerClass
 })
 
 
-const mapDispatchToProps = {}
-
-
-const PDP = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Page)
-
-
-export default PDP
+export default connect(
+    mapStateToProps
+)(PDP)
--- amp/app/containers/plp/container.jsx
+++ amp/app/containers/plp/container.jsx
@@ -1,22 +1,43 @@
-import Page from '../../components/page/'
+import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
+import AmpImage from 'mobify-amp-sdk/dist/components/amp-image'
+import AmpLightbox from '../../components/amp-lightbox'
 
-const containerClass = 't-plp'
+const containerClass = 't-home'
+
+const PLP = ({links, title}) => {
+    return (
+        <div className={containerClass}>
+            <div dangerouslySetInnerHTML={{__html: '<button on="tap:my-lightbox">Open lightbox</button>'}} />
+            <AmpLightbox id="my-lightbox">
+                <amp-img src="https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif" width="500" height="500" />
+            </AmpLightbox>
+            <AmpImage src="/static/mobify.png" width="252" height="64" layout="fixed" />
+
+            <h1>{title}</h1>
+            {links.map((linkText, i) => <p key={i}>{ linkText }</p>)}
+        </div>
+    )
+}
+
+PLP.propTypes = {
+    /**
+     * An array of links
+     */
+    links: PropTypes.array,
+    /**
+     * A title
+     */
+    title: PropTypes.string
+}
 
 const mapStateToProps = (state) => ({
     links: state.links,
-    title: `PLP! - ${state.title}` || '',
+    title: `Home! - ${state.title}` || '',
     className: containerClass
 })
 
 
-const mapDispatchToProps = {}
-
-
-const PLP = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Page)
-
-
-export default PLP
+export default connect(
+    mapStateToProps
+)(PLP)
