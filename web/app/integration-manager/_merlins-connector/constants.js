18908b1adf780736caa9887ffdd974330660d7f1
Cdok <cdokurno@mobify.com>
2017-05-24 11:46:36 -0700

    formatted / styled merlins constants

--- web/app/integration-manager/_merlins-connector/constants.js
+++ web/app/integration-manager/_merlins-connector/constants.js
@@ -2,15 +2,15 @@
 /* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 
+export const CHECKOUT_SHIPPING_URL = '/checkout/'
+export const CART_URL = '/checkout/cart/'
+export const PAYMENT_URL = '/checkout/payment/'
+export const CREATE_ACCOUNT_POST_URL = '/customer/account/createpost/'
+export const LOGIN_POST_URL = '/customer/account/loginPost/'
+
 // configuration is not currently used by the Merlin's connector
 let config = {} // eslint-disable-line
 
 export const registerConfig = (cfg) => {
     config = cfg
 }
-
-export const CHECKOUT_SHIPPING_URL = '/checkout/'
-export const CART_URL = '/checkout/cart/'
-export const PAYMENT_URL = '/checkout/payment/'
-export const CREATE_ACCOUNT_POST_URL = '/customer/account/createpost/'
-export const LOGIN_POST_URL = '/customer/account/loginPost/'
