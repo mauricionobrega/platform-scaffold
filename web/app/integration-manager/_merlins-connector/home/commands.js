3e5a18712bcf759a22391049bb6d423c09c3897a
Jennifer Bandelin <jennifer@mobify.com>
2017-05-24 14:23:32 -0700

    Remove test code

--- web/app/integration-manager/_merlins-connector/home/commands.js
+++ web/app/integration-manager/_merlins-connector/home/commands.js
@@ -4,17 +4,11 @@
 
 import {fetchPageData} from '../app/commands'
 import homeParser from './parser'
-import {receiveHomeData, receiveUserCustomContent} from '../../results'
-import {receiveCheckoutCustomContent} from '../../checkout/results'
-import {receiveCartCustomContent} from '../../cart/results'
+import {receiveHomeData} from '../../results'
 
 export const initHomePage = (url) => (dispatch) => {
     return dispatch(fetchPageData(url))
         .then(([$, $response]) => {
             dispatch(receiveHomeData(homeParser($, $response)))
-            // For Testing purposes, don't merge this into develop
-            dispatch(receiveUserCustomContent({test: 'user content'}))
-            dispatch(receiveCheckoutCustomContent({test: 'checkout content'}))
-            dispatch(receiveCartCustomContent({test: 'cart content'}))
         })
 }
