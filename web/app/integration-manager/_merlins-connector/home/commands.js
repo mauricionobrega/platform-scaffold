0b0483cb4a7f00446ea7f99af51fdcb4cfba00e4
Jennifer Bandelin <jennifer@mobify.com>
2017-05-24 13:57:24 -0700

    Add temporary custom action dispatches

--- web/app/integration-manager/_merlins-connector/home/commands.js
+++ web/app/integration-manager/_merlins-connector/home/commands.js
@@ -4,11 +4,17 @@
 
 import {fetchPageData} from '../app/commands'
 import homeParser from './parser'
-import {receiveHomeData} from '../../results'
+import {receiveHomeData, receiveUserCustomContent} from '../../results'
+import {receiveCheckoutCustomContent} from '../../checkout/results'
+import {receiveCartCustomContent} from '../../cart/results'
 
 export const initHomePage = (url) => (dispatch) => {
     return dispatch(fetchPageData(url))
         .then(([$, $response]) => {
             dispatch(receiveHomeData(homeParser($, $response)))
+            // For Testing purposes, don't merge this into develop
+            dispatch(receiveUserCustomContent({test: 'user content'}))
+            dispatch(receiveCheckoutCustomContent({test: 'checkout content'}))
+            dispatch(receiveCartCustomContent({test: 'cart content'}))
         })
 }
