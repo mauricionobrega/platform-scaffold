44b282e392d1caebc76bb8e1e7e3cb9eae4b4a60
Cdok <cdokurno@mobify.com>
2017-05-24 11:44:10 -0700

    formatted / style fix to sfcc constants

--- web/app/integration-manager/_sfcc-connector/constants.js
+++ web/app/integration-manager/_sfcc-connector/constants.js
@@ -2,6 +2,9 @@
 /* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 
+const API_TYPE = 'shop'
+const API_VERSION = 'v17_4'
+
 let config = {}
 
 export const registerConfig = (cfg) => {
@@ -10,24 +13,16 @@ export const registerConfig = (cfg) => {
 
 export const getSiteID = () => config.siteID
 
-const API_TYPE = 'shop'
-const API_VERSION = 'v17_4'
-
-
 export const getApiEndPoint = () => `/s/${config.siteID}/dw/${API_TYPE}/${API_VERSION}`
 
-export const getRequestHeaders = () => {
-    return {
-        'Content-Type': 'application/json',
-        'x-dw-client-id': config.clientID
-    }
-}
+export const getRequestHeaders = () => ({
+    'Content-Type': 'application/json',
+    'x-dw-client-id': config.clientID
+})
 
 export const getHomeURL = () => `/on/demandware.store/${config.siteID}/default/Home-Show`
-
 export const getSignInURL = () => `/on/demandware.store/${config.siteID}/default/Account-Show`
 export const getSignOutURL = () => `/on/demandware.store/${config.siteID}/default/Logout-Logout`
-
 export const getCheckoutShippingURL = () => `/on/demandware.store/${config.siteID}/default/COShipping-Start`
 export const getCartURL = () => `/on/demandware.store/${config.siteID}/default/Cart-Show`
 export const getPaymentURL = () => `/on/demandware.store/${config.siteID}/default/COBilling-Start`
