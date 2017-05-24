a2e144a8dda29347fa89cef3967dba1232bef5f0
Jennifer Bandelin <jennifer@mobify.com>
2017-05-24 15:43:53 -0700

    Add reducer test for locations custom branch

--- web/app/store/checkout/reducer.test.js
+++ web/app/store/checkout/reducer.test.js
@@ -5,7 +5,7 @@
 /* eslint-env jest */
 import Immutable from 'immutable'
 import reducer from './reducer'
-import {receiveCheckoutCustomContent} from '../../integration-manager/checkout/results'
+import * as checkoutActions from '../../integration-manager/checkout/results'
 
 /* eslint-disable newline-per-chained-call */
 
@@ -20,7 +20,15 @@ test('unknown action type leaves state unchanged', () => {
 test('receiveCheckoutCustomContent updates the custom branch of the checkout state', () => {
     const customContent = {test: 'checkout content'}
     const expectedState = Immutable.fromJS({custom: customContent})
-    const action = receiveCheckoutCustomContent(customContent)
+    const action = checkoutActions.receiveCheckoutCustomContent(customContent)
 
     expect(reducer(Immutable.Map(), action)).toEqual(expectedState)
+})
+
+test('receiveLocationsCustomContent updates the custom branch of locations', () => {
+    const customContent = {test: 'locations content'}
+    const expectedState = {locations: {custom: customContent}}
+    const action = checkoutActions.receiveLocationsCustomContent(customContent)
+
+    expect(reducer(Immutable.Map(), action).toJS()).toEqual(expectedState)
 })
