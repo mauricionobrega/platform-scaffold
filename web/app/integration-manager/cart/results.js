d9050dbbef68afebd7e6de73abc92e433f601960
Jennifer Bandelin <jennifer@mobify.com>
2017-05-24 13:15:04 -0700

    Add reducer and result for custom branch in the cart state

--- web/app/integration-manager/cart/results.js
+++ web/app/integration-manager/cart/results.js
@@ -3,6 +3,8 @@
 /* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
 
 import {createTypedAction} from '../../utils/utils'
+import {createAction} from 'progressive-web-sdk/dist/utils/action-creation'
 import {Cart} from './types'
 
 export const receiveCartContents = createTypedAction('Receive Cart Contents', Cart)
+export const receiveCartCustomContent = createAction('Receive Cart Custom Contents', ['custom'])
